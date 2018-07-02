32b4f458e6771389b1ab87dc2e07ba4b49cc5f21
ybouret <yann.bouret@gmail.com>
2018-07-02 08:47:08 +0200

    no...revert to dynamic

---
+++ src/main/y/dynamic-object.hpp
@@ -0,0 +1,14 @@
+//! \file
+#ifndef Y_DYNAMIC_OBJECT_INCLUDED
+#define Y_DYNAMIC_OBJECT_INCLUDED 1
+
+#include "y/object.hpp"
+
+namespace yocto
+{
+    
+}
+
+#endif
+
+
--- src/main/y/dynamic.hpp
+++
@@ -1,3 +0,0 @@
-//! \file dynamic.hpp
-
-
