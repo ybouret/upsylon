fde20e15c0f0127cb65e5b5689861e55329318fe
ybouret <yann.bouret@gmail.com>
2018-07-04 13:28:09 +0200

    cleanup, still need to test

--- src/main/tests/test-threads.cpp
+++ src/main/tests/test-threads.cpp
@@ -3,28 +3,6 @@
 
 using namespace upsylon;
 
-namespace
-{
-#if 0
-    static const int count = 8;
-    static int       ready = 0;
-
-    static void myfunc( void *data )
-    {
-        concurrent::threads &crew = *static_cast<concurrent::threads *>(data);
-        {
-            crew.access.lock();
-            ++ready;
-            std::cerr << "init myfunc..." << ready << std::endl;
-            crew.sync.wait(crew.access);
-            std::cerr << "quit myfunc..." << std::endl;
-
-            --ready;
-            crew.access.unlock();
-        }
-    }
-#endif
-}
 
 Y_UTEST(threads)
 {
@@ -36,48 +14,6 @@ Y_UTEST(threads)
     }
     concurrent::threads threads(n,true);
 
-#if 0
-    concurrent::threads crew;
-    std::cerr << "Starting..." << std::endl;
-    for(int i=0;i<count;++i)
-    {
-        crew.launch(myfunc,&crew);
-    }
 
-    while(true)
-    {
-        if( crew.access.try_lock() )
-        {
-            if(ready<count)
-            {
-                crew.access.unlock();
-            }
-            else
-            {
-                crew.access.unlock();
-                break;
-            }
-        }
-    }
-
-    std::cerr << "back in main" << std::endl;
-    crew.sync.broadcast();
-
-    while(true)
-    {
-        if( crew.access.try_lock() )
-        {
-            if(ready>0)
-            {
-                crew.access.unlock();
-            }
-            else
-            {
-                crew.access.unlock();
-                break;
-            }
-        }
-    }
-#endif
 }
 Y_UTEST_DONE()
