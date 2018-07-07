4ca169d6ed3b24542bb45699959cc1893a6bb6bf
ybouret <yann.bouret@gmail.com>
2018-07-07 12:59:20 +0200

    need to change code

--- src/main/y/dynamic.hpp
+++ src/main/y/dynamic.hpp
@@ -6,18 +6,28 @@
 
 namespace upsylon
 {
-    //! common functions for dynamic containers
+    //! common functions for dynamic containers: size() and capacity()
     class dynamic
     {
     public:
         //! destructor
         virtual ~dynamic() throw();
 
+        //______________________________________________________________________
+        //
+        // virtual interface
+        //______________________________________________________________________
+
         //! currently available objects
         virtual size_t size()     const throw() = 0;
 
         //! maximum available objects
         virtual size_t capacity() const throw() = 0;
+
+        //______________________________________________________________________
+        //
+        // non-virtual interface
+        //______________________________________________________________________
 
         //! available objects
         inline  size_t available() const throw() { assert(size()<=capacity()); return capacity() - size(); }
