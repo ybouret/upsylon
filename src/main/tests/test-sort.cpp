9007302589e21539cdff3d4ce503b1925b25fa60
ybouret <yann.bouret@gmail.com>
2018-07-13 14:45:27 +0200

    WIP merge sort

---
+++ src/main/tests/test-sort.cpp
@@ -0,0 +1,11 @@
+#include "y/sort/merge.hpp"
+#include "y/utest/run.hpp"
+
+using namespace upsylon;
+
+Y_UTEST(sort)
+{
+    
+}
+Y_UTEST_DONE()
+
---
+++ src/main/y/sort/merge.hpp
@@ -0,0 +1,64 @@
+//! \file
+#ifndef Y_SORT_MERGE_INCLUDED
+#define Y_SORT_MERGE_INCLUDED 1
+
+#include "y/core/list.hpp"
+
+namespace upsylon
+{
+    //! assuming a doubly linked node
+    template <typename NODE>
+    class merging
+    {
+    public:
+        typedef core::list_of<NODE> sub_list;
+        typedef int (*compare_node_proc)(const NODE *lhs, const NODE *rhs, void *args);
+
+        template <typename LIST>
+        void sort( LIST &source, compare_node_proc proc, void *args)
+        {
+            if( source.size > 1 )
+            {
+                sub_list L;
+                sub_list R;
+                const size_t        mid = source.size >> 1;
+                for( size_t i=source.size-mid;i>0;--i) L.push_front( source.pop_back() );
+                for( size_t i=mid;i>0;--i)             R.push_front( source.pop_back() );
+                assert(0==source.size);
+
+                merging<NODE>::template sort<sub_list>(L,proc,args);
+                merging<NODE>::template sort<sub_list>(R,proc,args);
+                merging<NODE>::template fusion<LIST>(source,L,R,proc,args);
+            }
+        }
+
+    private:
+        //! merge two sorted list
+        template <typename LIST>
+        static inline
+        void fusion( LIST &target, sub_list &L, sub_list &R, compare_node_proc proc, void *args )
+        {
+            //! while we can compare two nodes
+            assert(0==target.size);
+            while( L.size > 0 && R.size > 0 )
+            {
+                if( proc(L.head,R.head,args) < 0 )
+                {
+                    target.push_back( L.pop_front() );
+                }
+                else
+                {
+                    target.push_back( R.pop_front() );
+                }
+            }
+
+            //! append remaining lists
+            assert(0==L.size||0==R.size);
+            while( L.size > 0 ) target.push_back( L.pop_front() );
+            while( R.size > 0 ) target.push_back( R.pop_front() );
+        }
+    };
+}
+
+#endif
+
