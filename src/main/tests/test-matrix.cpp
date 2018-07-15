6b6f3ba3a8d8f05936665b1d8d5aa5892a3db0cd
ybouret <yann.bouret@gmail.com>
2018-07-15 09:24:51 +0200

    WIP matrix

--- src/main/tests/test-matrix.cpp
+++ src/main/tests/test-matrix.cpp
@@ -1,8 +1,37 @@
 #include "y/container/matrix.hpp"
 #include "y/utest/run.hpp"
+#include "y/string.hpp"
+#include <typeinfo>
+#include "support.hpp"
 
 using namespace upsylon;
 
+namespace  {
+
+    template <typename T>
+    static inline
+    void do_test()
+    {
+        std::cerr << "sizeof(matrix<" << typeid(T).name() << ">)=" << sizeof(matrix<T>) << std::endl;
+        { matrix<T> m0; }
+
+        for(size_t iter=0;iter<10;++iter)
+        {
+            matrix<T> m(1+alea.leq(10),1+alea.leq(10));
+
+            for(size_t r=1;r<=m.rows;++r)
+            {
+                for(size_t c=1;c<=m.cols;++c)
+                {
+                    T v = support::get<T>();
+                    m[r][c] = v;
+                }
+            }
+            std::cerr << "m" << m.rows << "x" << m.cols << "=" << m << std::endl;
+        }
+
+    }
+}
 Y_UTEST(matrix)
 {
     matrix_data m(3,5,4);
@@ -11,6 +40,10 @@ Y_UTEST(matrix)
     std::cerr << "r_indx=" << m.r_indx << std::endl;
     std::cerr << "c_indx=" << m.c_indx << std::endl;
 
+    do_test<float>();
+    do_test<double>();
+    do_test<int>();
+    do_test<string>();
 }
 Y_UTEST_DONE()
 
--- src/main/y/container/matrix.hpp
+++ src/main/y/container/matrix.hpp
@@ -38,6 +38,118 @@ namespace upsylon
         void __kill() throw();
     };
 
+
+    template <typename T>
+    class matrix : public matrix_data
+    {
+    public:
+        Y_DECL_ARGS(T,type);
+        typedef lightweight_array<T> row;
+
+        row r_scalars;   //!< size() = rows
+        row c_scalars;   //!< size() = cols
+        row r_auxiliary; //!< size() = rows
+        row c_auxiliary; //!< size() = cols
+
+        inline matrix(const size_t nr=0, const size_t nc=0) :
+        matrix_data(nr,nc,sizeof(T)), row_ptr(0)
+        {
+            setup();
+            init0();
+        }
+
+        //! row access
+        inline array<T>  & operator[](const size_t r) throw()       { assert(r>0); assert(r<=rows); return row_ptr[r]; }
+        //! row access
+        inline const array<T>  & operator[](const size_t r) const throw() { assert(r>0); assert(r<=rows); return row_ptr[r]; }
+
+        //! destructor
+        inline virtual ~matrix() throw()
+        {
+            __clear(total_items);
+        }
+
+        inline friend std::ostream & operator<<( std::ostream &os, const matrix &m )
+        {
+            os << '[';
+            for(size_t r=1;r<=m.rows;++r)
+            {
+                const array<T> &R = m[r];
+                for(size_t c=1;c<=m.cols;++c)
+                {
+                    os << R[c]; if(c<m.cols) os << ' ';
+                }
+                if(r<m.rows) os << ';';
+            }
+            os << ']';
+            return os;
+        }
+        
+
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(matrix);
+        row *row_ptr; //! [1..rows]
+
+        //! just preparing memory
+        void setup() throw()
+        {
+            //__________________________________________________________________
+            //
+            // prepare auxiliary arrays
+            //__________________________________________________________________
+            {
+            T *extra = static_cast<T*>(workspace)+items;
+            new (&r_scalars) row(extra,rows);
+            new (&c_scalars) row(extra,cols);
+            extra += largest;
+            new (&r_auxiliary) row(extra,rows);
+            new (&c_auxiliary) row(extra,cols);
+            }
+            //__________________________________________________________________
+            //
+            // prepare rows
+            //__________________________________________________________________
+            row_ptr = memory::io::cast<row>(workspace,rows_offset);
+            {
+                T *p = memory::io::cast<T>(workspace,data_offset);
+                for(size_t r=0;r<rows;++r,p+=cols)
+                {
+                    new (row_ptr+r) row(p,cols);
+                }
+            }
+            --row_ptr;
+        }
+
+        inline void __clear(size_t count) throw()
+        {
+            mutable_type *entry = memory::io::cast<mutable_type>(workspace,data_offset);
+            while(count-->0)
+            {
+                destruct(entry+count);
+            }
+        }
+
+        //! default initialisation
+        inline void init0()
+        {
+            size_t        count = 0;
+            mutable_type *entry = memory::io::cast<mutable_type>(workspace,data_offset);
+            try
+            {
+                while(count<total_items)
+                {
+                    new (entry+count) T();
+                    ++count;
+                }
+            }
+            catch(...)
+            {
+                __clear(count);
+                throw;
+            }
+        }
+    };
+
 }
 
 #endif
