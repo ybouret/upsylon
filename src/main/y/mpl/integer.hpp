a7efdb2a6357d80846300a0a81706339ac4f40b5
ybouret <yann.bouret@gmail.com>
2018-07-15 16:43:56 +0200

    WIP integer

--- src/main/y/mpl/integer.hpp
+++ src/main/y/mpl/integer.hpp
@@ -35,6 +35,12 @@ namespace upsylon
             return (i<0) ? __negative : ( (0<i)  ? __positive : __zero );
         }
 
+        //! absolute value
+        inline word_t word_for(const integer_t i) throw()
+        {
+            return (i<0) ? word_t(-i) : word_t(i);
+        }
+
         //! integer class
         class integer : public object
         {
@@ -44,15 +50,37 @@ namespace upsylon
 
             //! zero constructor
             inline integer() : s(__zero), n() {}
+
             //! destructor
             inline virtual ~integer() throw() {}
+
             //! copy constructor
             inline integer(const integer &z) : s(z.s), n(z.n) {}
 
-            void update() throw()
+            //!  constructor from integral
+            inline integer(const integer_t &i) : s( sign_for(i) ), n( word_for(i) ) {}
+
+            //! constructor from natural
+            inline integer(const natural &u) : s( u.is_zero() ? __zero : __positive ), n(u) {}
+
+            //! status update
+            inline void update() throw()
             {
                 if(n.is_zero()) (sign_type &)s = __zero;
             }
+
+            // output
+            inline friend std::ostream & operator<<( std::ostream &os, const integer &z )
+            {
+                if(__negative==z.s) os << '-';
+                return (os << z.n);
+            }
+
+            //! assignment
+            
+
+        private:
+
         };
 
     }
