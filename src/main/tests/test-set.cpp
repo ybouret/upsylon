f13a7a1e8266e0f622bc6d68fc813728675c90ec
ybouret <yann.bouret@gmail.com>
2018-07-15 06:51:42 +0200

    intr_ptr is ok

--- src/main/tests/test-set.cpp
+++ src/main/tests/test-set.cpp
@@ -1,7 +1,8 @@
 #include "y/associative/set.hpp"
 #include "y/utest/run.hpp"
 #include "y/sequence/vector.hpp"
-
+#include "y/ptr/intr.hpp"
+#include "y/ptr/counted.hpp"
 #include "support.hpp"
 
 using namespace upsylon;
@@ -9,16 +10,17 @@ using namespace upsylon;
 namespace
 {
     template <typename KEY,typename T>
-    class dummy
+    class dummy : public counted_object
     {
     public:
+        typedef intr_ptr< KEY,dummy<KEY,T> > pointer;
         const KEY    label;
         const T      value;
 
         const KEY & key() const throw() { return label; }
 
         dummy( const KEY &l, const T &v ) : label( l ), value( v ) {}
-        ~dummy() throw() {}
+        virtual ~dummy() throw() {}
         dummy( const dummy &other ) : label(other.label), value(other.value) {}
 
 
@@ -30,38 +32,69 @@ namespace
     template <typename KEY,typename T>
     static inline void do_test()
     {
-        typedef set< KEY, dummy<KEY,T> > set_type;
-        { set_type db0; }
-        { set_type db1(100,as_capacity); }
-        set_type db;
-        size_t count = 0;
-        vector<KEY> keys;
-        for(size_t i=0;i<1000;++i)
+        // by copy
+        std::cerr << "-- by copy" << std::endl;
         {
-            const KEY k = support::get<KEY>();
-            const T   v = support::get<T>();
-            const dummy<KEY,T> d(k,v);
-
-            if( db.insert(d) )
+            typedef set< KEY, dummy<KEY,T> > set_type;
+            { set_type db0; }
+            { set_type db1(100,as_capacity); }
+            set_type db;
+            size_t count = 0;
+            vector<KEY> keys;
+            for(size_t i=0;i<1000;++i)
             {
-                ++count;
-                keys.push_back(k);
+                const KEY k = support::get<KEY>();
+                const T   v = support::get<T>();
+                const dummy<KEY,T> d(k,v);
+
+                if( db.insert(d) )
+                {
+                    ++count;
+                    keys.push_back(k);
+                }
+            }
+            std::cerr << "count=" << count << std::endl;
+            Y_CHECK(count==db.size());
+            Y_CHECK(count==keys.size());
+            alea.shuffle(*keys,keys.size());
+            const set_type &dbc = db;
+            for(size_t i=1;i<=keys.size();++i)
+            {
+                Y_ASSERT(db.search(keys[i]));
+                Y_ASSERT(dbc.search(keys[i]));
+            }
+            for(size_t i=1;i<=keys.size();++i)
+            {
+                Y_ASSERT(db.remove(keys[i]));
             }
         }
-        std::cerr << "count=" << count << std::endl;
-        Y_CHECK(count==db.size());
-        Y_CHECK(count==keys.size());
-        alea.shuffle(*keys,keys.size());
-        const set_type &dbc = db;
-        for(size_t i=1;i<=keys.size();++i)
+
+        // by pointer
+        std::cerr << "-- by pointer" << std::endl;
         {
-            Y_ASSERT(db.search(keys[i]));
-            Y_ASSERT(dbc.search(keys[i]));
+            typedef typename dummy<KEY,T>::pointer dptr_t;
+            typedef set<KEY,dptr_t>                dset_t;
+
+            dset_t db;
+            size_t count = 0;
+            vector<KEY> keys;
+            for(size_t i=0;i<1000;++i)
+            {
+                const KEY k = support::get<KEY>();
+                const T   v = support::get<T>();
+                const dptr_t d( new dummy<KEY,T>(k,v) );
+
+                if( db.insert(d) )
+                {
+                    ++count;
+                    keys.push_back(k);
+                }
+            }
+            std::cerr << "count=" << count << std::endl;
+            Y_CHECK(count==db.size());
+            Y_CHECK(count==keys.size());
         }
-        for(size_t i=1;i<=keys.size();++i)
-        {
-            Y_ASSERT(db.remove(keys[i]));
-        }
+
     }
 
 }
---
+++ src/main/y/ptr/intr.hpp
@@ -0,0 +1,67 @@
+
+//! \file
+#ifndef Y_PTR_INTR_INCLUDED
+#define Y_PTR_INTR_INCLUDED 1
+
+#include "y/ptr/ptr.hpp"
+
+namespace upsylon
+{
+    //! A Reference Counted with key
+    /**
+     T must have the following methods
+     - void   withhold() throw()
+     - bool   liberate() throw()
+     - size_t refcount() const throw()
+     - const_key_type & key() const throw()
+     */
+    template <typename KEY,typename T>
+    class intr_ptr : public ptr<T>
+    {
+    public:
+        Y_DECL_ARGS(KEY,key_type);
+
+        //! attache and withhold address
+        inline intr_ptr(T *addr) throw() : ptr<T>(addr)
+        {
+            assert(this->pointee||die("not accepting null address"));
+            this->pointee->withhold();
+        }
+
+        //! copy by withholding
+        inline intr_ptr(const intr_ptr &other) throw() :
+        ptr<T>(other)
+        {
+            this->pointee->withhold();
+        }
+
+        //! assign another pointer
+        inline intr_ptr & operator=(const intr_ptr &other) throw()
+        {
+            intr_ptr tmp(other);
+            this->swap_with(tmp);
+            return *this;
+        }
+
+        //! delete resource if necessary
+        inline virtual ~intr_ptr() throw()
+        {
+            if(this->pointee->liberate())
+            {
+                delete (this->pointee);
+            }
+        }
+
+        //! transitive key access
+        inline const const_key_type & key() const throw()
+        {
+            assert(this->pointee);
+            return this->pointee->key();
+        }
+
+    };
+}
+
+#endif
+
+
