b02ba53de6565cbfef971b8cf07d3dfd116cb22f
ybouret <yann.bouret@gmail.com>
2018-07-20 23:05:51 +0200

    splitting tao codes

--- src/math/y/math/kernel/tao.hpp
+++ src/math/y/math/kernel/tao.hpp
@@ -11,6 +11,7 @@ namespace upsylon
     namespace math
     {
 
+        //! Templated Algebraic Operations
         struct tao
         {
             ////////////////////////////////////////////////////////////////////
@@ -18,44 +19,10 @@ namespace upsylon
             // level-1 : array ops
             //
             ////////////////////////////////////////////////////////////////////
-            template <typename T>
-            static void _ld( array<T> &a, typename type_traits<T>::parameter_type v )
-            {
-                for(size_t i=a.size();i>0;--i)
-                {
-                    a[i] = v;
-                }
-            }
-
-            template <typename T> struct __ld
-            {
-                Y_DECL_ARGS(T,type);
-                array<type> *a;
-                const_type  *v;
-                static inline void call(void *args, parallel &ctx, lockable &)
-                {
-                    __ld        *self = (__ld *)args;
-                    array<type> &a    = *(self->a);
-                    const_type  &v    = *(self->v);
-                    size_t offset = 1;
-                    size_t length = a.size();
-                    ctx.split(length,offset);
-                    while(length--)
-                    {
-                        a[offset++] = v;
-                    }
-                }
-            };
-
-            template <typename T>
-            static void _ld( array<T> &a, typename type_traits<T>::parameter_type v, concurrent::for_each &loop )
-            {
-                __ld<T> args = { &a, &v };
-                loop.run( __ld<T>::call, &args );
-            }
-
-            template <typename T>
-            static void ld( array<T> &a, typename type_traits<T>::parameter_type v, concurrent::for_each *loop )
+#include "tao-ld.hxx"
+            //! a[1..size()]=v
+            template <typename T> static void
+            ld( array<T> &a, typename type_traits<T>::parameter_type v, concurrent::for_each *loop )
             {
                 if(loop)
                 {
