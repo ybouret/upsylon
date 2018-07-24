c7b5ef84a5bbb1f941a134ffaab0f4033670aab9
ybouret <yann.bouret@gmail.com>
2018-07-24 08:02:24 +0200

    WIP

--- src/math/tests/test-data-set.cpp
+++ src/math/tests/test-data-set.cpp
@@ -1,10 +1,43 @@
 #include "y/math/io/data-set.hpp"
 #include "y/utest/run.hpp"
+#include "y/ios/icstream.hpp"
+
+#include "y/sequence/vector.hpp"
+#include "y/sequence/list.hpp"
 
 using namespace upsylon;
+using namespace math;
+
+namespace
+{
+    template <typename T>
+    static inline
+    void load(const string &filename,
+              const size_t  ix,
+              const size_t  iy)
+    {
+        data_set<T> ds;
+        ios::icstream fp(filename);
+        vector<T> x;
+        list<T>   y;
+        ds.use(ix,x);
+        ds.use(iy,y);
+
+        ds.free_all();
+        ds.load(fp);
+    }
+}
 
 Y_UTEST(data_set)
 {
+
+    if(argc>1)
+    {
+        const string fn = argv[1];
+        load<float>(fn,1,2);
+        load<double>(fn,2,1);
+    }
+
 }
 Y_UTEST_DONE()
 
--- src/math/y/math/io/data-set.hpp
+++ src/math/y/math/io/data-set.hpp
@@ -3,8 +3,12 @@
 #define Y_MATH_DATA_SET_INCLUDED 1
 
 #include "y/associative/map.hpp"
-#include "y/ios/istream.hpp"
 #include "y/container/sequence.hpp"
+#include "y/ios/istream.hpp"
+#include "y/exception.hpp"
+#include "y/string/tokenizer.hpp"
+#include "y/string/convert.hpp"
+#include "y/comparison.hpp"
 
 namespace upsylon
 {
@@ -16,13 +20,74 @@ namespace upsylon
         {
         public:
             Y_DECL_ARGS(T,type);
-            typedef sequence<type> sequence_type;
+            typedef sequence<type>           sequence_type;
+            typedef sequence_type           *column;
+            typedef map<size_t,column>       colmap;
 
-            inline explicit data_set() {}
+            inline explicit data_set() : columns(4,as_capacity) {}
             inline virtual ~data_set() throw() {}
-            
+
+            inline void use( const size_t column_index, sequence_type &seq )
+            {
+                if(column_index<=0) throw exception("data_set: column index <= 0");
+                if(!columns.insert(column_index,&seq))
+                {
+                    throw exception("data_set: multiple column #%u", unsigned(column_index));
+                }
+            }
+
+            inline void free_all() throw()
+            {
+                for(typename colmap::iterator i=columns.begin();i!=columns.end();++i)
+                {
+                    (**i).free();
+                }
+            }
+
+            inline void load( ios::istream &fp, const size_t skip=0, const size_t nmax=0)
+            {
+                // preparing columns, order by increasing index
+                columns.sort_keys( comparison::increasing<size_t> );
+
+                unsigned iline = 1;
+                string   line;
+                for(;iline<=skip;++iline)
+                {
+                    if(!fp.gets(line)) throw exception("data_set: EOF while skipping line %u/%u", iline, unsigned(skip));
+                }
+                size_t loaded = 0;
+                while(fp.gets(line))
+                {
+                    // check content
+                    if(line.size()<=0) continue;
+                    if(line[0] == '#') continue;
+
+
+                    // now parse!
+                    tokenizer<char> tkn(line);
+                    for(typename colmap::iterator i=columns.begin();i!=columns.end();++i)
+                    {
+                        const size_t idx = i.key();
+                        while( tkn.count() != idx )
+                        {
+                            if(!tkn.next(__sep))
+                            {
+                                throw exception("");
+                            }
+                        }
+                    }
+                    if(nmax>0&&++loaded>=nmax)
+                    {
+                        break;
+                    }
+                    ++iline;
+                }
+            }
+
         private:
             Y_DISABLE_COPY_AND_ASSIGN(data_set);
+            colmap columns;
+            static inline bool __sep(const int C) { return ' '==C || '\t' == C; }
         };
     }
 }
