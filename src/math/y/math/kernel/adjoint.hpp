f91f73618fbea903707878e5028d31995cddedb5
ybouret <yann.bouret@gmail.com>
2018-07-25 16:25:28 +0200

    modified adjoint

--- src/math/y/math/kernel/adjoint.hpp
+++ src/math/y/math/kernel/adjoint.hpp
@@ -9,8 +9,37 @@ namespace upsylon
 {
     namespace math
     {
-        template <typename T>
-        inline void adjoint( matrix<T> &target, const matrix<T> &source )
+        template <typename T, typename U>
+        inline void adjoint( matrix<T> &target, const matrix<U> &source )
+        {
+            assert(target.rows==source.rows);
+            assert(target.cols==source.cols);
+            assert(source.is_square);
+            const size_t n = source.rows;
+            if(n<=1)
+            {
+                target[1][1] = T(1);
+            }
+            else
+            {
+                const size_t nm1 = n-1;
+                matrix<T> M(nm1,nm1);
+                for(size_t c=n;c>0;--c)
+                {
+                    array<T> &tc = target[c];
+                    for(size_t r=n;r>0;--r)
+                    {
+                        M.minor_of(source,r,c);
+                        const T d = __determinant(M);
+                        tc[r] = ( (r+c) & 1) ? -d:d;
+                    }
+                }
+            }
+        }
+
+
+        template <typename T, typename U>
+        inline void iadjoint( matrix<T> &target, const matrix<U> &source )
         {
             assert(target.rows==source.rows);
             assert(target.cols==source.cols);
@@ -30,14 +59,14 @@ namespace upsylon
                     for(size_t r=n;r>0;--r)
                     {
                         M.minor_of(source,r,c);
-                        const T d = __determinant(M);
+                        const T d = ideterminant(M);
                         tc[r] = ( (r+c) & 1) ? -d:d;
                     }
                 }
             }
-
-
         }
+
+
     }
 }
 
