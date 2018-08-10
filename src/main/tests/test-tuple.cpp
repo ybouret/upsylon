1773a1255d0cf3f43eebe21e61ce049b2228a3b4
ybouret <yann.bouret@gmail.com>
2018-08-10 08:19:04 +0200

    WIP tuples

--- src/main/tests/test-tuple.cpp
+++ src/main/tests/test-tuple.cpp
@@ -1,5 +1,6 @@
 #include "y/container/tuple.hpp"
 #include "y/utest/run.hpp"
+#include "y/string.hpp"
 
 using namespace upsylon;
 
@@ -7,16 +8,60 @@ namespace
 {
     Y_PAIR_DECL(STANDARD,dummy2,int,v1,unsigned,v2);
     Y_PAIR_END();
+    
+    Y_TRIPLE_DECL(STANDARD,dummy3,long int,v1,unsigned,v2,size_t,v3);
+    Y_TRIPLE_END();
+    
+    Y_QUAD_DECL(STANDARD,dummy4,int16_t,v1,int32_t,v2,int64_t,v3,char,v4);
+    Y_QUAD_END();
+    
+    Y_PENTUPLE_DECL(STANDARD,dummy5,char,v1,short,v2,int,v3,long,v4,string,v5);
+    Y_PENTUPLE_END();
 }
 
 Y_UTEST(tuple)
 {
-    dummy2 d2a(1,2);
-    dummy2 d2b(1,3);
-    Y_CHECK(d2a!=d2b);
-    Y_CHECK(!(d2a==d2b));
-    std::cerr << "d2a=" << d2a << std::endl;
-    std::cerr << "d2b=" << d2b << std::endl;
+    {
+        dummy2 d2a(1,2);
+        dummy2 d2b(1,3);
+        dummy2 d2c = d2a;
+        Y_CHECK(d2a!=d2b);
+        Y_CHECK(!(d2a==d2b));
+        std::cerr << "d2a=" << d2a << std::endl;
+        std::cerr << "d2b=" << d2b << std::endl;
+    }
+    
+    {
+        dummy3 d3a(1,2,3);
+        dummy3 d3b(1,3,3);
+        dummy3 d3c = d3a;
+        Y_CHECK(d3a!=d3b);
+        Y_CHECK(!(d3a==d3b));
+        std::cerr << "d3a=" << d3a << std::endl;
+        std::cerr << "d3b=" << d3b << std::endl;
+    }
+    
+    {
+        dummy4 d4a(1,2,3,'a');
+        dummy4 d4b(1,2,3,'b');
+        dummy4 d4c = d4a;
+        Y_CHECK(d4a!=d4b);
+        Y_CHECK(!(d4a==d4b));
+        std::cerr << "d4a=" << d4a << std::endl;
+        std::cerr << "d4b=" << d4b << std::endl;
+    }
+    
+    {
+        dummy5 d5a('a','b','c','d',"e");
+        dummy5 d5b('a','b','c','d',"f");
+        dummy5 d5c = d5a;
+        Y_CHECK(d5a!=d5b);
+        Y_CHECK(!(d5a==d5b));
+        std::cerr << "d5a=" << d5a << std::endl;
+        std::cerr << "d5b=" << d5b << std::endl;
+    }
+    
+    
 }
 Y_UTEST_DONE()
 
--- src/main/y/container/tuple.hpp
+++ src/main/y/container/tuple.hpp
@@ -13,6 +13,16 @@ namespace upsylon
     //! for tuple in a template
 #define Y_TUPLE_TEMPLATE(T) typename type_traits< T >::parameter_type
     
+    //! open brace
+#define Y_TUPLE_INI() {
+    //! close brace
+#define Y_TUPLE_END() }
+    //__________________________________________________________________________
+    //
+    // PAIR
+    //__________________________________________________________________________
+
+    
     //! pair prototype
 #define Y_PAIR_DECL(PARAM,CLASS,T1,N1,T2,N2) \
 class CLASS : public virtual object {        \
@@ -41,7 +51,125 @@ T1 N1; T2 N2
     //! pair end
 #define Y_PAIR_END() }
     
+    //__________________________________________________________________________
+    //
+    // TRIPLE
+    //__________________________________________________________________________
     
+    //! triple prototype
+#define Y_TRIPLE_DECL(PARAM,CLASS,T1,N1,T2,N2,T3,N3) \
+class CLASS : public virtual object {        \
+public:                                      \
+typedef Y_TUPLE_##PARAM(T1) PARAM1;          \
+typedef Y_TUPLE_##PARAM(T2) PARAM2;          \
+typedef Y_TUPLE_##PARAM(T3) PARAM3;          \
+inline CLASS( PARAM1 P1, PARAM2 P2, PARAM3 P3) : N1(P1), N2(P2), N3(P3)  {} \
+inline CLASS(const CLASS &other) : object(), N1(other.N1), N2(other.N2), N3(other.N3) {} \
+inline virtual ~CLASS() throw() {}                                         \
+friend inline                                                              \
+std::ostream & operator<<( std::ostream &os, const CLASS &self )           \
+{                                                                          \
+os\
+<< '(' << #N1 << '=' << self.N1 \
+<< ',' << #N2 << '=' << self.N2 \
+<< ',' << #N3 << '=' << self.N3 \
+<< ')'; \
+return os;                                                                 \
+}                                                                          \
+friend inline bool operator==(const CLASS &lhs, const CLASS &rhs) throw()  \
+{                                                                          \
+return (lhs.N1==rhs.N1) && (lhs.N2==rhs.N2) && (lhs.N3==rhs.N3);           \
+}                                                                          \
+friend inline bool operator!=(const CLASS &lhs, const CLASS &rhs) throw()  \
+{                                                                          \
+return (lhs.N1!=rhs.N1) || (lhs.N2!=rhs.N2) || (lhs.N3!=rhs.N3);           \
+}                                                                          \
+T1 N1; T2 N2; T3 N3
+    
+    //! triple end
+#define Y_TRIPLE_END() }
+    
+    //__________________________________________________________________________
+    //
+    // QUAD
+    //__________________________________________________________________________
+    
+    //! quad prototype
+#define Y_QUAD_DECL(PARAM,CLASS,T1,N1,T2,N2,T3,N3,T4,N4) \
+class CLASS : public virtual object {        \
+public:                                      \
+typedef Y_TUPLE_##PARAM(T1) PARAM1;          \
+typedef Y_TUPLE_##PARAM(T2) PARAM2;          \
+typedef Y_TUPLE_##PARAM(T3) PARAM3;          \
+typedef Y_TUPLE_##PARAM(T4) PARAM4;          \
+inline CLASS( PARAM1 P1, PARAM2 P2, PARAM3 P3, PARAM4 P4) : N1(P1), N2(P2), N3(P3), N4(P4)  {} \
+inline CLASS(const CLASS &other) : object(), N1(other.N1), N2(other.N2), N3(other.N3), N4(other.N4) {} \
+inline virtual ~CLASS() throw() {}                                         \
+friend inline                                                              \
+std::ostream & operator<<( std::ostream &os, const CLASS &self )           \
+{                                                                          \
+os\
+<< '(' << #N1 << '=' << self.N1 \
+<< ',' << #N2 << '=' << self.N2 \
+<< ',' << #N3 << '=' << self.N3 \
+<< ',' << #N4 << '=' << self.N4 \
+<< ')'; \
+return os;                                                                 \
+}                                                                          \
+friend inline bool operator==(const CLASS &lhs, const CLASS &rhs) throw()  \
+{                                                                          \
+return (lhs.N1==rhs.N1) && (lhs.N2==rhs.N2) && (lhs.N3==rhs.N3) && (lhs.N4==rhs.N4); \
+}                                                                          \
+friend inline bool operator!=(const CLASS &lhs, const CLASS &rhs) throw()  \
+{                                                                          \
+return (lhs.N1!=rhs.N1) || (lhs.N2!=rhs.N2) || (lhs.N3!=rhs.N3) || (lhs.N4!=rhs.N4); \
+}                                                                          \
+T1 N1; T2 N2; T3 N3; T4 N4
+    
+    //! quad end
+#define Y_QUAD_END() }
+    
+    //__________________________________________________________________________
+    //
+    // PENTUPLE
+    //__________________________________________________________________________
+    
+    //! pentuple prototype
+#define Y_PENTUPLE_DECL(PARAM,CLASS,T1,N1,T2,N2,T3,N3,T4,N4,T5,N5) \
+class CLASS : public virtual object {        \
+public:                                      \
+typedef Y_TUPLE_##PARAM(T1) PARAM1;          \
+typedef Y_TUPLE_##PARAM(T2) PARAM2;          \
+typedef Y_TUPLE_##PARAM(T3) PARAM3;          \
+typedef Y_TUPLE_##PARAM(T4) PARAM4;          \
+typedef Y_TUPLE_##PARAM(T5) PARAM5;          \
+inline CLASS( PARAM1 P1, PARAM2 P2, PARAM3 P3, PARAM4 P4, PARAM5 P5) : N1(P1), N2(P2), N3(P3), N4(P4), N5(P5)  {} \
+inline CLASS(const CLASS &other) : object(), N1(other.N1), N2(other.N2), N3(other.N3), N4(other.N4), N5(other.N5) {} \
+inline virtual ~CLASS() throw() {}                                         \
+friend inline                                                              \
+std::ostream & operator<<( std::ostream &os, const CLASS &self )           \
+{                                                                          \
+os\
+<< '(' << #N1 << '=' << self.N1 \
+<< ',' << #N2 << '=' << self.N2 \
+<< ',' << #N3 << '=' << self.N3 \
+<< ',' << #N4 << '=' << self.N4 \
+<< ',' << #N5 << '=' << self.N5 \
+<< ')'; \
+return os;                                                                 \
+}                                                                          \
+friend inline bool operator==(const CLASS &lhs, const CLASS &rhs) throw()  \
+{                                                                          \
+return (lhs.N1==rhs.N1) && (lhs.N2==rhs.N2) && (lhs.N3==rhs.N3) && (lhs.N4==rhs.N4) && (lhs.N5==rhs.N5); \
+}                                                                          \
+friend inline bool operator!=(const CLASS &lhs, const CLASS &rhs) throw()  \
+{                                                                          \
+return (lhs.N1!=rhs.N1) || (lhs.N2!=rhs.N2) || (lhs.N3!=rhs.N3) || (lhs.N4!=rhs.N4) || (lhs.N5!=rhs.N5); \
+}                                                                          \
+T1 N1; T2 N2; T3 N3; T4 N4; T5 N5
+    
+    //! quad end
+#define Y_PENTUPLE_END() }
     
 }
 
