a0435733132ee985448345d10ba27041e101da06
ybouret <yann.bouret@gmail.com>
2018-08-21 14:42:27 +0200

    updated zfind to std

---
+++ src/math/y/math/fcn/zfind.cpp
@@ -0,0 +1,14 @@
+
+#include "y/math/fcn/zfind.hpp"
+
+
+namespace upsylon
+{
+    namespace math
+    {
+        const unsigned zfind::__z;
+        const unsigned zfind::__n;
+        const unsigned zfind::__p;
+    }
+}
+
