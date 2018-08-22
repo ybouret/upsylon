bf85f73f5164e7d29f009980d369d9cf11c0d83c
ybouret <yann.bouret@gmail.com>
2018-08-22 11:38:19 +0200

    image WIP

--- src/ink/y/ink/image.hpp
+++ src/ink/y/ink/image.hpp
@@ -4,11 +4,48 @@
 #include "y/ink/color/rgba2data.hpp"
 #include "y/ink/color/data2rgba.hpp"
 #include "y/ink/pixmaps.hpp"
+#include "y/string.hpp"
+#include "y/associative/set.hpp"
+#include "y/ptr/intr.hpp"
 
 namespace upsylon
 {
     namespace ink
     {
+
+        //! Interface for Image Input/Output
+        class imageIO : public counted_object
+        {
+        public:
+            typedef intr_ptr<string,imageIO> pointer;
+            typedef set<string,pointer>      database;
+
+
+            const string name;
+            inline virtual ~imageIO() throw() {}
+            inline const string & key() const throw() { return name; }
+
+            //==============================================================
+            //
+            // virtual interface
+            //
+            //==============================================================
+            virtual bitmap  *load(const string          &filename,
+                                  unit_t                 depth,
+                                  rgba2data             &proc,
+                                  const void            *options) const = 0;
+
+            virtual void     save(const string        &filename,
+                                  const bitmap        &bmp,
+                                  data2rgba           &proc,
+                                  const void          *options) const = 0;
+
+        protected:
+            inline explicit imageIO(const char *id) : name(id) {}
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(imageIO);
+        };
     }
 }
 
