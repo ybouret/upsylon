fd66054de1d57d55026c68dd4ffaee20db0eb953
ybouret <yann.bouret@gmail.com>
2018-08-23 14:19:56 +0200

    WIP quad if necessary

--- src/math/tests/test-intg.cpp
+++ src/math/tests/test-intg.cpp
@@ -9,7 +9,14 @@ namespace {
     template <typename T>
     static inline T f(const T x )
     {
-        return square_of(__cos(x))/(2*numeric<T>::pi);
+        return square_of(__cos(x*x))/(2*numeric<T>::pi);
+    }
+
+
+    template <typename T>
+    static inline T g(const T x )
+    {
+        return __sin(x*x);
     }
 
     template <typename T>
@@ -17,6 +24,16 @@ namespace {
     {
         T sum = 0;
         if(integrate::quad(sum,f<T>,T(0),T(2*numeric<T>::pi),T(1e-12)))
+        {
+            std::cerr << "success" << std::endl;
+        }
+        else
+        {
+            std::cerr << "failure" << std::endl;
+        }
+        std::cerr << "sum=" << sum << std::endl;
+
+        if(integrate::quad(sum,g<T>,T(0),T(10),T(1e-12)))
         {
             std::cerr << "success" << std::endl;
         }
