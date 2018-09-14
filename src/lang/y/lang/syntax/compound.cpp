c5e4586c78cbdc7e53eb4a3c005799680688db3b
ybouret <yann.bouret@gmail.com>
2018-09-14 08:54:40 +0200

    grammar WIP

--- src/lang/y/lang/syntax/compound.cpp
+++ src/lang/y/lang/syntax/compound.cpp
@@ -8,6 +8,19 @@ namespace upsylon
 
         namespace Syntax
         {
+            Operand:: ~Operand() throw()
+            {
+            }
+
+            Operand:: Operand(const Rule &r)  throw(): rule(r), next(0), prev(0) {}
+
+            Operand::List::  List() throw() : ListType() {}
+            Operand::List:: ~List() throw()  {}
+
+            void Operand::List:: add(const Rule &r)
+            {
+                push_back( new Operand(r) );
+            }
 
         }
 
@@ -15,4 +28,115 @@ namespace upsylon
 
 }
 
+
+namespace upsylon
+{
+    namespace Lang
+    {
+
+        namespace Syntax
+        {
+            Compound:: ~Compound() throw()
+            {
+            }
+
+            Compound:: Compound( const uint32_t t, const string &id ) :
+            Rule(t,id),
+            Operand::List()
+            {
+            }
+
+
+
+        }
+
+    }
+
+}
+
+namespace upsylon
+{
+    namespace Lang
+    {
+
+        namespace Syntax
+        {
+            Aggregate:: ~Aggregate() throw()
+            {
+            }
+
+            Aggregate:: Aggregate(  const string &id ) :
+            Compound(UUID,id)
+            {
+            }
+
+
+            bool Aggregate:: accept( Y_LANG_SYNTAX_RULE_ARGS ) const
+            {
+                Node * subTree = Node::Create(*this);
+
+                // try to accept every operand
+                {
+                    auto_ptr<Node> guard(subTree);
+                    for(const Operand *op=head;op;op=op->next)
+                    {
+                        if(!op->rule.accept(subTree, lexer, source) )
+                        {
+                            guard.dismiss();
+                            Node::BackTo(lexer, subTree);
+                            return false;
+                        }
+                    }
+                    guard.dismiss();
+                }
+
+                // success, subTree may be empty but never NULL!
+                Grow(tree,subTree);
+                return true;
+            }
+        }
+
+    }
+
+}
+
+
+namespace upsylon
+{
+    namespace Lang
+    {
+
+        namespace Syntax
+        {
+            Alternate:: ~Alternate() throw()
+            {
+            }
+
+            Alternate:: Alternate(  const string &id ) :
+            Compound(UUID,id)
+            {
+            }
+
+
+            bool Alternate:: accept( Y_LANG_SYNTAX_RULE_ARGS ) const
+            {
+
+                for(const Operand *op=head;op;op=op->next)
+                {
+                    Node *tmp = NULL;
+                    if(op->rule.accept(tmp, lexer, source) )
+                    {
+                        // success
+                        if(tmp) Grow(tree,tmp);
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+    }
+
+}
 
--- src/lang/y/lang/syntax/compound.hpp
+++ src/lang/y/lang/syntax/compound.hpp
@@ -11,36 +11,86 @@ namespace upsylon
 
         namespace Syntax
         {
-            class Compound : public Rule
+            //! node holding a rule
+            class Operand : public Object
             {
             public:
-                virtual ~Compound() throw();
+                typedef core::list_of_cpp<Operand> ListType; //!< base class for list
+
+                //! list of operands
+                class List : public ListType
+                {
+                public:
+                    explicit List() throw();      //!< initialize
+                    virtual ~List() throw();      //!< destructor
+                    void     add( const Rule &r); //!< push back an operand
+                private:
+                    Y_DISABLE_COPY_AND_ASSIGN(List);
+                };
+
+                const Rule &rule; //!< existing rule reference
+                Operand    *next; //!< for list
+                Operand    *prev; //!< for list
+
+                virtual ~Operand() throw();              //!< destructor
+                explicit Operand(const Rule &r) throw(); //!< initialize
+
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(Operand);
+            };
+
+            //! base class for compound rule
+            class Compound : public Rule, public Operand::List
+            {
+            public:
+                virtual ~Compound() throw(); //!< desctructor
+                //! syntax helper
+                inline Compound & operator<<(const Rule &r) { add(r); return *this; }
 
             protected:
+                //! initialize
                 explicit Compound(const uint32_t t, const string &id);
 
             private:
                 Y_DISABLE_COPY_AND_ASSIGN(Compound);
             };
 
-
-            class Aggregate : public Rule
+            //! aggregate different rules
+            class Aggregate : public Compound
             {
             public:
+                //! class identifier
                 static const uint32_t UUID = Y_FOURCC('A','G','G',' ');;
-                explicit Aggregate(const string &id);
-                virtual ~Aggregate() throw();
+                explicit Aggregate(const string &id); //!< initialize
+                virtual ~Aggregate() throw();         //!< destructor
+
+                //! syntax helper
+                inline Aggregate & operator += ( const Rule &r ) { add(r); return *this; }
+
+                //! accept all or no nodes
+                /**
+                 if true, an empty sub-tree is added => not weak
+                 */
+                virtual bool accept( Y_LANG_SYNTAX_RULE_ARGS ) const;
 
             private:
                 Y_DISABLE_COPY_AND_ASSIGN(Aggregate);
             };
 
-            class Alternate : public Rule
+            //! choice of different rules
+            class Alternate : public Compound
             {
             public:
+                //! class identifier
                 static const uint32_t UUID = Y_FOURCC('A','L','T',' ');;
-                explicit Alternate(const string &id);
-                virtual ~Alternate() throw();
+                explicit Alternate(const string &id); //!< initialize
+                virtual ~Alternate() throw();         //!< destructor
+
+                //! syntax helper
+                inline Alternate & operator |= ( const Rule &r ) { add(r); return *this; }
+
+                //! true if one of the operand accepts
+                virtual bool accept( Y_LANG_SYNTAX_RULE_ARGS ) const;
 
             private:
                 Y_DISABLE_COPY_AND_ASSIGN(Alternate);
