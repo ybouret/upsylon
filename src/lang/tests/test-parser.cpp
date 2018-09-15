74804464d74dad7bd47d023ff7e5e4ee54a54037
ybouret <yann.bouret@gmail.com>
2018-09-15 17:14:34 +0200

    WIP parser

---
+++ src/lang/tests/test-parser.cpp
@@ -0,0 +1,30 @@
+#include "y/lang/syntax/parser.hpp"
+#include "y/utest/run.hpp"
+
+using namespace upsylon;
+using namespace Lang;
+
+namespace
+{
+    class myParser : public Syntax::Parser
+    {
+    public:
+        explicit myParser() : Syntax::Parser("JSON")
+        {
+
+        }
+
+
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(myParser);
+    };
+}
+
+Y_UTEST(parser)
+{
+    myParser json;
+    
+}
+Y_UTEST_DONE()
+
+
