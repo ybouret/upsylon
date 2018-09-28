69ff18001f37808ae0a5f59706ffa2ebf914203c
ybouret <yann.bouret@gmail.com>
2018-09-28 13:38:36 +0200

    WIP walk down

--- src/lang/y/lang/dynamo/gen-walk-down.cpp
+++ src/lang/y/lang/dynamo/gen-walk-down.cpp
@@ -1,10 +1,12 @@
 #include "y/lang/dynamo/generator.hpp"
+#include "y/exception.hpp"
 
 namespace upsylon
 {
 
     namespace Lang
     {
+        
         void DynamoGenerator:: walkDown( const Node *node )
         {
             assert(node);
@@ -18,8 +20,27 @@ namespace upsylon
                 ++level;
                 if("RULE"==node->rule.name)
                 {
+                    //__________________________________________________________
+                    //
+                    // found a top rule
+                    //__________________________________________________________
                     const string name = getNodeName(*node,"ID", 0);
                     if(verbose) { indent() << "-> setup " << name << std::endl;}
+                    Syntax::Rule *top = (Syntax::Rule *)(parser->getRuleByName(name));
+                    if(!top)
+                    {
+                        throw exception("{%s} unregistered rule '%s'", **(parser->name), *name );
+                    }
+
+                    //__________________________________________________________
+                    //
+                    // get the compound rule
+                    //__________________________________________________________
+                    assert( Syntax::Aggregate::UUID == top->uuid || Syntax::Alternate::UUID == top->uuid );
+                    assert( NULL != top->data );
+                    Syntax::Compound &members = *(Syntax::Compound *)(top->data);
+                    
+
                 }
                 else
                 {
