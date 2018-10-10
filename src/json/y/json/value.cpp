d7b2cd4e1dec778bdb546636bfbaed23f71bfdde
ybouret <yann.bouret@gmail.com>
2018-10-10 22:41:23 +0200

    WIP JSON API

--- src/json/y/json/value.cpp
+++ src/json/y/json/value.cpp
@@ -1,13 +1,33 @@
 #include "y/json/value.hpp"
-
+#include "y/exception.hpp"
 
 namespace upsylon
 {
     namespace JSON
     {
-        const NullType_t  NullType  = {};
-        const TrueType_t  TrueType  = {};
-        const FalseType_t FalseType = {};
+        
+#define Y_JSON_VALUE(TYPE) case Is##TYPE : return #TYPE
+        
+        const char *ValueTypeText(const ValueType t) throw()
+        {
+            switch(t)
+            {
+                    Y_JSON_VALUE(Null);
+                    Y_JSON_VALUE(False);
+                    Y_JSON_VALUE(True);
+                    Y_JSON_VALUE(Number);
+                    Y_JSON_VALUE(String);
+                    Y_JSON_VALUE(Array);
+                    Y_JSON_VALUE(Object);
+            }
+            return "???";
+        }
+        
+        const NullType_t   NullType   = {};
+        const TrueType_t   TrueType   = {};
+        const FalseType_t  FalseType  = {};
+        const ArrayType_t  ArrayType  = {};
+        const ObjectType_t ObjectType = {};
 
         Value:: Value() throw() :
         type( IsNull ),
@@ -33,7 +53,21 @@ namespace upsylon
         impl( NULL    )
         {
         }
+        
+        Value:: Value( const ArrayType_t & ) :
+        type( IsArray ),
+        impl( new Array() )
+        {
+        }
+        
+        Value:: Value( const ObjectType_t &) :
+        type( IsObject ),
+        impl( new Object() )
+        {
+        }
+        
 
+        
         Value:: Value( const Number x ) :
         type( IsNumber ),
         impl( object::acquire1<Number>() )
@@ -66,7 +100,9 @@ namespace upsylon
                 case IsTrue:   break;
                 case IsFalse:  break;
                 case IsNumber: *(Number *)(impl=object::acquire1<Number>()) = *(const Number *)other.impl; break;
-                case IsString: impl = new String( *(const String *) other.impl );  break;
+                case IsString: impl = new String( *(const String  *)other.impl );  break;
+                case IsArray:  impl = new Array(  *(const Array   *)other.impl );  break;
+                case IsObject: impl = new Object( *(const Object  *)other.impl );  break;
             }
         }
 
@@ -79,6 +115,8 @@ namespace upsylon
                 case IsFalse: break;
                 case IsNumber: assert(impl); object::release1<Number>( *(Number **)&impl ); break;
                 case IsString: assert(impl); delete static_cast<String *>(impl); break;
+                case IsArray:  assert(impl); delete static_cast<Array  *>(impl); break;
+                case IsObject: assert(impl); delete static_cast<Object *>(impl); break;
             }
         }
 
@@ -95,5 +133,97 @@ namespace upsylon
             return *this;
         }
 
+#define Y_JSON_VALUE_AS(TYPE) \
+template <> TYPE       & Value::as<TYPE>() throw()       { assert(Is##TYPE==type); return *static_cast<TYPE*>(impl); } \
+template <> const TYPE & Value::as<TYPE>() const throw() { assert(Is##TYPE==type); return *static_cast<const TYPE*>(impl); }
+
+        Y_JSON_VALUE_AS(Number)
+        Y_JSON_VALUE_AS(Array)
+        Y_JSON_VALUE_AS(Object)
+        Y_JSON_VALUE_AS(String)
+        
+        Array::  Array() throw() : _Array() {}
+        Array:: ~Array() throw() {}
+        
+        Array:: Array(const Array &other) : _Array(other) {}
+        
+        Array  & Array:: push( const ArrayType_t  &_) { const Value tmp(_); push_back(tmp); return back().as<Array>();  }
+        Object & Array:: push( const ObjectType_t &_) { const Value tmp(_); push_back(tmp); return back().as<Object>(); }
+        Number & Array:: push( const Number        _) { const Value tmp(_); push_back(tmp); return back().as<Number>(); }
+        String & Array:: push( const String       &_) { const Value tmp(_); push_back(tmp); return back().as<String>(); }
+        String & Array:: push( const char         *_) { const Value tmp(_); push_back(tmp); return back().as<String>(); }
+
+        _Pair:: _Pair( const string &l, const Value &v ) :
+        label(l),
+        value(v)
+        {}
+        
+        _Pair:: ~_Pair() throw()
+        {
+        }
+        
+        const string & _Pair:: key() const throw()
+        {
+            return label;
+        }
+        
+        Object::  Object() throw() : _Object() {}
+       
+        Object:: ~Object() throw() {}
+        
+        Object:: Object( const Object &other ) : _Object(other) {}
+       
+        Value &Object:: add(const string &label, const Value &v)
+        {
+            Pair p = new _Pair(label,v);
+            if(!insert(p))
+            {
+                throw exception("JSON::Object mutliple label '%s' for JSON::%s",*label,v.typeName() );
+            }
+            return p->value;
+        }
+
+        Array  & Object:: add(const string &label, const ArrayType_t  &_)
+        {
+            Value tmp(_);
+            Value &v = add(label,NullType);
+            v.swap_with(tmp);
+            return v.as<Array>();
+        }
+
+        Object & Object:: add(const string &label, const ObjectType_t  &_)
+        {
+            Value tmp(_);
+            Value &v = add(label,NullType);
+            v.swap_with(tmp);
+            return v.as<Object>();
+        }
+
+        Number & Object::add(const string &label, const Number _)
+        {
+            Value tmp(_);
+            Value &v = add(label,NullType);
+            v.swap_with(tmp);
+            return v.as<Number>();
+        }
+
+        String & Object::add(const string &label, const String &_)
+        {
+            Value tmp(_);
+            Value &v = add(label,NullType);
+            v.swap_with(tmp);
+            return v.as<String>();
+        }
+
+        
+        String & Object::add(const string &label, const char * _)
+        {
+            Value tmp(_);
+            Value &v = add(label,NullType);
+            v.swap_with(tmp);
+            return v.as<String>();
+        }
+
+        
     }
 }
--- src/json/y/json/value.hpp
+++ src/json/y/json/value.hpp
@@ -2,29 +2,46 @@
 #define Y_JSON_VALUE_INCLUDED 1
 
 #include "y/ios/ostream.hpp"
+#include "y/sequence/vector.hpp"
+#include "y/associative/set.hpp"
+#include "y/ptr/intr.hpp"
 
 namespace upsylon
 {
 
     namespace JSON
     {
-        typedef double Number;
-        typedef string String;
+        typedef double Number; //!< alias
+        typedef string String; //!< alias
 
+        //! enumeration of different value types
         enum ValueType
         {
             IsNull,
             IsTrue,
             IsFalse,
             IsNumber,
-            IsString
+            IsString,
+            IsArray,
+            IsObject
         };
 
-        typedef struct int2type<IsNull>  NullType_t;  extern const NullType_t  NullType;
-        typedef struct int2type<IsTrue>  TrueType_t;  extern const TrueType_t  TrueType;
-        typedef struct int2type<IsFalse> FalseType_t; extern const FalseType_t FalseType;
-
-
+        const char *ValueTypeText(const ValueType t) throw();
+        
+        typedef struct int2type<IsNull>   NullType_t;
+        extern  const  NullType_t         NullType;
+        typedef struct int2type<IsTrue>   TrueType_t;
+        extern  const  TrueType_t         TrueType;
+        typedef struct int2type<IsFalse>  FalseType_t;
+        extern  const  FalseType_t        FalseType;
+        typedef struct int2type<IsArray>  ArrayType_t;
+        extern  const  ArrayType_t        ArrayType;
+        typedef struct int2type<IsObject> ObjectType_t;
+        extern  const  ObjectType_t       ObjectType;
+        
+        class Array;
+        class Object;
+        
         class Value
         {
         public:
@@ -32,21 +49,111 @@ namespace upsylon
             virtual ~Value() throw();
             Value(const Value &other);
             Value & operator=( const Value &other );
-            size_t size() const throw();
+            size_t length() const throw();
 
             Value( const NullType_t  & ) throw();
             Value( const TrueType_t  & ) throw();
             Value( const FalseType_t & ) throw();
-            Value( const double        );
+            Value( const ArrayType_t & );
+            Value( const ObjectType_t &);
+            Value( const Number        );
             Value( const char   *text  );
             Value( const string &str   );
             
             const ValueType type;
             void  swap_with( Value &other ) throw();
+            
+            template <typename T> T       & as() throw();
+            template <typename T> const T & as() const throw();
 
+            inline const char *typeName() const throw() { return ValueTypeText(type); }
+            
         private:
             void *impl;
         };
+        
+        typedef vector<Value> _Array; //!< base class for Array
+        
+        //! array of values
+        class Array : public _Array
+        {
+        public:
+            explicit Array() throw();
+            virtual ~Array() throw();
+            Array(const Array &);
+            
+            inline void   push( const Value &v ) { push_back(v);  }
+            inline size_t length() const throw() { return size(); }
+            inline void   pop()    throw()       { pop_back();    }
+            
+            inline void   push( const NullType_t  &_) { const Value tmp(_); push_back(tmp); }
+            inline void   push( const TrueType_t  &_) { const Value tmp(_); push_back(tmp); }
+            inline void   push( const FalseType_t &_) { const Value tmp(_); push_back(tmp); }
+            Array  &push( const ArrayType_t  &);
+            Object &push( const ObjectType_t &);
+            Number &push( const Number x);
+            String &push( const String &);
+            String &push( const char   *);
+            
+        private:
+            Y_DISABLE_ASSIGN(Array);
+        };
+        
+        
+        class _Pair : public counted_object
+        {
+        public:
+            typedef intr_ptr<string,_Pair> Pointer;
+            
+            const string label;
+            Value        value;
+            
+            virtual ~_Pair() throw();
+            explicit _Pair(const string &l,const Value &);
+            const string & key() const throw();
+            
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(_Pair);
+        };
+        
+        typedef _Pair::Pointer    Pair;
+        typedef set<string,Pair> _Object;
+        
+        class Object : public _Object
+        {
+        public:
+            explicit Object() throw();
+            virtual ~Object() throw();
+            Object(const Object &);
+            inline size_t length() const throw() { return size(); }
+            
+            Value        &add(const string &label, const Value &v);
+            inline Value &add(const char   *label, const Value &v) { const string _(label); return add(_,v); }
+           
+            inline Value & add(const string &label, const NullType_t  &_) { const Value tmp(_); return add(label,tmp); }
+            inline Value & add(const char   *label, const NullType_t  &_) { const Value tmp(_); return add(label,tmp); }
+            
+            inline Value & add(const string &label, const TrueType_t  &_) { const Value tmp(_); return add(label,tmp); }
+            inline Value & add(const char   *label, const TrueType_t  &_) { const Value tmp(_); return add(label,tmp); }
+
+            inline Value & add(const string &label, const FalseType_t  &_) { const Value tmp(_); return add(label,tmp); }
+            inline Value & add(const char   *label, const FalseType_t  &_) { const Value tmp(_); return add(label,tmp); }
+            
+            Array        &add(const string &label, const ArrayType_t   &);
+            inline Array &add(const char   *label, const ArrayType_t &_) { const string __(label); return add(__,_); }
+            
+            Object        &add(const string &label, const ObjectType_t  &);
+            inline Object &add(const char   *label, const ObjectType_t  &_) { const string __(label); return add(__,_); }
+            
+            Number &add(const string &label, const Number);
+            String &add(const string &label, const String&);
+            String &add(const string &label, const char  *);
+
+        private:
+            Y_DISABLE_ASSIGN(Object);
+        };
+        
+        
     }
 
 }
