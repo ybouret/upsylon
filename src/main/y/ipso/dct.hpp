27cd7fffd176afdb0ea8fafed3bab5b7eca736a2
ybouret <yann.bouret@gmail.com>
2018-12-14 13:58:50 +0100

    need to work on DCT

--- src/main/y/ipso/dct.hpp
+++ src/main/y/ipso/dct.hpp
@@ -8,7 +8,7 @@ namespace upsylon
 {
     namespace ipso
     {
-        class DCT 
+        class DCT
         {
         public:
             typedef field2D<double> Table;
@@ -29,6 +29,40 @@ namespace upsylon
             virtual ~CommonDCT() throw();
             explicit CommonDCT(const unit_t W,const unit_t H);
 
+#if 0
+            template <typename TABLE>
+            void forward(Table           &self,
+                         const TABLE     &src,
+                         const unit_t     xx,
+                         const unit_t     yy) throw()
+            {
+                //Pixmap<double> &self  = *this;
+                const unit_t    N     = width.x;
+                const unit_t    M     = width.h;
+
+                for(unit_t j=0;j<M;++j)
+                {
+                    for(unit_t i=0;i<N;++i)
+                    {
+                        double       q   = 0;
+                        const double wij = LAMBDA[j][i];
+                        for(unit_t y=0;y<M;++y)
+                        {
+                            const double Cyj    = YCOS[y][j];
+                            for(unit_t x=0;x<N;++x)
+                            {
+                                const double Cxi = XCOS[x][i];
+                                const double wxy = Cxi*Cyj;
+                                q += double( src[yy+y][xx+x] ) * wxy;
+                            }
+                        }
+
+                        self[j][i] = q * wij;
+                    }
+                }
+            }
+#endif
+            
         private:
             Y_DISABLE_COPY_AND_ASSIGN(CommonDCT);
             Table XCOS;
