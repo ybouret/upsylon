1fc6a42af684d24c8bce20214a4b6b442143f4da
ybouret <yann.bouret@gmail.com>
2019-01-29 15:45:46 +0100

    updated polynomial fit

--- src/math/y/math/fit/polynomial.hpp
+++ src/math/y/math/fit/polynomial.hpp
@@ -3,7 +3,7 @@
 #ifndef Y_MATH_FIT_POLYNOMIAL_INCLUDED
 #define Y_MATH_FIT_POLYNOMIAL_INCLUDED 1
 
-#include "y/math/fit/samples.hpp"
+#include "y/math/fit/ls.hpp"
 #include "y/math/polynomial.hpp"
 #include "y/type/utils.hpp"
 
@@ -30,11 +30,11 @@ namespace upsylon
 
                 //! initialize with moments using simple sample
                 template <typename T> static inline
-                void Initialize(array<double>      &aorg,
-                                Sample<T>           &sample)
+                void Initialize(array<T>   &aorg,
+                                Sample<T> &sample)
                 {
-                    const size_t n = aorg.size();
-                    const size_t N = sample.X.size();
+                    const size_t   n = aorg.size();
+                    const size_t   N = sample.X.size();
                     matrix<double> mu(n,n);
                     for(size_t i=1,im=0;i<=n;++i,++im)
                     {
@@ -58,12 +58,37 @@ namespace upsylon
                     }
                     if(! LU::build(mu) )
                     {
-                        throw exception("Polynomial::Initialize(singular distribution)");
+                        throw exception("Fit::Polynomial::Initialize(singular distribution)");
                     }
                     LU::solve(mu,aorg);
+                    for(size_t i=N;i>0;--i)
+                    {
+                        sample.Yf[i] = polynomial::eval(sample.X[i],aorg);
+                    }
                 }
+
+                //! fit using polynomial evaluation
+                template <typename T> static inline
+                void FitWith(LeastSquares<T> &LS,
+                             array<T>        &aorg,
+                             array<T>        &aerr,
+                             array<bool>     &used,
+                             Sample<T>       &sample)
+                {
+                    Create(sample.variables,aorg.size());
+                    Initialize(aorg,sample);
+                    typename Fit::Type<T>::CFunction f = Fit::Polynomial::Compute<double>;
+                    typename Fit::Type<T>::Function  F(f);
+                    if(!LS.fit(sample, F, aorg, aerr, used))
+                    {
+                        throw exception("Fit::Polynomial::Unexpected Algebraic Failure");
+                    }
+                }
+
             };
-            
+
+
+
         }
     }
 }
