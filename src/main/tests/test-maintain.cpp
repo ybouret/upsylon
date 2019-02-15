da67d63180c313d1ccd36c8dc819cd8566a28a0b
ybouret <yann.bouret@gmail.com>
2019-02-15 16:03:50 +0100

    updated tests

--- src/main/tests/test-maintain.cpp
+++ src/main/tests/test-maintain.cpp
@@ -17,23 +17,46 @@ static inline
 void do_maintain( SEQUENCE &strings )
 {
     strings.free();
-    const size_t n = 1 + alea.leq(100);
-    std::cerr << "Generating " << n << " strings" << std::endl;
-    sz=0;
-    for(size_t i=n;i>0;--i)
     {
-        const string s = support::get<string>();
-        strings.push_back(s);
-        sz += strings.back().size();
-    }
-    sz/=n;
-    std::cerr << "strings=" << strings << std::endl;
+        const size_t n = 1 + alea.leq(100);
+        std::cerr << "Generating " << n << " strings" << std::endl;
+        sz=0;
+        for(size_t i=n;i>0;--i)
+        {
+            const string s = support::get<string>();
+            strings.push_back(s);
+            sz += strings.back().size();
+        }
+        sz/=n;
+        std::cerr << "strings  =" << strings << std::endl;
 
-    list<size_t> indices;
-    maintain::build_indices(indices, strings.begin(), strings.size(), keep_short_string, false);
-    std::cerr << "indices=" << indices << std::endl;
-    maintain::strip(strings,indices);
-    std::cerr << "stripped=" << strings << std::endl;
+        list<size_t> indices;
+        maintain::build_indices(indices, strings.begin(), strings.size(), keep_short_string, false);
+        std::cerr << "indices  =" << indices << std::endl;
+        maintain::strip(strings,indices);
+        std::cerr << "stripped =" << strings << std::endl;
+    }
+
+    {
+        const size_t n = 1 + alea.leq(100);
+        std::cerr << "Generating " << n << " strings" << std::endl;
+        sz=0;
+        for(size_t i=n;i>0;--i)
+        {
+            const string s = support::get<string>();
+            strings.push_back(s);
+            sz += strings.back().size();
+        }
+        sz/=n;
+        std::cerr << "strings  =" << strings << std::endl;
+
+        vector<size_t> indices;
+        maintain::build_indices(indices, strings.begin(), strings.size(), keep_short_string, true);
+        std::cerr << "indices  =" << indices << std::endl;
+        maintain::strip(strings,indices);
+        std::cerr << "stripped =" << strings << std::endl;
+    }
+
 }
 
 Y_UTEST(maintain)
