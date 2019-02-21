0fac0a82f83a0a7b42081b2e82ebf1cc7ac7c2e9
ybouret <yann.bouret@gmail.com>
2019-02-21 09:00:19 +0100

    OK, first test is fine...

--- src/ink/tests/test-sub.cpp
+++ src/ink/tests/test-sub.cpp
@@ -10,13 +10,19 @@ using namespace Ink;
 namespace
 {
     template <typename T>
-    static inline void do_sub( Pixmap<T> &pxm, const char id )
+    static inline void do_sub( Bitmap &bmp, const char id)
     {
+        ImageIO   &img = Image::instance();
         std::cerr << std::endl;
-        std::cerr << "DoSub for " << id << ", depth=" << pxm.depth << ", Area=" << pxm << std::endl;
-        Tiles tiles(pxm,4);
+        std::cerr << "DoSub for " << id << ", depth=" << bmp.depth << ", Area=" << bmp << std::endl;
+        Tiles tiles(bmp,4);
+        Pixmap<T>  pxm( &bmp );
+        img.save( vformat("full%c.png",id),pxm,NULL);
         for(size_t i=1;i<=tiles.size();++i)
         {
+            const Area &A = *tiles[i];
+            Pixmap<T>   P( &bmp, A );
+            img.save( vformat("full%c-%u.png",id,unsigned(i)), P, NULL);
         }
     }
 }
@@ -26,25 +32,22 @@ Y_UTEST(sub)
     Image     &__img = Image::Init();
     ImageIO   &img   = __img;
     Dispatcher par = new concurrent::simd();
-    put_rgb    ioRGB;
+    put_rgb    io3;
+    put_gs1    io1;
+    put_gsf    ioF;
 
     for(int iarg=1;iarg<argc;++iarg)
     {
-        const string fn = argv[iarg];
-        Bitmap      *bmp = img.load(fn,sizeof(RGB),ioRGB,NULL);
-        const arc_ptr<Bitmap> guard = bmp;
-        Pixmap3      img3( bmp );
-        Engine       eng(par,img3);
-        const size_t w = img3.w;
-        const size_t h = img3.h;
-        Pixmap1      img1(w,h);
-        PixmapF      imgF(w,h);
-        Filter::Apply(img1,img3,RGBtoByte,eng);
-        Filter::Apply(imgF,img3,RGBtoFloat,eng);
+        const string    fn   = argv[iarg];
+        arc_ptr<Bitmap> bmp3 = img.load(fn,sizeof(RGB),    io3,NULL);
+        arc_ptr<Bitmap> bmp1 = img.load(fn,sizeof(uint8_t),io1,NULL);
+        arc_ptr<Bitmap> bmpF = img.load(fn,sizeof(float),  ioF,NULL);
 
-        do_sub(img3,'3');
-        do_sub(img1,'1');
-        do_sub(imgF,'F');
+
+        do_sub<RGB>(*bmp3,'3');
+        do_sub<uint8_t>(*bmp1,'1');
+        do_sub<float>(*bmpF,'F');
+
     }
 }
 Y_UTEST_DONE()
