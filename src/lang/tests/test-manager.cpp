c12da0add0dc4824532a7e688a320121a5487ba2
ybouret <yann.bouret@gmail.com>
2019-03-28 11:46:48 +0100

    WIP manager...

--- src/lang/tests/test-manager.cpp
+++ src/lang/tests/test-manager.cpp
@@ -6,6 +6,12 @@ using namespace Lang;
 
 Y_UTEST(manager)
 {
+    std::cerr << "sizeof(DynamoLoader)     = " << sizeof(DynamoLoader)      << std::endl;
+    std::cerr << "sizeof(DynamoCompiler)   = " << sizeof(DynamoCompiler)    << std::endl;
+    std::cerr << "sizeof(DynamoGenerator)  = "  << sizeof(DynamoGenerator)  << std::endl;
+    std::cerr << "sizeof(DynamoManager)    = "  << sizeof(DynamoManager)    << std::endl;
+    
+    
     DynamoManager &mgr = DynamoManager::instance();
     
     if(argc>1)
@@ -14,29 +20,44 @@ Y_UTEST(manager)
         
         // make an XNode
         auto_ptr<XNode> xnode = mgr.loader().load( Module::OpenFile(fileName) );
-        
-        const size_t xnodeOutBytes = xnode->outputBytes();
-        size_t       xnodeCount    = 0;
-        xnode->save("xnode.bin", &xnodeCount);
-        const string xnodeCode = xnode->toBinary();
-       
-        std::cerr << "xnodeOutBytes="<< xnodeOutBytes << std::endl;
-        Y_CHECK(xnodeOutBytes==xnodeCount);
-        Y_CHECK(xnodeOutBytes==ios::local_file::length_of("xnode.bin"));
-        Y_CHECK(xnodeOutBytes==xnodeCode.size());
-       
-        // reload from file
         {
-            auto_ptr<XNode> reload = XNode::Load( Module::OpenFile("xnode.bin"), mgr.loader() );
-            const string reloaded_file = reload->toBinary();
-            Y_CHECK(xnodeCode==reloaded_file);
+            const size_t xnodeOutBytes = xnode->outputBytes();
+            size_t       xnodeCount    = 0;
+            xnode->save("xnode.bin", &xnodeCount);
+            const string xnodeCode = xnode->toBinary();
+            
+            std::cerr << "xnodeOutBytes="<< xnodeOutBytes << std::endl;
+            Y_CHECK(xnodeOutBytes==xnodeCount);
+            Y_CHECK(xnodeOutBytes==ios::local_file::length_of("xnode.bin"));
+            Y_CHECK(xnodeOutBytes==xnodeCode.size());
+            
+            // reload from file
+            {
+                auto_ptr<XNode> reload = XNode::Load( Module::OpenFile("xnode.bin"), mgr.loader() );
+                const string reloaded_file = reload->toBinary();
+                Y_CHECK(xnodeCode==reloaded_file);
+            }
+            
+            // reload from data
+            {
+                auto_ptr<XNode> reload = XNode::Load( Module::OpenData("xnode",xnodeCode), mgr.loader() );
+                const string reloaded_data = reload->toBinary();
+                Y_CHECK(xnodeCode==reloaded_data);
+            }
         }
         
-        // reload from data
+        // make a DynamoNode
+        auto_ptr<DynamoNode> dnode = mgr.compiler().compile(*xnode);
         {
-            auto_ptr<XNode> reload = XNode::Load( Module::OpenData("xnode",xnodeCode), mgr.loader() );
-            const string reloaded_data = reload->toBinary();
-            Y_CHECK(xnodeCode==reloaded_data);
+            const size_t dnodeOutBytes = dnode->outputBytes();
+            size_t       dnodeCount    = 0;
+            dnode->save("dnode.bin", &dnodeCount);
+            const string dnodeCode = dnode->toBinary();
+            
+            std::cerr << "dnodeOutBytes="<< dnodeOutBytes << std::endl;
+            Y_CHECK(dnodeOutBytes==dnodeCount);
+            Y_CHECK(dnodeOutBytes==ios::local_file::length_of("dnode.bin"));
+            Y_CHECK(dnodeOutBytes==dnodeCode.size());
         }
     }
 }
