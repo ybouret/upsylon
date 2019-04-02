f13515f1ca8440290529375f7325e5d11b0906de
ybouret <yann.bouret@gmail.com>
2019-04-02 16:43:21 +0200

    modified rc

--- src/main/y_rc.cpp
+++ src/main/y_rc.cpp
@@ -7,7 +7,6 @@
 #include "y/program.hpp"
 #include "y/ios/rc/io.hpp"
 #include "y/fs/local.hpp"
-#include "y/lang/pattern/matching.hpp"
 
 using namespace upsylon;
 
@@ -40,15 +39,12 @@ static inline
 void add_rc(ios::rc::writer &rc,
             const string    &parent,
             const string    &path,
-            const vfs       &fs,
-            Lang::Matching  &exclude)
+            const vfs       &fs)
 {
 
-    vfs::entry ep(path,fs);
-    if(exclude.partly(path))
-    {
-        return;
-    }
+    const vfs::entry ep(path,fs);
+    assert(ep.base_name);
+
 
     if(ep.is_regular())
     {
@@ -69,11 +65,16 @@ void add_rc(ios::rc::writer &rc,
         // add recursively the directory
         //
         //======================================================================
+        if( '.' == ep.base_name[0] )
+        {
+            return;
+        }
+        
         const string           child = make_resource_name(parent,ep.path);
         auto_ptr<vfs::scanner> scan  = fs.new_scanner(ep.path);
         for(const vfs::entry  *sub   = scan->next(); sub; sub=scan->next() )
         {
-            add_rc(rc,child,sub->path,fs,exclude);
+            add_rc(rc,child,sub->path,fs);
         }
         return;
     }
@@ -94,16 +95,16 @@ Y_PROGRAM_START()
     }
     else
     {
-        Lang::Matching exclude( "[.]svn" );
-
-        const string filename = argv[1];
+        const string    filename = argv[1];
         ios::rc::writer rc(filename,true);
-        string parent = "";
+        string          parent = "";
+
         for(int iarg=2;iarg<argc;++iarg)
         {
             const string arg = argv[iarg];
-            add_rc(rc,parent,arg,local_fs::instance(),exclude);
+            add_rc(rc,parent,arg,local_fs::instance());
         }
+
         rc.finalize();
         return 0;
     }
