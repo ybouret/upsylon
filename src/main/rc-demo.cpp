72a50d4aa2f25960c76c97d4dfa5063518ad474b
ybouret <yann.bouret@gmail.com>
2019-04-02 07:59:38 +0200

    cosmetic enhancement

--- src/main/rc-demo.cpp
+++ src/main/rc-demo.cpp
@@ -5,7 +5,11 @@ using namespace upsylon;
 
 Y_PROGRAM_START()
 {
+    std::cerr << std::endl;
+    std::cerr << "<" << argv[0] << ">" << std::endl;
     const ios::rc::loader rc( argv[0], true );
+    std::cerr << std::endl;
     std::cerr << "rc=" << rc.db() << std::endl;
+    std::cerr << std::endl;
 }
 Y_PROGRAM_END()
