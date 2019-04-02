653de7f8354a1bf5b837526ae503d20b38be66e7
ybouret <yann.bouret@gmail.com>
2019-04-02 09:38:31 +0200

    clean up code

--- src/main/y_rc.cpp
+++ src/main/y_rc.cpp
@@ -1,9 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// very crude resources assembler
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #include "y/program.hpp"
 #include "y/ios/rc/io.hpp"
 #include "y/fs/local.hpp"
 
 using namespace upsylon;
 
+////////////////////////////////////////////////////////////////////////////////
+//
+// make global path to local path using base name
+//
+////////////////////////////////////////////////////////////////////////////////
 static inline
 string make_resource_name(const string &parent,
                           const string &path )
@@ -19,58 +30,75 @@ string make_resource_name(const string &parent,
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+//
+// recursive call
+//
+////////////////////////////////////////////////////////////////////////////////
 static inline
 void add_rc(ios::rc::writer &rc,
             const string    &parent,
-            const string    &path )
+            const string    &path,
+            const vfs       &fs)
 {
-    vfs &fs = local_fs::instance();
 
     vfs::entry ep(path,fs);
 
     if(ep.is_regular())
     {
+        //======================================================================
+        //
         // add the file
-        const string id = make_resource_name(parent,ep.path);
-        rc.append_file(id,path);
+        //
+        //======================================================================
+        const string identifier = make_resource_name(parent,ep.path);
+        rc.append_file(identifier,path);
+        return;
     }
 
     if( ep.is_subdir() )
     {
+        //======================================================================
+        //
         // add recursively the directory
-        const string sub_path = ep.base_name;
-        const string child    = make_resource_name(parent,sub_path);
-        auto_ptr<vfs::scanner> scan = fs.new_scanner(ep.path);
-        for(const vfs::entry *sub = scan->next(); sub; sub=scan->next() )
+        //
+        //======================================================================
+        const string           child = make_resource_name(parent,ep.path);
+        auto_ptr<vfs::scanner> scan  = fs.new_scanner(ep.path);
+        for(const vfs::entry  *sub   = scan->next(); sub; sub=scan->next() )
         {
-            add_rc(rc,child,sub->path);
+            add_rc(rc,child,sub->path,fs);
         }
+        return;
     }
+
 
 }
 
 Y_PROGRAM_START()
 {
+    //==========================================================================
+    //
     // check if a target is given
+    //
+    //==========================================================================
     if(argc<=1)
     {
         return 0;
     }
-
-    const string filename = argv[1];
+    else
     {
-
+        const string filename = argv[1];
         ios::rc::writer rc(filename,true);
         string parent = "";
         for(int iarg=2;iarg<argc;++iarg)
         {
             const string arg = argv[iarg];
-            add_rc(rc,parent,arg);
+            add_rc(rc,parent,arg,local_fs::instance());
         }
         rc.finalize();
+        return 0;
     }
-
-
 
 }
 Y_PROGRAM_END()
