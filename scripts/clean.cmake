0470728955cff75cfe0ab374d2a3321bce12e4b8
ybouret <yann.bouret@gmail.com>
2019-04-22 10:53:19 +0200

    cleaning...

--- scripts/clean.cmake
+++ scripts/clean.cmake
@@ -28,6 +28,10 @@ FILE(GLOB_RECURSE tmp .DS_Store)
 SET(to_remove ${to_remove} ${tmp})
 ENDIF()
 
+MESSAGE( STATUS "collection old backup files" )
+FILE(GLOB_RECURSE tmp "*~")
+SET(to_remove ${to_remove} ${tmp})
+
 #specific stuff
 LIST( APPEND to_remove "src/R++/rcode.so"  )
 LIST( APPEND to_remove "src/R++/rcode.dll" )
