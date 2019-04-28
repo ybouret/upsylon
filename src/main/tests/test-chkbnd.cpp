b19cf87685f1f16eb5e75580cca59ed2f44e4b4a
ybouret <yann.bouret@gmail.com>
2019-04-28 19:01:30 +0200

    WIP chk bnd

--- src/main/tests/test-chkbnd.cpp
+++ src/main/tests/test-chkbnd.cpp
@@ -1,4 +1,5 @@
 #include "y/type/ints-chkbnd.hpp"
+#include "y/os/endian.hpp"
 #include "y/utest/run.hpp"
 
 using namespace upsylon;
@@ -21,9 +22,30 @@ static inline void check_positive(const char *id)
 #define SHOW(OUTPUT,INPUT,VALUE) do { const OUTPUT res = check_bound<OUTPUT,INPUT>(VALUE);\
 std::cerr << "check_bound<" << #OUTPUT << "," << #INPUT << ">(" << VALUE << ")=" << res << std::endl; } while(false)
 
+template <typename OUTPUT,typename UNSIGNED_INPUT>
+void check_full()
+{
+    uint64_t j = 0;
+    for(UNSIGNED_INPUT i=0; j<limit_of<UNSIGNED_INPUT>::maximum; ++j, ++i)
+    {
+        core::chkbnd<OUTPUT,UNSIGNED_INPUT>(i);
+    }
+}
+
 
 Y_UTEST(chkbnd)
 {
+    check_full<uint8_t,uint8_t>();
+    check_full<uint16_t,uint8_t>();
+    check_full<uint32_t,uint8_t>();
+    check_full<uint64_t,uint8_t>();
+
+    check_full<int8_t,uint8_t>();
+    check_full<int16_t,uint8_t>();
+    check_full<int32_t,uint8_t>();
+    check_full<int64_t,uint8_t>();
+
+
     //CHECK_POS(uint8_t);
     CHECK_POS(uint16_t);
     CHECK_POS(uint32_t);
@@ -49,7 +71,16 @@ Y_UTEST(chkbnd)
     SHOW(unsigned short,int,100000);
     SHOW(short,unsigned short,38000);
 
-    
+    const uint32_t X = 0x12345678;
+    union
+    {
+        uint32_t dw;
+        uint8_t  b[4];
+    } item = { swap_be(X) };
+    std::cerr << std::hex << "b=0x" << uint32_t(item.b[0]) << uint32_t(item.b[1]) << int32_t(item.b[2]) << uint32_t(item.b[3]) << std::endl;
+
+
+
 
 }
 Y_UTEST_DONE()
--- src/main/y/type/ints-chkbnd.hpp
+++ src/main/y/type/ints-chkbnd.hpp
@@ -10,6 +10,45 @@
 namespace upsylon
 {
 
+    namespace core
+    {
+        //! for unsigned U=uint[8|16|32|64]
+        template <typename OUTPUT,typename U> OUTPUT chkbnd(const U &u) throw();
+
+        //______________________________________________________________________
+        //
+        // INPUT=uint8_t
+        //______________________________________________________________________
+        template <> uint8_t  chkbnd<uint8_t, uint8_t>(const uint8_t &u) throw() { return u; } //!< u8->u8
+        template <> uint16_t chkbnd<uint16_t,uint8_t>(const uint8_t &u) throw() { return u; } //!< u8->u16
+        template <> uint32_t chkbnd<uint32_t,uint8_t>(const uint8_t &u) throw() { return u; } //!< u8->u32
+        template <> uint64_t chkbnd<uint64_t,uint8_t>(const uint8_t &u) throw() { return u; } //!< u8->u64
+
+#define Y_CHKBND(OUTPUT,INPUT,VALUE) return ( (u>=static_cast<INPUT>(VALUE)) ? static_cast<OUTPUT>(VALUE) : static_cast<OUTPUT>(u) )
+
+        template <> int8_t   chkbnd<int8_t,uint8_t>( const uint8_t &u) throw() { Y_CHKBND(int8_t,uint8_t,127); } //!< u8->i8
+        template <> int16_t  chkbnd<int16_t,uint8_t>(const uint8_t &u) throw() { return u; }                     //!< u8->i16
+        template <> int32_t  chkbnd<int32_t,uint8_t>(const uint8_t &u) throw() { return u; }                     //!< u8->i32
+        template <> int64_t  chkbnd<int64_t,uint8_t>(const uint8_t &u) throw() { return u; }                     //!< u8->i64
+
+
+
+        //______________________________________________________________________
+        //
+        // INPUT=uint16_t
+        //______________________________________________________________________
+        template <> uint8_t  chkbnd<uint8_t, uint16_t>(const uint16_t &u) throw() { Y_CHKBND(uint8_t,uint16_t,255); } //!< u16->u8
+        template <> uint16_t chkbnd<uint16_t,uint16_t>(const uint16_t &u) throw() { return u; }  //!< u16->u16
+        template <> uint32_t chkbnd<uint32_t,uint16_t>(const uint16_t &u) throw() { return u; }  //!< u16->u32
+        template <> uint64_t chkbnd<uint64_t,uint16_t>(const uint16_t &u) throw() { return u; }  //!< u16->u64
+
+        template <> int8_t  chkbnd<int8_t, uint16_t>(const uint16_t &u) throw() { Y_CHKBND(int8_t,uint16_t,127);    } //!< u16->u8
+        template <> int16_t chkbnd<int16_t,uint16_t>(const uint16_t &u) throw() { Y_CHKBND(int16_t,uint16_t,32767); } //!< u16->u16
+        template <> int32_t chkbnd<int32_t,uint16_t>(const uint16_t &u) throw() { return u; }                         //!< u16->u32
+        template <> int64_t chkbnd<int64_t,uint16_t>(const uint16_t &u) throw() { return u; }                         //!< u16->u64
+
+
+    }
     
     //! return a positive bounded value of OUPUT based on INPUT type
     template <typename OUTPUT,typename INPUT>
