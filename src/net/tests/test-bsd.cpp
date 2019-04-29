1fc03978284fa3e3b7ce6e001eae9d673ec33e18
ybouret <yann.bouret@gmail.com>
2019-04-29 15:39:09 +0200

    WIP net

--- src/net/tests/test-bsd.cpp
+++ src/net/tests/test-bsd.cpp
@@ -7,11 +7,13 @@ using namespace upsylon;
 static inline void display( net::bsd_socket &s )
 {
     std::cerr << "socket: sndbuf=" << s.sndbuf() << " | rcvbuf=" << s.rcvbuf() << std::endl;
+    s.blocking(true);
+    s.blocking(false);
 }
 
 Y_UTEST(bsd)
 {
-
+    network::verbose = true;
     net::bsd_socket tcp_v4(net::tcp,net::v4); display(tcp_v4);
     net::bsd_socket tcp_v6(net::tcp,net::v6); display(tcp_v6);
     net::bsd_socket udp_v4(net::udp,net::v4); display(udp_v4);
