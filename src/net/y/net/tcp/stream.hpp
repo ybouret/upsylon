f23725b36ff152432b651250cea1c325c008e3c2
ybouret <yann.bouret@gmail.com>
2019-04-29 08:16:50 +0200

    rewrite files

--- src/net/y/net/tcp/stream.hpp
+++ src/net/y/net/tcp/stream.hpp
@@ -2,13 +2,9 @@
 #ifndef Y_NET_TCP_STREAM_INCLUDED
 #define Y_NET_TCP_STREAM_INCLUDED 1
 
-#include "y/net/tcp/client.hpp"
+#include "y/net/tcp/cache.hpp"
 #include "y/ios/istream.hpp"
 #include "y/ios/ostream.hpp"
-#include "y/core/node.hpp"
-#include "y/core/list.hpp"
-#include "y/core/pool.hpp"
-#include "y/memory/slab.hpp"
 
 namespace upsylon
 {
@@ -28,49 +24,7 @@ namespace upsylon
             Y_DISABLE_COPY_AND_ASSIGN(tcp_stream);
         };
 
-        //! reusable cache for tcp_istream
-        class tcp_cache_ : public net_object
-        {
-        public:
-            //! a dynamic code
-            struct byte_node
-            {
-                byte_node *prev; //!< for list
-                byte_node *next; //!< for list
-                uint8_t    code; //!< current byte
-            };
-            typedef memory::slab<byte_node>  byte_slab; //!< alias
-            typedef core::list_of<byte_node> byte_list; //!< alias
-
-            explicit tcp_cache_(const size_t n);    //!< setup memory
-            virtual ~tcp_cache_() throw();          //!< reset and release
-            void     reset() throw();               //!< empty cache into pool
-            bool     load( const tcp_client &cln ); //!< load an empty cache with something
-            size_t   size() const throw();          //!< content.size
-
-            char getch() throw();   //!< return first char in cache
-            void putch(char C);     //!< try to unread
-
-        private:                    //
-            byte_list    content;   //!< list of read bytes
-        public:                     //
-            const size_t capacity;  //!< total capacity
-        private:                    //
-            const size_t s_offset;  //
-            const size_t s_length;  //
-        public:                     //
-            const size_t allocated; //!< allocated global memory
-        private:                    //
-            uint8_t     *buffer;    //
-            byte_slab    pool;      //
-
-            Y_DISABLE_COPY_AND_ASSIGN(tcp_cache_);
-        };
-
-        //! internal creation
-        tcp_cache_                 *tcp_cache_new( const size_t n );
-        typedef arc_ptr<tcp_cache_> tcp_cache; //!< alias
-
+      
 
         //! istream decorator for tcp_client TODO: create dedicated cache
         class tcp_istream : public ios::istream, public tcp_stream
