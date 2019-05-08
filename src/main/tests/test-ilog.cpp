4869c4fc5fec1bdb0b5ca5c3af9d84de0c1a633b
ybouret <yann.bouret@gmail.com>
2019-05-08 09:54:34 +0200

    found a ceil function for log of powers of two

--- src/main/tests/test-ilog.cpp
+++ src/main/tests/test-ilog.cpp
@@ -12,27 +12,57 @@ using namespace upsylon;
 namespace
 {
 
-    double S_(unsigned k, const double y )
+    struct LnApprox
     {
-        const   unsigned tkp1 = k+k+1;
-        return  2.0/tkp1 * ipower((y-1)/(y+1),tkp1);
-    }
-
-    double L_(unsigned k, const double y)
-    {
-        double ans = 0;
-        while(k-- > 0 )
+        static inline double S_(unsigned k, const double y )
         {
-            ans += S_(k,y);
+            const   unsigned tkp1 = k+k+1;
+            return  2.0/tkp1 * ipower((y-1)/(y+1),tkp1);
         }
-        return ans;
-    }
+
+        static inline double L_(unsigned k, const double y)
+        {
+            double ans = 0;
+            while(k-- > 0 )
+            {
+                ans += S_(k,y);
+            }
+            return ans;
+        }
+
+        static bool Test(const unsigned  nbits,
+                         const unsigned  max_power,
+                         double         &max_diff,
+                         unsigned       &factor)
+        {
+            const double   ln2 = log(2.0);
+            const double   ip2 = ipower(2.0,nbits);
+            const unsigned fac = unsigned( ceil( ipower(2.0,nbits) * ln2 ) );
+            factor = fac;
+            std::cerr << "Testing for " << fac << "/" << ip2 << std::endl;
+            max_diff = 0;
+            for(unsigned i=0;i<=max_power;++i)
+            {
+                const double   iln2 = i*ln2;
+                const unsigned appx = ((i*fac)>>nbits)+1;
+                if( double(appx) < iln2 )
+                {
+                    std::cerr << "appx=" << appx << " for iln2=" << iln2 << "@" << i << std::endl;
+                    return false;
+                }
+                max_diff = max_of(max_diff,double(appx)-iln2);
+            }
+            return true;
+        }
+    };
 
 
 }
 
 Y_UTEST(ilog)
 {
+
+#if 0
     progress       bar;
     const unsigned nmax=10000;
 
@@ -63,8 +93,28 @@ Y_UTEST(ilog)
     }
     bar.update(1);
     std::cerr << std::endl;
+#endif
+
+    ios::ocstream::overwrite("ilog.dat");
 
 
+    for(unsigned nbits=0;nbits<=16;++nbits)
+    {
+        std::cerr << "nbits=" << nbits << std::endl;
+        double     dmax = 0;
+        unsigned   lfac = 0;
+        const bool ans  = LnApprox::Test(nbits,10000,dmax,lfac);
+        if( ans )
+        {
+            std::cerr << "success" << std::endl;
+            ios::ocstream::echo("ilog.dat","%u %g %u\n",nbits,dmax,lfac);
+
+        }
+        else
+        {
+            std::cerr << "failure" << std::endl;
+        }
+    }
 
 
 
