2f91d1afb0b24a04bbc24fc40a7bbe5398ebe386
ybouret <yann.bouret@gmail.com>
2019-05-08 12:39:08 +0200

    engine WIP

--- src/net/y/net/io/engine.hpp
+++ src/net/y/net/io/engine.hpp
@@ -3,13 +3,25 @@
 #define Y_NET_IO_ENGINE_INCLUDED 1
 
 #include "y/net/socket/set.hpp"
-
+#include "y/net/tcp/server.hpp"
 
 namespace upsylon
 {
     namespace net
     {
 
+        class tcp_client_protocol;
+
+        class tcp_server_protocol : public tcp_server
+        {
+        public:
+            virtual ~tcp_server_protocol() throw() {}
+
+            explicit tcp_server_protocol();
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(tcp_server_protocol);
+        };
 
 
         //! handle connections
