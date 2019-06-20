1892e7f251781665e6f39772341725fb771e8740
ybouret <yann.bouret@gmail.com>
2019-06-20 15:59:28 +0200

    WIP points

--- src/math/tests/test-points.cpp
+++ src/math/tests/test-points.cpp
@@ -6,41 +6,78 @@
 using namespace upsylon;
 using namespace math;
 
+template <typename T> struct dummy
+{
+    T u, v;
+    inline dummy( T a=0, T b=0) : u(a), v(b)
+    {
+    }
+
+    friend inline std::ostream &operator<<( std::ostream &os, const dummy &d )
+    {
+        os << "{" << d.u << "," << d.v<< "}";
+        return os;
+    }
+};
+
 template <typename T,
 template <typename> class POINT,
 template <typename> class SEQUENCE>
-static inline void do_points()
+static inline void do_points(const size_t np)
 {
+    std::cerr << "Perform Tests On Points" << std::endl;
     typedef Geometric::Points<T,POINT,SEQUENCE>  PointsType;
     typedef Geometric::PointInfoFor<T,POINT>     PointInfo;
     typedef typename PointInfo::Type             PointType;
     typedef typename PointInfo::Core             CorePoint;
     
     PointsType points;
+    const float noise = 0.01f;
+    const float radius = 5.0;
+    for(size_t i=0;i<np;++i)
+    {
+        const float theta = ((numeric<float>::two_pi*i)/np) * (1.0f+noise*alea.symm<float>() );
+        const float x     = radius * cosf( theta ) * (1.0f+noise*alea.symm<float>() );
+        const float y     = radius * sinf( theta ) * (1.0f+noise*alea.symm<float>() );
+        points.add(x,y);
+    }
+
+    std::cerr << "Points: " << std::endl;
     for(typename PointsType::iterator i=points.begin(); i != points.end(); ++i)
     {
         const PointType &p = *i;
-        std::cerr << p << std::endl;
+        const CorePoint &c = PointInfo::Type2Core(p);
+        std::cerr << "\t" << p << " / " <<  c << std::endl;
     }
+    std::cerr << "Barycenter: " << points.barycenter() << std::endl;
+    std::cerr << std::endl;
 
 }
 
-#define DO_POINTS__(TYPE,SEQ)      \
-do_points<TYPE,point2d,SEQ>(); \
-do_points<TYPE,complex,SEQ>(); \
-do_points<TYPE,point3d,SEQ>()
+#define DO_POINTS__(TYPE,SEQ)    \
+do_points<TYPE,point2d,SEQ>(np); \
+do_points<TYPE,complex,SEQ>(np); \
+do_points<TYPE,point3d,SEQ>(np); \
+do_points<TYPE,dummy,SEQ>(np)
 
 #define DO_POINTS_(SEQ) \
-DO_POINTS__(float,SEQ); DO_POINTS__(double,SEQ)
+DO_POINTS__(float,SEQ); \
+DO_POINTS__(double,SEQ)
 
-
+#include "y/string/convert.hpp"
 
 Y_UTEST(points)
 {
+    size_t np=3;
+    if(argc>1)
+    {
+        np = string_convert::to<size_t>( argv[1], "np" );
+    }
     DO_POINTS_(list);
     DO_POINTS_(vector_for<memory::global>::device);
     DO_POINTS_(vector_for<memory::pooled>::device);
 
+    do_points<float,dummy,list>(np);
 }
 Y_UTEST_DONE()
 
--- src/math/y/math/geometric/points.hpp
+++ src/math/y/math/geometric/points.hpp
@@ -14,20 +14,38 @@ namespace upsylon
         namespace Geometric
         {
 
-            template <const size_t DIM,typename T>
-            struct InfoFor;
+            template <const size_t DIM,typename T> struct InfoFor;
 
             template <typename T,template <typename> class POINT>
             struct PointInfoFor
             {
                 Y_DECL_ARGS(T,type);
-                typedef POINT<mutable_type>                         Type;
-                static const size_t                                 Dimension = sizeof(Type)/sizeof(type);
-                typedef typename InfoFor<Dimension,type>::PointType Core;
-                static inline void CheckSizes()
+                typedef POINT<mutable_type>             Type;                                  //!< user point type
+                static const size_t                     Dimension = sizeof(Type)/sizeof(type); //!< the point dimension
+                typedef InfoFor<Dimension,mutable_type> InfoType;                              //!< info for this topolgy
+                typedef typename InfoType::PointType    Core;                                  //!< internal representation
+
+                static inline Core &Type2Core(  Type &t ) throw()
                 {
-                    Y_STATIC_CHECK(sizeof(Type)==sizeof(Core),PointSize);
+                    return *(Core *)&t;
                 }
+
+                static inline const Core &Type2Core( const Type &t ) throw()
+                {
+                    return *(const Core *)&t;
+                }
+
+                static inline Type & Core2Type( Core &c ) throw()
+                {
+                    return *(Type *)&c;
+                }
+
+                static inline const Type & Core2Type( const Core &c ) throw()
+                {
+                    return *(const Type *)&c;
+                }
+
+
 
             };
 
@@ -50,16 +68,44 @@ namespace upsylon
             {
             public:
                 Y_DECL_ARGS(T,type);
-                typedef PointInfoFor<T,POINT>    PointInfo;
-                typedef typename PointInfo::Type PointType;
-                typedef typename PointInfo::Core CorePoint;
+                typedef PointInfoFor<T,POINT>                 PointInfo;
+                typedef typename PointInfo::Type              PointType;
+                typedef typename PointInfo::Core              CorePoint;
+                typedef SEQUENCE<PointType>                   SequenceType;
+                typedef typename SequenceType::iterator       iterator;
+                typedef typename SequenceType::const_iterator const_iterator;
 
-                inline explicit Points() {}
+
+                inline explicit Points() : origin() {}
                 inline virtual ~Points() throw() {}
 
+                inline Points & add(param_type x, param_type y)               { const PointType p(x,y);   this->push_back(p); return *this; }
+                inline Points & add(param_type x, param_type y, param_type z) { const PointType p(x,y,z); this->push_back(p); return *this; }
+
+                inline PointType barycenter() const
+                {
+                    const size_t n = this->size();
+                    switch(n)
+                    {
+                        case 0:  return PointInfo::Core2Type(origin);
+                        case 1:  return this->front();
+                        default: break;
+                    }
+                    CorePoint      bar;
+                    const_iterator i=this->begin();
+                    for(size_t count=n;count>0;--count,++i)
+                    {
+                        bar += PointInfo::Type2Core(*i);
+                    }
+                    bar /= n;
+                    return PointInfo::Core2Type(bar);
+                }
 
             private:
                 Y_DISABLE_ASSIGN(Points);
+                
+            public:
+                const CorePoint origin;
             };
 
         }
