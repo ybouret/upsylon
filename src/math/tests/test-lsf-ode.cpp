eee145088a86ee0127f447f440533ba736df4154
ybouret <yann.bouret@gmail.com>
2019-07-08 00:03:43 +0200

    WIP differential solver

--- src/math/tests/test-lsf-ode.cpp
+++ src/math/tests/test-lsf-ode.cpp
@@ -2,17 +2,125 @@
 #include "y/math/fit/explode.hpp"
 #include "y/utest/run.hpp"
 #include "y/ios/ocstream.hpp"
+#include "y/math/ode/explicit/driver-ck.hpp"
+#include "y/math/fit/samples-io.hpp"
+#include "y/math/fit/ls.hpp"
 
 using namespace upsylon;
 using namespace math;
 
 namespace
 {
-
+    class Shape : public ODE::System<double>
+    {
+    public:
+        explicit Shape() throw() {}
+        virtual ~Shape() throw() {}
+        
+        virtual  size_t dimension() const throw()
+        {
+            return 2;
+        }
+        
+        virtual void setup( Array &y ) const throw()
+        {
+            y[1] = 1;
+            y[2] = 0;
+        }
+        
+        virtual void rates( array<double> &dydx,
+                           double ,
+                           const array<double> &y,
+                           const array<double> &aorg,
+                           const Variables     &vars)
+        {
+            assert(dydx.size()==2);
+            assert(y.size()==2);
+            
+            const double omega = vars(aorg,"omega");
+            
+            dydx[1] = y[2];
+            dydx[2] = - omega*omega * y[1];
+        }
+        
+        virtual double start() const throw()
+        {
+            return 0;
+        }
+        
+        virtual double delta() const throw()
+        {
+            return 0.1;
+        }
+        
+        virtual double query( const array<double> &y )
+        {
+            return y[1];
+        }
+        
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(Shape);
+        
+    };
 }
 
 Y_UTEST(lsf_ode)
 {
+    
+    Shape                       shape;
+    ODE::DriverCK<double>::Type driver;
+    
+    Fit::ExplODE<double> F(shape,driver);
+    
+   
+    
+    const size_t NP    = 10 + alea.leq(30);
+    const double range = 5+alea.leq(10);
+    double noise = 0.1;
+    
+    vector<double> X,Y,Yf;
+    Fit::Sample<double> sample(X,Y,Yf);
+    
+    Fit::Variables &vars = sample.variables; vars << "omega";
+    const size_t    nvar = vars.size();
+    std::cerr << "vars=" << vars << std::endl;
+    vector<double> aorg(nvar,0);
+    vector<double> aerr(nvar,0);
+    vector<bool>   used(nvar,0);
+    
+    double &omega = vars(aorg,"omega");
+    omega = 2.0;
+    
+    for(size_t i=NP;i>0;--i)
+    {
+        X.push_back( range * alea.to<double>() );
+        Y.push_back( cos( omega * X.back()) * (1.0+noise*alea.symm<double>()) );
+        Yf.push_back(0);
+    }
+    
+    sample.prepare();
+    std::cerr << "D2=" << sample.computeD2(F, aorg) << std::endl;
+    Fit::IO::Save("ode-fit0.dat",sample);
+
+    
+    Fit::LeastSquares<double> ls;
+    
+    omega *= 0.9;
+    
+    vars.on(used, "omega");
+    
+    ls.verbose = true;
+    
+    if( !ls.fit(sample, F, aorg, aerr, used) )
+    {
+        throw exception("couldn't fit");
+    }
+    
+    vars.display(std::cerr, aorg, aerr);
+    Fit::IO::Save("ode-fit1.dat",sample);
+
+    
+    
 
 }
 Y_UTEST_DONE()
--- src/math/y/math/fit/explode.hpp
+++ src/math/y/math/fit/explode.hpp
@@ -3,7 +3,7 @@
 #define Y_MATH_FIT_EXPLODE_INCLUDED 1
 
 #include "y/math/fit/samples.hpp"
-#include "y/math/ode/types.hpp"
+#include "y/math/ode/explicit/solver.hpp"
 
 namespace upsylon
 {
@@ -18,15 +18,15 @@ namespace upsylon
             public:
                 typedef typename Field<T>::Callback   Callback; //!< alias
                 typedef typename Fit::Type<T>::Array  Array;    //!< alias
-
-                inline virtual ~System() throw();
+                typedef Fit::Variables                Variables; //!< alias
+                inline virtual ~System() throw() {}
 
                 virtual size_t dimension() const throw()                 = 0; //!< dimensionality
                 virtual void   setup( array<T> & ) const throw()         = 0; //!< initialize internal variables at initial coordinate
                 virtual T      start() const throw()                     = 0; //!< return linear=starting point
                 virtual T      query( const array<T> &Y )                = 0; //!< extract scalar data from state
                 virtual void   rates(Array &dYdx, T x, const Array &Y,                   //|
-                                     const Array &aorg, const Fit::Variables &vars) = 0; //!< differential rates
+                                     const Array &aorg, const Variables &vars) = 0; //!< differential rates
                 virtual T      delta() const throw() = 0;
 
             protected:
@@ -39,9 +39,7 @@ namespace upsylon
 
         namespace Fit
         {
-
-
-
+            
             //! wrapper to provide sequential call
             template <typename T>
             class ExplODE : public Type<T>::Sequential
@@ -50,13 +48,16 @@ namespace upsylon
                 typedef typename ODE::Field<T>::Equation Equation;
                 typedef typename Type<T>::Array          Array;
 
-                inline explicit ExplODE( ODE::System<T> &s ) :
-                sys(s),
+                inline explicit ExplODE(ODE::System<T>         &sys_,
+                                        ODE::ExplicitSolver<T> &slv_) :
+                sys(sys_),
+                slv(slv_),
                 ode( this, & ExplODE<T>::compute ),
                 arr( sys.dimension() ),
                 p_aorg(0),
                 p_vars(0)
                 {
+                    slv.start( sys.dimension() );
                 }
 
                 inline const array<T> &fields() const throw() { return arr; }
@@ -64,33 +65,54 @@ namespace upsylon
 
             private:
                 Y_DISABLE_COPY_AND_ASSIGN(ExplODE);
-                ODE::System<T>  &sys;
-                Equation         ode;
-                vector<T>        arr;
-                const Array     *p_aorg;
-                const Variables *p_vars;
+                ODE::System<T>         &sys;
+                ODE::ExplicitSolver<T> &slv;
+                Equation                ode;
+                vector<T>               arr;
+                const Array            *p_aorg;
+                const Variables        *p_vars;
 
-                inline void compute( Array &dYdx, T x, const Array &Y )
+                inline void compute( Array &dYdx, T x, const Array &Y)
                 {
-
+                    assert(p_aorg);
+                    assert(p_vars);
+                    return sys.rates(dYdx, x, Y, *p_aorg, *p_vars);
                 }
 
-                inline virtual T on_initialize(T x, const Array &aorg, const Variables &vars)
+                inline virtual T on_initialize(T x1, const Array &aorg, const Variables &vars)
                 {
+                    // link
+                    p_aorg = &aorg;
+                    p_vars = &vars;
+                    
+                    // initialize up to x1
                     sys.setup(arr);
-                    T xstart = sys.start();
-                    T ctrl   = sys.delta();
-
+                    T x0    = sys.start();
+                    T ctrl  = sys.delta();
+                    
+                    // differential step
+                    slv( ode, arr, x0, x1, ctrl, NULL);
+                    
+                    // done
                     return sys.query(arr);
                 }
 
-                inline virtual T on_compute_to(T, const Array &aorg, const Variables &vars)
+                inline virtual T on_compute_to(T x1, const Array &aorg, const Variables &vars)
                 {
-
+                    // link
+                    p_aorg = &aorg;
+                    p_vars = &vars;
+                    
+                    const T x0   = this->current;
+                    T       ctrl = sys.delta();
+                    
+                    // differential step
+                    slv( ode, arr, x0, x1, ctrl, NULL);
+                    
+                    // done
+                    return sys.query(arr);
                 }
 
-            public:
-                T ctrl;
             };
         }
     }
