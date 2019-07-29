9ec573655afcdefd0b370d0cf26b8b563b01e93b
ybouret <yann.bouret@gmail.com>
2019-07-29 14:44:21 +0200

    upgraded data set

---
+++ src/math/y/math/io/data-set.cpp
@@ -0,0 +1,18 @@
+#include "y/math/io/data-set.hpp"
+
+namespace upsylon
+{
+    namespace math
+    {
+        data_set_:: ~data_set_() throw()
+        {
+
+        }
+
+        data_set_:: data_set_() throw() : labels()
+        {
+        }
+        
+    }
+}
+
--- src/math/y/math/io/data-set.hpp
+++ src/math/y/math/io/data-set.hpp
@@ -2,13 +2,14 @@
 #ifndef Y_MATH_DATA_SET_INCLUDED
 #define Y_MATH_DATA_SET_INCLUDED 1
 
+#include "y/sequence/vector.hpp"
 #include "y/associative/map.hpp"
-#include "y/container/sequence.hpp"
 #include "y/ios/icstream.hpp"
 #include "y/exception.hpp"
 #include "y/string/tokenizer.hpp"
 #include "y/string/convert.hpp"
 #include "y/comparison.hpp"
+#include "y/memory/pooled.hpp"
 
 namespace upsylon
 {
@@ -18,17 +19,32 @@ namespace upsylon
         {
             //! separators for columns
             inline bool data_set_separator(const int C) { return ' '==C || '\t' == C; }
-
         }
+
+        class data_set_
+        {
+        public:
+            typedef memory::pooled             memory_type;
+            typedef key_dumper                 hasher_type;
+            typedef vector<string,memory_type> strings;
+            virtual ~data_set_() throw();
+
+        protected:
+            explicit data_set_() throw();
+            strings labels;
+            
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(data_set_);
+        };
 
         //! temporaty object to load data files
         template <typename T>
-        class data_set
+        class data_set : public data_set_
         {
         public:
-            Y_DECL_ARGS(T,type);                //!< type alias
-            typedef sequence<type>     *column; //!< simple pointer to a sequence
-            typedef map<size_t,column>  colmap; //!< [index:column] map
+            Y_DECL_ARGS(T,type);                                        //!< type aliases
+            typedef sequence<type>                             *column; //!< simple pointer to a sequence
+            typedef map<size_t,column,hasher_type,memory_type>  colmap; //!< [index:column] map
 
             //! constructor, reserve a little memory
             inline explicit data_set(size_t n=2) : columns(n,as_capacity) {}
@@ -56,8 +72,17 @@ namespace upsylon
             //! load data from input stream,
             inline void load( ios::istream &fp, const size_t skip=0, const size_t nmax=0)
             {
+                labels.free();
+                const size_t  nc = columns.size();
+                labels.ensure(nc);
+
                 // preparing columns, order by increasing index
                 columns.sort_keys( comparison::increasing<size_t> );
+                for(typename colmap::iterator i=columns.begin();i!=columns.end();++i)
+                {
+                    const string l = vformat("column #%u", unsigned(i.key()));
+                    labels.push_back_(l);
+                }
 
                 unsigned iline = 1;
                 string   line(255,as_capacity);
@@ -65,6 +90,7 @@ namespace upsylon
                 {
                     if(!fp.gets(line)) throw exception("data_set: EOF while skipping line %u/%u", iline, unsigned(skip));
                 }
+
                 size_t loaded = 0;
                 while(fp.gets(line))
                 {
@@ -75,18 +101,19 @@ namespace upsylon
                     {
                         // now parse!
                         tokenizer<char> tkn(line);
-                        for(typename colmap::iterator i=columns.begin();i!=columns.end();++i)
+                        typename colmap::iterator i=columns.begin();
+                        for(size_t j=1;j<=nc;++i,++j)
                         {
                             const size_t idx = i.key();
                             while( tkn.count() != idx )
                             {
                                 if(!tkn.next(kernel::data_set_separator))
                                 {
-                                    throw exception("%u:data_set missing column #%u",iline,unsigned(idx));
+                                    throw exception("%u:data_set missing %s",iline,*labels[j]);
                                 }
                             }
                             const string word  = tkn.to_string();
-                            const T      value = string_convert::to<T>(word); // todo: add field?
+                            const T      value = string_convert::to<T>(word,*labels[j]);
                             (**i).push_back(value);
                         }
                         if(nmax>0&&++loaded>=nmax)
