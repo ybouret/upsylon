bb190d734cc0d6cb76161f6f6a4bcd2abd9d77d5
ybouret <yann.bouret@gmail.com>
2019-08-26 14:41:39 +0200

    corrected

--- src/main/y/string/io.cpp
+++ src/main/y/string/io.cpp
@@ -5,77 +5,80 @@ namespace  upsylon
 {
 
 #if 0
-    size_t string_io:: save_binary(ios::ostream &fp,
-                                   const string &s)
-    {
-        return fp.emit_block(s);
-    }
+	size_t string_io::save_binary(ios::ostream &fp,
+		const string &s)
+	{
+		return fp.emit_block(s);
+	}
 #endif
-    
-    string string_io:: load_binary(ios::istream &fp)
-    {
-        size_t n = fp.read_upack<size_t>();
-        string ans(n,as_capacity);
-        while(n-->0)
-        {
-            char C = 0;
-            fp.input(&C,1);
-            ans << C;
-        }
-        return ans;
-    }
 
-    template<>
-    string string:: read(ios::istream &fp)
-    {
-        size_t n = fp.read_upack<size_t>();
-        string ans(n,as_capacity);
-        while(n-->0)
-        {
-            char C = 0;
-            fp.input(&C,1);
-            ans << C;
-        }
-        return ans;
-    }
+	string string_io::load_binary(ios::istream &fp)
+	{
+		size_t n = fp.read_upack<size_t>();
+		string ans(n, as_capacity);
+		while (n-- > 0)
+		{
+			char C = 0;
+			fp.input(&C, 1);
+			ans << C;
+		}
+		return ans;
+	}
+
+	namespace core
+	{
+		template<>
+		string<char> string<char>::read(ios::istream &fp)
+		{
+			size_t n = fp.read_upack<size_t>();
+			string ans(n, as_capacity);
+			while (n-- > 0)
+			{
+				char C = 0;
+				fp.input(&C, 1);
+				ans << C;
+			}
+			return ans;
+		}
+	}
 
 
-    static inline void __emit_hexa(ios::ostream &fp, const uint8_t B)
-    {
-        fp << '\\' << 'x' << hexadecimal::lowercase[B];
-    }
+	static inline void __emit_hexa(ios::ostream &fp, const uint8_t B)
+	{
+		fp << '\\' << 'x' << hexadecimal::lowercase[B];
+	}
 
-    ios::ostream & string_io:: write_C(ios::ostream &fp, const string &s)
-    {
-        for(size_t i=0;i<s.size();++i)
-        {
-            const uint8_t B = s[i];
-            if(B<32)
-            {
-                switch(B)
-                {
-                    case '\n': fp << '\\' << 'n'; break;
-                    case '\r': fp << '\\' << 'r'; break;
-                    case '\t': fp << '\\' << 't'; break;
-                    default: __emit_hexa(fp,B);
-                }
-            }
-            else if(B>=127)
-            {
-                __emit_hexa(fp,B);
-            }
-            else
-            {
-                switch(B)
-                {
-                    case '"' : fp << '\\' << '"'; break;
-                    case '\'': fp << '\\' << '\''; break;
-                    default:
-                        fp << char(B);
-                }
-            }
-        }
-        return fp;
-    }
+	ios::ostream & string_io::write_C(ios::ostream &fp, const string &s)
+	{
+		for (size_t i = 0; i < s.size(); ++i)
+		{
+			const uint8_t B = s[i];
+			if (B < 32)
+			{
+				switch (B)
+				{
+				case '\n': fp << '\\' << 'n'; break;
+				case '\r': fp << '\\' << 'r'; break;
+				case '\t': fp << '\\' << 't'; break;
+				default: __emit_hexa(fp, B);
+				}
+			}
+			else if (B >= 127)
+			{
+				__emit_hexa(fp, B);
+			}
+			else
+			{
+				switch (B)
+				{
+				case '"': fp << '\\' << '"'; break;
+				case '\'': fp << '\\' << '\''; break;
+				default:
+					fp << char(B);
+				}
+			}
+		}
+		return fp;
+	}
 
 }
