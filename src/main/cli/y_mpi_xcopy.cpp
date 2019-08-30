69c3b1e569d8408c735a5f6de4d17039432e7112
ybouret <yann.bouret@gmail.com>
2019-08-30 07:34:37 +0200

    added utility

---
+++ src/main/cli/y_mpi_xcopy.cpp
@@ -0,0 +1,57 @@
+#include "y/program.hpp"
+#include "y/ios/icstream.hpp"
+#include "y/sequence/vector.hpp"
+#include "y/memory/pooled.hpp"
+#include "y/string/tokenizer.hpp"
+#include "y/sort/unique.hpp"
+#include "y/fs/vfs.hpp"
+#include <cstdlib>
+
+using namespace upsylon;
+
+static inline bool is_blank( const int C ) throw() { return C == ' ' || C == '\t' ; }
+
+Y_PROGRAM_START()
+{
+    if(argc<=1)
+    {
+        throw exception("usage: %s hostfile [binary]",program);
+    }
+
+    // parsing hosts
+    vector<string,memory::pooled> hosts(16,as_capacity);
+    {
+        const string  hostfile = argv[1];
+        ios::icstream fp(hostfile);
+        string        line;
+        while( fp.gets(line) )
+        {
+            if( line.size() <= 0 ) continue;
+            if( line[0] == '#' )   continue;
+            tokenizer<char> tkn(line);
+            if( tkn.next(is_blank) )
+            {
+                const string id( tkn.token(), tkn.units() );
+                hosts.push_back(id);
+            }
+        }
+    }
+    unique(hosts);
+
+    if(argc>2)
+    {
+        const string filename = argv[2];
+        const string basename = vfs::get_base_name(filename);
+        for(size_t i=1;i<=hosts.size();++i)
+        {
+            const string cmd      = "scp " + filename + " " + hosts[i] + ":" + basename;
+            std::cerr << "executing <" << cmd << ">" << std::endl;
+            (void) system( *cmd );
+        }
+    }
+
+
+}
+Y_PROGRAM_END()
+
+
