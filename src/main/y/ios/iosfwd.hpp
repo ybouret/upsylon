d92be7d95475aa371c5450f6f088c727420f7ce5
ybouret <yann.bouret@gmail.com>
2019-08-31 11:40:47 +0200

    need to work on reduction

---
+++ src/main/y/ios/iosfwd.hpp
@@ -0,0 +1,17 @@
+//! \file
+#ifndef Y_IOS_FWD_INCLUDED
+#define Y_IOS_FWD_INCLUDED 1
+
+#include "y/os/platform.hpp"
+
+namespace upsylon
+{
+    namespace ios
+    {
+        class istream; //!< forward istream declaration
+        class ostream; //!< forward ostream declaration
+    }
+}
+
+#endif
+
