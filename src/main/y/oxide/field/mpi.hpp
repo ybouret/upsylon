3ed3944933b7315c5cbb5e4b0831b096c6046aa9
ybouret <yann.bouret@gmail.com>
2019-09-08 14:53:04 +0200

    cleanup

--- src/main/y/oxide/field/mpi.hpp
+++ src/main/y/oxide/field/mpi.hpp
@@ -9,47 +9,7 @@ namespace upsylon
 {
     namespace Oxide
     {
-#if 0
-        //! MPI related routines
-        struct Comm
-        {
-            static const int    Tag        = 0x07; //!< default tag
-
-            //! send a block
-            static inline
-            void Send(mpi             &MPI,
-                      const IO::Block &block,
-                      const int        target,
-                      const comm_mode  mode)
-            {
-                MPI.vSend(mode,block,target,Tag);
-
-            }
-
-            //! recv a block
-            static inline
-            void Recv(mpi            &MPI,
-                      IO::Block      &block,
-                      const int       source,
-                      const comm_mode mode)
-            {
-                MPI.vRecv(mode,block,source,Tag);
-            }
-
-
-            //! sendrecv
-            static inline
-            void SendRecv(mpi             & MPI,
-                          const IO::Block & sendBlock,
-                          const int         target,
-                          IO::Block       & recvBlock,
-                          const int         source,
-                          const comm_mode   mode)
-            {
-                MPI.vSendRecv(mode,sendBlock,target,Tag,recvBlock,source,Tag);
-            }
-        };
-#endif
+        
     }
 }
 
