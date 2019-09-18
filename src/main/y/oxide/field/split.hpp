298bd5947d97eca2c1c0c5b29761911341534768
ybouret <yann.bouret@gmail.com>
2019-09-18 08:28:45 +0200

    will match preferred split

---
+++ src/main/y/oxide/field/split.hpp
@@ -0,0 +1,105 @@
+
+//! \file
+#ifndef Y_OXIDE_FIELD_SPLIT_INCLUDED
+#define Y_OXIDE_FIELD_SPLIT_INCLUDED 1
+
+#include "y/oxide/field/divide.hpp"
+
+namespace upsylon
+{
+    namespace Oxide
+    {
+
+        //======================================================================
+        //
+        //
+        //! information about splitting layouts
+        //
+        //
+        //======================================================================
+        template <typename COORD> class Split
+        {
+        public:
+            //------------------------------------------------------------------
+            //
+            // types and definitions
+            //
+            //------------------------------------------------------------------
+            static const size_t                             Dimensions = Coord::Get<COORD>::Dimensions;
+            typedef Layout<COORD>                           LayoutType;       //!< alias
+            typedef typename LayoutType::coord              coord;            //!< alias
+            typedef typename LayoutType::const_coord        const_coord;      //!< alias
+            typedef vector<COORD>                           MappingsType;     //!< will store mappings
+
+            //------------------------------------------------------------------
+            //
+            // members
+            //
+            //------------------------------------------------------------------
+            const MappingsType mappings;            //!< all possible mappings
+            const_coord        optimal;             //!< optimal mappings
+            const_coord        favorite;            //!< from preferred
+
+            //! create possible mappings and pick optimal
+            inline explicit Split(const size_t      cores,
+                                  const LayoutType &fullLayout,
+                                  const_coord       boundaries,
+                                  coord             preferred) :
+            mappings(),
+            optimal( Divide::Find(fullLayout,cores,boundaries, (MappingsType *)&mappings  ) ),
+            favorite(optimal)
+            {
+                //______________________________________________________________
+                //
+                // check that everything is in place
+                //______________________________________________________________
+                if( Coord::Product(optimal) <= 0 )
+                    Divide::ErrorNoMappingFor(cores);
+
+                //______________________________________________________________
+                //
+                // analyze preferred request
+                //______________________________________________________________
+                size_t     active=0;
+                for(size_t dim=0;dim<Dimensions;++dim)
+                {
+                    Coord1D &c = Coord::Of(preferred,dim);
+                    if(c!=0)
+                    {
+                        c=1;
+                        ++active;
+                    }
+                }
+
+                //______________________________________________________________
+                //
+                // try to match request
+                //______________________________________________________________
+                if(active>0&&active<Dimensions)
+                {
+                    // user requested a preferred layout
+                }
+                else
+                {
+                    // keep the default (=optimal?) choice
+                }
+
+            }
+
+            //! cleanup
+            inline virtual ~Split() throw()
+            {
+                bzset_(optimal);
+                bzset_(favorite);
+            }
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(Split);
+        };
+
+    }
+
+}
+
+#endif
+
--- src/main/y/oxide/mpi/parallel.hpp
+++ src/main/y/oxide/mpi/parallel.hpp
@@ -3,7 +3,7 @@
 #define Y_OXIDE_MPI_PARALLEL_INCLUDED 1
 
 #include "y/mpi/mpi.hpp"
-#include "y/oxide/field/divide.hpp"
+#include "y/oxide/field/split.hpp"
 
 namespace upsylon
 {
@@ -17,7 +17,7 @@ namespace upsylon
         //
         //
         //======================================================================
-        template <typename COORD> class Parallel
+        template <typename COORD> class Parallel : public Split<COORD>
         {
         public:
             //------------------------------------------------------------------
@@ -25,65 +25,32 @@ namespace upsylon
             // types and definitions
             //
             //------------------------------------------------------------------
-            static const size_t                             Dimensions = Coord::Get<COORD>::Dimensions;
+            //static const size_t                             Dimensions = Coord::Get<COORD>::Dimensions;
             typedef Layout<COORD>                           LayoutType;       //!< alias
             typedef typename LayoutType::coord              coord;            //!< alias
             typedef typename LayoutType::const_coord        const_coord;      //!< alias
-            typedef vector<COORD>                           MappingsType;     //!< will store mappings
+            //typedef vector<COORD>                           MappingsType;     //!< will store mappings
 
             //------------------------------------------------------------------
             //
             // members
             //
             //------------------------------------------------------------------
-            const MappingsType mappings;            //!< all possible mappings
-            const_coord        optimal;             //!< optimal mappings
-            const_coord        favorite;            //!< from preferred
+
 
             //! create possible mappings and pick optimal
             inline explicit Parallel(mpi              &MPI,
                                      const LayoutType &fullLayout,
                                      const_coord       boundaries,
                                      coord             preferred) :
-            mappings(),
-            optimal( Divide::Find(fullLayout,MPI.size,boundaries, (MappingsType *)&mappings  ) ),
-            favorite(optimal)
+            Split<COORD>( MPI.size, fullLayout, boundaries, preferred)
             {
-                if( Coord::Product(optimal) <= 0 ) Divide::ErrorNoMappingFor(MPI.size);
-                size_t    active=0;
-                for(size_t dim=0;dim<Dimensions;++dim)
-                {
-                    Coord1D &c = Coord::Of(preferred,dim);
-                    if(c!=0)
-                    {
-                        c=1;
-                        ++active;
-                    }
-                }
-#if 0
-                MPI.print0(stderr,"{%ld",Coord::Of(preferred,0));
-                for(size_t dim=1;dim<Dimensions;++dim)
-                {
-                    MPI.print0(stderr," %ld",Coord::Of(preferred,dim));
-                }
-                MPI.print0(stderr,"}");
-#endif
-                if(active>0&&active<Dimensions)
-                {
-                    // user requested a preferred layout
-                }
-                else
-                {
-                    // keep the default (=optimal?) choice
-                }
-
             }
 
             //! cleanup
             inline virtual ~Parallel() throw()
             {
-                bzset_(optimal);
-                bzset_(favorite);
+
             }
 
         private:
