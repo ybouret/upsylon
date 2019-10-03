325d8f47cf7d65fb0a174f454e04427311121994
ybouret <yann.bouret@gmail.com>
2019-10-03 08:47:09 +0200

    WIP euclidean

--- src/math/tests/test-euclidean.cpp
+++ src/math/tests/test-euclidean.cpp
@@ -1,12 +1,55 @@
 #include "y/math/euclidean/point.hpp"
+#include "y/type/complex.hpp"
 #include "y/utest/run.hpp"
+#include <typeinfo>
 
 using namespace upsylon;
 using namespace math;
+using namespace Euclidean;
+
+namespace {
+
+    template <typename T, template <class> class POINT>
+    static inline void make_euclidean()
+    {
+
+        typedef Point<T,POINT>              PointType;
+        typedef typename PointType::Pointer SharedPoint;
+        typedef Node<T,POINT>               NodeType;
+        typedef typename NodeType::List     List;
+
+        const std::type_info &tid = typeid(T);
+        const std::type_info &pid = typeid(PointType);
+
+        std::cerr << "Using " << pid.name() << "/" << tid.name() << std::endl;
+        std::cerr << "|_sizeof(type)   = " << sizeof(T)           << std::endl;
+        std::cerr << "|_sizeof(point)  = " << sizeof(PointType)   << std::endl;
+        std::cerr << "|_sizeof(shared) = " << sizeof(SharedPoint) << std::endl;
+        std::cerr << "|_sizeof(node)   = " << sizeof(NodeType)    << std::endl;
+        std::cerr << "|_sizeof(list)   = " << sizeof(List)        << std::endl;
+
+        List points;
+        for(size_t i=10+alea.leq(100);i>0;--i)
+        {
+            SharedPoint P = new PointType();
+            points.push_back( new NodeType(P) );
+        }
+
+    }
+
+}
 
 
 Y_UTEST(euclidean)
 {
+
+    make_euclidean<float,point2d>();
+    make_euclidean<double,point2d>();
+    make_euclidean<float,point3d>();
+    make_euclidean<double,point3d>();
+    make_euclidean<float,complex>();
+    make_euclidean<double,complex>();
+
 }
 Y_UTEST_DONE()
 
--- src/math/y/math/euclidean/point.hpp
+++ src/math/y/math/euclidean/point.hpp
@@ -3,6 +3,10 @@
 #define Y_EUCLIDRAN_POINT_INCLUDED 1
 
 #include "y/type/point3d.hpp"
+#include "y/type/args.hpp"
+#include "y/type/bzset.hpp"
+#include "y/ptr/arc.hpp"
+#include "y/core/list.hpp"
 
 namespace upsylon {
 
@@ -10,6 +14,50 @@ namespace upsylon {
 
         namespace Euclidean {
 
+            template <typename T, template <class> class POINT >
+            class Point  : public counted_object
+            {
+            public:
+                Y_DECL_ARGS(T,type);
+                typedef POINT<type>     Type;
+                static const size_t     Dimensions = sizeof(Type)/sizeof(type);
+                typedef arc_ptr<Point>  Pointer;
+
+                Type position;
+                Type celerity;
+
+                inline explicit Point() throw() : position(), celerity() {}
+                inline explicit Point(const Type p) throw() : position(p), celerity() {}
+                inline explicit Point(const Type p, const Type c) throw() : position(p), celerity(c) {}
+
+                inline virtual ~Point() throw()
+                {
+                    bzset(position);
+                    bzset(celerity);
+                }
+
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(Point);
+            };
+
+            template <typename T,template <class> class POINT >
+            class Node : public Point<T,POINT>::Pointer
+            {
+            public:
+                typedef Point<T,POINT>              PointType;
+                typedef typename PointType::Pointer SharedPoint;
+                typedef typename PointType::Type    Type;
+                typedef core::list_of_cpp<Node>     List;
+
+                Node *next;
+                Node *prev;
+
+                inline explicit Node( const SharedPoint &P ) throw() : SharedPoint(P), next(0), prev(0) {}
+                inline virtual ~Node() throw() {}
+
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(Node);
+            };
 
         }
 
