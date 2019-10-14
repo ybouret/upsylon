b0cb74625ad8d5e0a81c72f807b465281a1815ca
ybouret <yann.bouret@gmail.com>
2019-10-14 19:47:20 +0200

    will change to auto_cast

--- src/main/tests/test-cast.cpp
+++ src/main/tests/test-cast.cpp
@@ -10,22 +10,29 @@ namespace {
     template <typename T, typename U> static inline
     void checkCast()
     {
-        typedef class_cast<T,U> Cast;
+        typedef class_cast<T,U> __cast;
         static const char      *tid = typeid(T).name();
         static const char      *uid = typeid(U).name();
 
-        std::cerr << "<" << tid << "> is " << ( Cast::derived ? "" : "NOT ") << "derived from <" << uid << ">" << std::endl;
-
-
+        std::cerr << "<" << tid << "> is " << ( __cast::derived ? "" : "NOT ") << "derived from <" << uid << ">" << std::endl;
     }
 
+    
 }
 
 Y_UTEST(cast)
 {
     checkCast<int,double>();
     checkCast<int,int>();
-    checkCast<string,memory::ro_buffer>();
+    checkCast<memory::ro_buffer,string>();
+    checkCast<string,memory::rw_buffer>();
+
+    {
+        int    i = 10;
+        double x = class_cast<double,int>::_(i);
+        std::cerr << i << "=>" << x << std::endl;
+    }
+    
     
 }
 Y_UTEST_DONE()
--- src/main/y/type/class-cast.hpp
+++ src/main/y/type/class-cast.hpp
@@ -4,14 +4,30 @@
 #define Y_TYPE_CLASS_CAST_INCLUDED 1
 
 #include "y/type/class-conversion.hpp"
+#include "y/type/select.hpp"
 
 namespace upsylon {
 
     template <typename T,typename U>
-    struct class_cast
+    class class_cast
     {
+    public:
         static const bool derived = Y_IS_SUPERSUBCLASS(T,U);
+        typedef typename select_type<derived,T &,      T>::result type;
+        typedef typename select_type<derived,const T &,T>::result const_type;
 
+        static inline type       _( U       &args ) { return __(args,int2type<derived>()); }
+        static inline const_type _( const U &args ) { return __(args,int2type<derived>()); }
+
+    private:
+        
+        static inline T & __( U &args, int2type<true>  ) throw() { return args; }
+        static inline T   __( U &args, int2type<false> )         { return static_cast<T>(args); }
+        
+        static inline const T & __( const U &args, int2type<true>  ) throw() { return args; }
+        static inline T         __( const U &args, int2type<false> )         { return static_cast<T>(args); }
+
+        
     };
 
 }
