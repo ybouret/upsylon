649690d26aa54830c38532fce51cb2f3d1687be8
ybouret <yann.bouret@gmail.com>
2019-11-22 14:52:38 +0100

    WIP graphic

--- src/graphic/y/graphic/pixmap.hpp
+++ src/graphic/y/graphic/pixmap.hpp
@@ -9,6 +9,22 @@ namespace upsylon {
 
     namespace Graphic {
 
+
+        template <typename T>
+        class Pixmap : public Surface
+        {
+        public:
+            explicit Pixmap(const size_t W, const size_t H);
+
+            virtual ~Pixmap() throw()
+            {
+            }
+
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(Pixmap);
+        };
+
     }
     
 }
