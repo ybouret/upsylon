12d09adfeba9b55c38925ed0b7c35fd8a26ed779
ybouret <yann.bouret@gmail.com>
2019-11-28 11:52:32 +0100

    emergency

---
+++ src/graphic/y/graphic/color/yuv.cpp
@@ -0,0 +1,44 @@
+
+#include "y/graphic/color/yuv.hpp"
+#include "y/type/utils.hpp"
+
+namespace upsylon {
+
+    namespace Graphic {
+
+        const float YUV:: Umax =  0.436f;
+        const float YUV:: Umin = -0.436f;
+
+        const float YUV:: Vmax =  0.615f;
+        const float YUV:: Vmin = -0.615f;
+
+        float YUV:: toY(const float Y) throw()
+        {
+            return clamp<float>(0.0f,Y,1.0f);
+        }
+
+        float YUV:: toU(const float U) throw()
+        {
+            return clamp<float>(Umin,U,Umax);
+        }
+
+        float YUV:: toV(const float V) throw()
+        {
+            return clamp<float>(Vmin,V,Vmax);
+        }
+
+        YUV:: ~YUV() throw()
+        {
+        }
+
+        YUV:: YUV(const float Y,const float U,const float V) throw() :
+        y( toY(Y) ),
+        u( toU(U) ),
+        v( toV(V) )
+        {
+        }
+    }
+
+}
+
+
---
+++ src/graphic/y/graphic/color/yuv.hpp
@@ -0,0 +1,42 @@
+//! \file
+
+
+#ifndef Y_GRAPHIC_COLOR_RGB_INCLUDED
+#define Y_GRAPHIC_COLOR_RGB_INCLUDED 1
+
+#include "y/os/platform.hpp"
+
+namespace upsylon {
+
+    namespace Graphic {
+
+        class YUV
+        {
+        public:
+            static const float Umin;
+            static const float Umax;
+            static const float Vmin;
+            static const float Vmax;
+
+            float y; //!< 0..1
+            float u; //!< Umin..Umax
+            float v; //!< Vmin..Vmax
+
+            YUV(const float Y=0,const float U=0,const float V=0) throw();
+            YUV(const YUV &) throw();
+            YUV&operator=(const YUV &) throw();
+            
+            ~YUV() throw();
+
+            static float toY(const float) throw();
+            static float toU(const float) throw();
+            static float toV(const float) throw();
+
+        };
+
+    }
+
+}
+
+#endif
+
