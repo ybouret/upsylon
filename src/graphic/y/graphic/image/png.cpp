733dd7e5dee8cfbe2c7355927840d16b20b1c0dc
ybouret <yann.bouret@gmail.com>
2019-12-03 10:41:51 +0100

    need to redesign interface

--- src/graphic/y/graphic/image/png.cpp
+++ src/graphic/y/graphic/image/png.cpp
@@ -3,5 +3,18 @@
 #include "png.h"
 
 namespace upsylon {
-    
+
+    namespace Graphic {
+
+        PNG_Format:: PNG_Format() : Image::Format("PNG")
+        {
+
+        }
+
+        PNG_Format:: ~PNG_Format() throw()
+        {
+        }
+        
+
+    }
 }
--- src/graphic/y/graphic/image/png.hpp
+++ src/graphic/y/graphic/image/png.hpp
@@ -3,12 +3,22 @@
 #ifndef Y_GRAPHIC_IMAGE_IO_INCLUDED
 #define Y_GRAPHIC_IMAGE_IO_INCLUDED 1
 
-#include "y/graphic/bitmap.hpp"
+#include "y/graphic/image.hpp"
 
 namespace upsylon {
 
     namespace Graphic {
-        
+
+        class PNG_Format : public Image::Format
+        {
+        public:
+            explicit PNG_Format();
+            virtual ~PNG_Format() throw();
+
+        private:
+            Y_DISABLE_COPY_AND_ASSIGN(PNG_Format);
+        };
+
 
     }
 }
