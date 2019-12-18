0b3d4774283c8db010dcbab8bcc072f0458db189
ybouret <yann.bouret@gmail.com>
2019-12-18 09:00:15 +0100

    typo

--- src/graphic/y/graphic/ops/filter/gradients.cpp
+++ src/graphic/y/graphic/ops/filter/gradients.cpp
@@ -10,6 +10,7 @@ namespace upsylon {
 
         Gradients:: Gradients(const Kernel::Filter::Pointer &GX,
                               const Kernel::Filter::Pointer &GY) throw() :
+        Object(),
         X(GX),
         Y(GY)
         {
@@ -17,6 +18,7 @@ namespace upsylon {
 
 
         Gradients:: Gradients(const Gradients &G) throw() :
+        Object(),
         X(G.X),
         Y(G.Y)
         {
