07b023805106f852a0ebaf3fdf9bf437041955e8
ybouret <yann.bouret@gmail.com>
2020-01-16 11:07:26 +0100

    removed atom

--- src/math/tests/main.cpp
+++ src/math/tests/main.cpp
@@ -18,11 +18,6 @@ Y_UTEST_INIT(64)
     Y_UTEST(tridiag);
     Y_UTEST(cyclic);
     Y_UTEST(sparse);
-    Y_UTEST(atom0);
-    Y_UTEST(atom1);
-    Y_UTEST(atom1b);
-    Y_UTEST(atom2);
-    Y_UTEST(atom3);
 
     Y_UTEST(ode);
     Y_UTEST(rk4);
