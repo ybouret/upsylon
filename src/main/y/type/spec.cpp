ac42458ad63f4f1208a6c70a54f5200304a1979b
ybouret <yann.bouret@gmail.com>
2020-01-21 09:07:43 +0100

    code cleanup

--- src/main/y/type/spec.cpp
+++ src/main/y/type/spec.cpp
@@ -4,18 +4,12 @@
 
 namespace upsylon {
 
-    type_spec::alias:: ~alias() throw()
-    {
-    }
+    type_spec::alias:: ~alias() throw() {}
 
-    type_spec:: alias:: alias( const string &id ) :
-    next(0), prev(0), name(id) {}
+    type_spec:: alias:: alias(const string &id) : next(0), prev(0), name(id) {}
     
 
-    type_spec:: ~type_spec() throw()
-    {
-
-    }
+    type_spec:: ~type_spec() throw() {}
 
 
 #define Y_TYPE_SPEC_CTOR() \
@@ -23,33 +17,29 @@ info( tid ),               \
 uuid( tid.name() ),        \
 user()
 
-    type_spec:: type_spec( const std::type_info &tid ) :
+    type_spec:: type_spec(const std::type_info &tid) : Y_TYPE_SPEC_CTOR() { }
+
+    type_spec:: type_spec(const std::type_info &tid,
+                          const string         &known) :
     Y_TYPE_SPEC_CTOR()
     {
-
+        (void)aka(known);
     }
 
-    type_spec:: type_spec(const std::type_info &tid, const string &known) :
+    type_spec:: type_spec(const std::type_info &tid,
+                          const char           *known) :
     Y_TYPE_SPEC_CTOR()
     {
-        aka(known);
-    }
-
-    type_spec:: type_spec(const std::type_info &tid, const char *known) :
-    Y_TYPE_SPEC_CTOR()
-    {
-        aka(known);
+        (void)aka(known);
     }
 
 
     bool type_spec:: aka( const string &usr )
     {
-        if( usr == uuid )
-        {
+        if( usr == uuid ) {
             return true;
         }
-        else
-        {
+        else {
             for(const alias *scan=user.head; scan; scan=scan->next)
             {
                 if(usr==scan->name) return true;
@@ -104,7 +94,7 @@ namespace upsylon {
     std::type_info,
     type_spec_pointer,
     type_info_hasher,
-    type_spec_memory> type_spec_db;
+    type_spec_memory>                          type_spec_db;
 
     typedef map<
     string,
@@ -138,6 +128,10 @@ namespace upsylon {
                 type_spec              *ptr = new type_spec( tid );
                 const type_spec_pointer tsp = ptr;
                 if(!insert(tsp)) throw exception("unexpected insert failure");
+
+                //--------------------------------------------------------------
+                // insert uuid in dictionary
+                //--------------------------------------------------------------
                 try
                 {
                     if( !dict.insert(tsp->uuid,tsp) )
