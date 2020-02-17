005b19e15e8c8f91d6a8bc2327ad51591223af57
ybouret <yann.bouret@gmail.com>
2020-02-17 16:49:29 +0100

    WIP pack

--- src/main/y/information/pack.hpp
+++ src/main/y/information/pack.hpp
@@ -4,14 +4,18 @@
 #define Y_INFORMATION_PACK_INCLUDED 1
 
 #include "y/information/filter.hpp"
+#include "y/ios/ovstream.hpp"
 
 namespace upsylon {
 
     namespace information {
 
 
-        struct Pack
+        class Pack
         {
+        public:
+
+        private:
             
         };
 
