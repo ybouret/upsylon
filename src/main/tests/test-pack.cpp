bd6d38359b42f5cd4618fc344dd40d14c6641063
ybouret <yann.bouret@gmail.com>
2020-02-21 15:19:46 +0100

    typos...

--- src/main/tests/test-pack.cpp
+++ src/main/tests/test-pack.cpp
@@ -57,22 +57,5 @@ Y_UTEST(pack)
     std::cerr << " shannon-fano: " << sz << " -> " << nShannonFano << std::endl;
 
 
-#if 0
-    information::Pack::Encoder   packEnc(1024*512);
-
-    if(argc>1)
-    {
-        const string   fileName = argv[1];
-        ios::icstream  source( fileName );
-        ios::ocstream  target( "pack.bin" );
-
-        packEnc.reset();
-        size_t       nr = 0;
-        const size_t nc = packEnc.process(target, source, &nr);
-        std::cerr << "packEnc: " << nr << " -> " << nc << std::endl;
-
-    }
-#endif
-
 }
 Y_UTEST_DONE()
