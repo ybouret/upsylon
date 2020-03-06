fc7e5a66e945ec213c61bcc8ba3a6ceee52d1b6f
ybouret <yann.bouret@gmail.com>
2020-03-06 09:23:30 +0100

    WIP nw

--- src/main/tests/test-meta-loop.cpp
+++ src/main/tests/test-meta-loop.cpp
@@ -5,41 +5,53 @@ using namespace upsylon;
 namespace {
 
     template <size_t I>
-    struct doStep
+    struct decStep
     {
-        static const size_t level = I;
-        static inline void dec()
+        static inline void run()
         {
-            std::cerr << "\t<dec@" << level << std::endl;
-            doStep<I-1>::dec();
-        }
-
-        static inline void inc()
-        {
-            doStep<I-1>::inc();
-            std::cerr << "\t>inc@" << level << std::endl;
-
+            std::cerr << "\tdec@" << I << std::endl;
+            decStep<I-1>::run();
         }
     };
 
     template <>
-    struct doStep<0>
+    struct decStep<0>
     {
-        static const size_t level = 0;
-        static inline void dec() {
-            std::cerr << "\t<dec@" << level << std::endl;
-        }
-
-        static inline void inc() {
-            std::cerr << "\t>inc@" << level << std::endl;
+        static inline void run()
+        {
+            std::cerr << "\tdec@" << 0 << std::endl;
         }
     };
+
+    template <size_t I>
+    struct incStep
+    {
+        static inline void run()
+        {
+            incStep<I-1>::run();
+            std::cerr << "\tinc@" << I << std::endl;
+        }
+    };
+
+    template <>
+    struct incStep<0>
+    {
+        static inline void run()
+        {
+            std::cerr << "\tinc@" << 0 << std::endl;
+        }
+    };
+
+
+
 
     template <size_t N>
     static inline void doLoop()
     {
-        doStep<N>::dec();
-        doStep<N>::inc();
+        std::cerr << "Decreasing..." << std::endl;
+        decStep<N>::run();
+        std::cerr << "Increasing..." << std::endl;
+        incStep<N>::run();
     }
 
 #define DO_LOOP(I) \
@@ -55,14 +67,106 @@ std::cerr << "<Loop " << I << "/>" << std::endl << std::endl;
         DO_LOOP(2);
         DO_LOOP(3);
         DO_LOOP(4);
-        DO_LOOP(32);
+        DO_LOOP(300);
     }
 
 }
 
+#if 1
+namespace {
+
+    template <size_t N> struct some_work;
+
+    template <
+    const size_t K,
+    const size_t N,
+    typename     T,
+    typename     FUNC>
+    struct doWork
+    {
+        static inline void run( T *a, FUNC &proc )
+        {
+            doWork<K-1,N,T,FUNC>::run(a,proc);
+            const size_t i = some_work<N>::I[K];
+            std::cerr << "\tI[" << K << "]=" << i << std::endl;
+            proc( a[i] );
+        }
+    };
+
+    template <
+    const size_t N,
+    typename     T,
+    typename     FUNC>
+    struct doWork<0,N,T,FUNC>
+    {
+        static inline void run( T *a, FUNC &proc )
+        {
+            const size_t i = some_work<N>::I[0];
+            std::cerr << "\tI[" << 0 << "]=" << i << std::endl;
+            proc( a[i] );
+        }
+    };
+
+
+
+    template <> struct some_work<1>
+    {
+        static const size_t I[4];
+
+        template <typename T, typename FUNC>
+        static inline void on( T *a, FUNC &proc )
+        {
+            std::cerr << "some_work<1>" << std::endl;
+            doWork<sizeof(I)/sizeof(I[0])-1,1,T,FUNC>::run(a,proc);
+        }
+    };
+
+    const size_t some_work<1>::I[4] =
+    {
+        0,0,0,0
+    };
+
+
+    template <> struct some_work<3>
+    {
+        static const size_t I[5];
+
+        template <typename T, typename FUNC>
+        static inline void on( T *a, FUNC &proc )
+        {
+            std::cerr << "some_work<3>" << std::endl;
+            doWork<sizeof(I)/sizeof(I[0])-1,3,T,FUNC>::run(a,proc);
+        }
+    };
+
+    const size_t some_work<3>::I[5] =
+    {
+        0,1,2,1,0
+    };
+
+
+
+    template <typename T>
+    void doProc( const T &a )
+    {
+        std::cerr << "\t-> " << a << std::endl;
+    }
+
+
+
+}
+#endif
+
+
 Y_UTEST(metaloop)
 {
     doLoops();
+
+    int a[8] = { 0,1,2,3,4,5,6,7 };
+    some_work<1>::on(a, doProc<int> );
+
+    some_work<3>::on(a, doProc<int> );
+
 }
 Y_UTEST_DONE()
 
---
+++ src/main/y/sort/nwsrt64.pl
@@ -0,0 +1,66 @@
+use Algorithm::Networksort;
+use Algorithm::Networksort::Best qw(:all);
+
+for (my $i = 2; $i <= 8; $i++) {
+    my $inputs = $i;
+    my $algorithm = "bosenelson";
+
+    #
+    # Generate the sorting network (a list of comparators).
+    #
+    my $nw = Algorithm::Networksort->new(inputs =>$inputs,algorithm => $algorithm);
+    #$nw->formats([ " swap(%d,%d)," ]);
+
+    #
+    # Print the comparator list using the default format,
+    # and print a graph of the list.
+    #
+    #print "$i", "\n";
+    print "$i-", $nw->title(), "\n";
+    print $nw->formatted(),"\n\n";
+
+}
+
+for (my $i = 9; $i <= 24; $i++)
+{
+    my $inputs = $i;
+
+    #
+    # First find if any networks exist for the size you want.
+    #
+    my @nwkeys = nw_best_names($inputs);
+
+    #
+    # For each sorting network, show the comparators.
+    #
+    for my $name (@nwkeys)
+    {
+        my $nw = nwsrt_best(name => $name);
+
+        #
+        # Print the list, and print the graph of the list.
+        #
+        print $nw->title(), "\n";
+        print $nw->formatted(),"\n\n";
+    }
+}
+
+for (my $i = 25; $i <= 64; $i++) {
+    my $inputs = $i;
+    my $algorithm = "bosenelson";
+
+    #
+    # Generate the sorting network (a list of comparators).
+    #
+    my $nw = Algorithm::Networksort->new(inputs =>$inputs,algorithm => $algorithm);
+    #$nw->formats([ " swap(%d,%d)," ]);
+
+    #
+    # Print the comparator list using the default format,
+    # and print a graph of the list.
+    #
+    #print "$i", "\n";
+    print "$i-", $nw->title(), "\n";
+    print $nw->formatted(),"\n\n";
+
+}
