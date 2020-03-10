85b86ecdde5c84ef68f63c6f15353ba2a136a23f
ybouret <yann.bouret@gmail.com>
2020-03-10 18:05:59 +0100

    WIP nothing...

--- src/main/tests/test-phrases.cpp
+++ src/main/tests/test-phrases.cpp
@@ -5,8 +5,56 @@
 #include "y/ios/icstream.hpp"
 #include "y/associative/suffix-tree.hpp"
 #include "y/os/progress.hpp"
+#include "y/core/node.hpp"
+#include "y/ptr/arc.hpp"
 
 using namespace upsylon;
+
+namespace {
+
+    typedef core::cpp_node_of<char>      char_type;
+    typedef core::list_of_cpp<char_type> char_list_;
+
+    class char_list : public char_list_, public counted_object
+    {
+    public:
+        inline  char_list() throw() {}
+
+        inline ~char_list() throw()
+        {
+        }
+
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(char_list);
+    };
+
+    typedef arc_ptr<char_list> shared_list;
+
+    static inline
+    void split( const shared_list &source )
+    {
+        for(size_t i=1;i<source->size;++i)
+        {
+            shared_list head = new char_list();
+            shared_list tail = new char_list();
+            const char_type *ch = source->head;
+            for(size_t j=1;j<=i;++j,ch=ch->next)
+            {
+                head->push_back( new char_type( **ch ) );
+            }
+            
+            for(ch=ch->next;ch;ch=ch->next)
+            {
+                tail->push_back( new char_type( **ch ) );
+            }
+        }
+    }
+
+
+
+
+
+}
 
 Y_UTEST(phrases)
 {
@@ -21,22 +69,29 @@ Y_UTEST(phrases)
         progress          bar;
         bar.start();
         double old = bar.query();
-        for( perm.start(); perm.valid(); perm.next())
+        for( perm.start(); perm.valid(); perm.next() )
         {
-            if( bar.query() - old >= 0.5 )
-            {
-                bar.update(perm.index, perm.count);
-                bar.display(std::cerr) << '\r';
-                old = bar.query();
-            }
+
             perm.apply(*motif,*alphabet);
             if(!dict.insert_by(motif,true))
             {
                 continue;
             }
 
-            
+            if( bar.query() - old >= 0.5 )
+            {
+                bar.update(perm.index, perm.count);
+                bar.display(std::cerr) << '\r';
+                old = bar.query();
+            }
 
+            shared_list source = new char_list();
+            for(size_t i=0;i<letters;++i)
+            {
+                source->push_back( new char_type(motif[i]) );
+            }
+
+            split(source);
 
         }
         bar.update(1.0);
@@ -44,44 +99,6 @@ Y_UTEST(phrases)
         std::cerr << "found [" << dict.entries() << "/" << perm.count << "]" << std::endl;
 
 
-#if 0
-        dict_type     dict( 32 * 1024, as_capacity );
-
-        if( argc > 2 )
-        {
-
-            {
-                ios::icstream fp( argv[2] );
-                string        line;
-                while( fp.gets(line) )
-                {
-                    dict.insert(line);
-                }
-            }
-            std::cerr << "#dict=" << dict.size() << std::endl;
-        }
-
-        const bool    hasDict = (dict.size() > 0);
-        if(hasDict) std::cerr << "use dictionary..." << std::endl;
-        permutation   perm(letters);
-        string        motif( letters, as_capacity, true);
-
-        db_type motifs( perm.count, as_capacity );
-        std::cerr << "studying #perm=" << perm.count << " [";
-        for( perm.start(); perm.valid(); perm.next() )
-        {
-            perm.apply(*motif,*alphabet);
-            if( !motifs.insert(motif,motif) ) continue;
-            const size_t n = motifs.size();
-            if( 0 == (n%1000) )
-            {
-                ( std::cerr << '.' ).flush();
-            }
-            //std::cerr << "[" << motif << "]" << std::endl;
-        }
-
-        std::cerr << "]\n[" << motifs.size() << "/" << perm.count << "]" << std::endl;
-#endif
 
 
     }
