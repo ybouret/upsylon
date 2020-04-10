f97a04859576668bbf965779445bfdde8b732330
ybouret <yann.bouret@gmail.com>
2020-04-10 08:09:48 +0200

    WIP grammar

--- src/jargon/y/jargon/grammar/load.cpp
+++ src/jargon/y/jargon/grammar/load.cpp
@@ -12,10 +12,15 @@ namespace upsylon {
         {
             static const char fn[] = "loadAxiom";
             assert(which);
+            // read the label name
             size_t        shift  = 0;
             const string  id     = string::read(fp, shift,which); r += shift;
+            
+            // find the associatied dogma
             const Dogma  *dogma  = axioms.search_by(id);
             if(!dogma) throw exception("%s.%s(unknown %s <%s>)", **title,fn, which, *id);
+           
+            // return the axiom
             return **dogma;
         }
         
@@ -25,28 +30,68 @@ namespace upsylon {
         {
             static const char fn[] = "loadTree";
             
+            //------------------------------------------------------------------
+            //
+            // read the prolog
+            //
+            //------------------------------------------------------------------
+
             char    C = 0;
             int    &r = aliasing::_(where.column);
-
-            if(!input.query(C)) throw exception("%s.%s(unexpected end of stream)", **title, fn);
             
+            if(!input.query(C)) throw exception("%s.%s(unexpected end of stream)", **title, fn);
             ++r;
+            
+            
             switch(C)
             {
-                case XNode::InactiveMark: return xcache.query();
+                case XNode::InactiveMark:
+                    //----------------------------------------------------------
+                    //
+                    //
+                    // legal but useless inactive
+                    //
+                    //
+                    //----------------------------------------------------------
+                    return xcache.query();
                     
                 case XNode::TerminalMark:
                 {
+                    //----------------------------------------------------------
+                    //
+                    //
+                    // get/create a terminal
+                    //
+                    //
+                    //----------------------------------------------------------
+
+                    //----------------------------------------------------------
+                    //
                     // get the axiom
+                    //
+                    //----------------------------------------------------------
                     const Axiom     &axiom = loadAxiom(input, r, "Terminal Label");
                     
+                    //----------------------------------------------------------
+                    //
                     // get the terminal
+                    //
+                    //----------------------------------------------------------
                     if(Terminal::UUID!=axiom.uuid) throw exception("%s.%s( <%s> is not a Terminal!)",**title,fn,**(axiom.label) );
                     const Terminal &t = axiom.as<Terminal>();
                     
-                    
-                    // read the lexeme
+                    //----------------------------------------------------------
+                    //
+                    // create the lexeme
+                    //
+                    //----------------------------------------------------------
                     auto_ptr<Lexeme> unit = new Lexeme(cache,where,t.label);
+                    
+                    //----------------------------------------------------------
+                    //
+                    // read the lexeme #chars
+                    //
+                    //----------------------------------------------------------
                     size_t           nch  = 0;
                     {
                         size_t          shift = 0;
@@ -55,6 +100,11 @@ namespace upsylon {
                         r+=shift;
                     }
                     
+                    //----------------------------------------------------------
+                    //
+                    // load the content
+                    //
+                    //----------------------------------------------------------
                     for(size_t i=1;i<=nch;++i)
                     {
                         if( !input.query(C))
@@ -68,22 +118,48 @@ namespace upsylon {
                     return xcache.query(t, unit.yield() );
                     
                 }
+                    
+                    
                 case XNode::InternalMark:
                 {
+                    //----------------------------------------------------------
+                    //
+                    //
+                    // get/create an internal node
+                    //
+                    //
+                    //----------------------------------------------------------
+
+                    //----------------------------------------------------------
+                    //
                     // load the axiom
+                    //
+                    //----------------------------------------------------------
                     const Axiom &axiom = loadAxiom(input, r, "Internal Label");
                     
                     
+                    //----------------------------------------------------------
+                    //
                     // get the aggregate
+                    //
+                    //----------------------------------------------------------
                     if(Aggregate::UUID!=axiom.uuid) throw exception("%s.%s( <%s> is not an Aggregrate!)",**title,fn,**(axiom.label) );
                     
                     const Aggregate &internal = axiom.as<Aggregate>();
                     if(Aggregate::Design == internal.feature) throw exception("%s.%s( <%s> is a Design Aggregrate!)",**title,fn,**(axiom.label) );
                    
+                    //----------------------------------------------------------
+                    //
                     // get the xnode
+                    //
+                    //----------------------------------------------------------
                     auto_ptr<XNode> xnode = xcache.query(internal);
                     
-                    // read the children
+                    //----------------------------------------------------------
+                    //
+                    // read the #children
+                    //
+                    //----------------------------------------------------------
                     size_t          nch   = 0;
                     {
                         size_t          shift = 0;
@@ -92,6 +168,11 @@ namespace upsylon {
                         r+=shift;
                     }
 
+                    //----------------------------------------------------------
+                    //
+                    // load all children
+                    //
+                    //----------------------------------------------------------
                     while(nch-- > 0)
                     {
                         xnode->children().push_back( loadTree(where,cache,input) );
