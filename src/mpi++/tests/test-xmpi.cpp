7d6d3c32dd30cccae1967b7508f80f5861f4e703
ybouret <yann.bouret@gmail.com>
2020-05-12 16:00:50 +0200

    WIP mpi++

--- src/mpi++/tests/test-xmpi.cpp
+++ src/mpi++/tests/test-xmpi.cpp
@@ -5,53 +5,55 @@
 #include "y/sequence/list.hpp"
 using namespace  upsylon;
 
+namespace
+{
+    template <typename T>
+    static inline void doSequences(mpi &MPI)
+    {
+        vector<T> V;
+        list<T>   L;
+        if(MPI.head)
+        {
+            for(size_t i=alea.leq(10)+1;i>0;--i)
+            {
+                const T tmp = support::get<T>();
+                V.push_back( tmp );
+                L.push_back( V.back() );
+            }
+            for(int rank=1;rank<MPI.size;++rank)
+            {
+                XMPI::Send(MPI, rank, V);
+                XMPI::Send(MPI, rank, L);
+                
+                V.free();
+                L.free();
+                XMPI::Recv(MPI, rank, L);
+                XMPI::Recv(MPI, rank, V);
+            }
+        }
+        else
+        {
+            XMPI::Recv(MPI, 0, V);
+            XMPI::Recv(MPI, 0, L);
+            Y_ASSERT(V.size()== L.size());
+            
+            XMPI::Send(MPI, 0, V);
+            XMPI::Send(MPI, 0, L);
+        }
+    }
+    
+}
+
 Y_UTEST(xmpi)
 {
     Y_MPI(SINGLE);
     
     if(MPI.parallel)
     {
-        if(MPI.head)
-        {
-            vector<int> iv;
-            list<int>   il;
-            for(size_t i=alea.leq(10)+1;i>0;--i)
-            {
-                iv.push_back( alea.full<int>() );
-                il.push_back( iv.back() );
-            }
-            
-            vector<int> riv;
-            list<int>   ril;
-            
-            for(int rank=1;rank<MPI.size;++rank)
-            {
-                XMPI::Send(MPI, rank, iv);
-                XMPI::Send(MPI, rank, il);
-                
-                riv.free();
-                ril.free();
-                XMPI::Recv(MPI, rank, ril);
-                XMPI::Recv(MPI, rank, riv);
-            }
-            
-                     
-            Y_ASSERT(iv.size()== il.size());
-            Y_ASSERT(iv.size()==riv.size());
-            Y_ASSERT(iv.size()==ril.size());
+        doSequences<int>(MPI);
+        doSequences<double>(MPI);
+        doSequences<string>(MPI);
 
-        }
-        else
-        {
-            vector<int> iv;
-            list<int>   il;
-            XMPI::Recv(MPI, 0, iv);
-            XMPI::Recv(MPI, 0, il);
-            Y_ASSERT(iv.size()== il.size());
-
-            XMPI::Send(MPI, 0, iv);
-            XMPI::Send(MPI, 0, il);
-        }
         
         MPI.Printf(stderr, "send: %lu | recv: %lu\n", (unsigned long) MPI.commSend.data.full, (unsigned long) MPI.commRecv.data.full );
     }
