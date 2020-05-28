9f43cc93cd02c6d699c5174d5a9161726b9c173e
ybouret <yann.bouret@gmail.com>
2020-05-28 16:41:07 +0200

    typos

--- src/main/tests/test-tensors.cpp
+++ src/main/tests/test-tensors.cpp
@@ -1,6 +1,7 @@
 #include "y/tensor/tensor3d.hpp"
 #include "y/utest/run.hpp"
 #include "support.hpp"
+#include "y/type/spec.hpp"
 
 using namespace upsylon;
 
@@ -10,18 +11,21 @@ namespace {
     template <typename T>
     static inline void doTest()
     {
+        std::cerr << std::endl;
+        const string &id = type_name_of<T>();
+        
         tensor1d<T> t1( 1+alea.leq(10) );
         support::fill1D(t1);
-        std::cerr << "tensor1d.bytes=" << t1.allocated() << std::endl;
+        std::cerr << "tensor1d<" << id << ">.bytes=" << t1.allocated() << std::endl;
         
         tensor2d<T> t2( 1+alea.leq(10), 1+alea.leq(10) );
         support::fill2D(t2);
-        std::cerr << "tensor2d.bytes=" << t2.allocated() << std::endl;
+        std::cerr << "tensor2d<" << id << ">.bytes=" << t2.allocated() << std::endl;
 
         
         tensor3d<T> t3( 1+alea.leq(10), 1+alea.leq(10), 1+alea.leq(10) );
         support::fill3D(t3);
-        std::cerr << "tensor3d.bytes=" << t3.allocated() << std::endl;
+        std::cerr << "tensor3d<" << id << ">.bytes=" << t3.allocated() << std::endl;
 
     }
 }
