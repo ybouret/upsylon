45b968e5d1aecbb2c7c5653458e289dc4f5be382
ybouret <yann.bouret@gmail.com>
2020-06-01 16:55:35 +0200

    typo

--- src/mpi++/tests/test-init.cpp
+++ src/mpi++/tests/test-init.cpp
@@ -92,8 +92,8 @@ Y_UTEST(init)
     
     MPI.sequential(DoSomethingWith);
     fflush(stderr);
-    Y_MPI_HEAD( std::cerr << "Will Execute Node Code" << std::endl );
-    Y_MPI_NODE( std::cerr << "Now Sequential @" << MPI.nodeName  << std::endl );
+    Y_MPI_HEAD( std::cerr << "Will Execute Node Code" << std::endl; std::cerr.flush() );
+    Y_MPI_NODE( std::cerr << "Now Sequential @" << MPI.nodeName  << std::endl; std::cerr.flush() );
     
 }
 Y_UTEST_DONE()
