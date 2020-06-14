5ec955a8599a3236b2480880b14dafe5c757d5c9
ybouret <yann.bouret@gmail.com>
2020-06-14 15:11:47 +0200

    updated docs

--- src/main/y/type/points.hpp
+++ src/main/y/type/points.hpp
@@ -10,12 +10,15 @@ namespace upsylon {
     //! point operations
     struct __point
     {
+        //! norm2 in 1D
         template <typename T> static inline
         T norm2(const T a) throw() { return a*a; }
         
+        //! norm2 in 2D
         template <typename T> static inline
         T norm2(const point2d<T> &a) throw() { return a.norm2(); }
         
+        //! norm2 in 3D
         template <typename T> static inline
         T norm2(const point3d<T> &a) throw() { return a.norm2(); }
         
