d9f1e343a5e3fcdccf528b9df87f8efbec7e9a5d
ybouret <yann.bouret@gmail.com>
2020-06-18 21:22:03 +0200

    need to rationalize counting

--- src/main/tests/test-counting.cpp
+++ src/main/tests/test-counting.cpp
@@ -1,5 +1,6 @@
 #include "y/counting/mloop.hpp"
 #include "y/counting/comb.hpp"
+#include "y/counting/perm.hpp"
 #include "y/type/point3d.hpp"
 
 #include "y/utest/run.hpp"
@@ -12,6 +13,9 @@ Y_UTEST(counting)
         const point3d<int> lo(1,2,3);
         const point3d<int> up(4,5,6);
         mloop< int,point3d<int> > loop(lo,up);
+        std::cerr << "loop: index=" << loop.index << std::endl;
+        std::cerr << "loop: count=" << loop.count << std::endl;
+
         std::cerr << "loop: from " << loop.lower << " to " << loop.upper << " : " << loop.count << std::endl;
         std::cerr << "      curr:" << *loop << " @index=" << loop.index << std::endl;
         loop.boot();
@@ -19,6 +23,15 @@ Y_UTEST(counting)
     }
 
     {
+        combination comb(6,2);
+        std::cerr << "comb: index=" << comb.index <<  " : " << comb << std::endl;
+        std::cerr << "comb: count=" << comb.count << std::endl;
+    }
+    
+    {
+        permutation perm(6);
+        std::cerr << "perm: index=" << perm.index << " : " << perm << std::endl;
+        std::cerr << "perm: count=" << perm.count << std::endl;
     }
 }
 Y_UTEST_DONE()
