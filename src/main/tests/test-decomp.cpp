b68fb55c114900c38d572789e0d18ed127bf30d5
ybouret <yann.bouret@gmail.com>
2020-06-24 09:08:56 +0200

    WIP decompistion

--- src/main/tests/test-decomp.cpp
+++ src/main/tests/test-decomp.cpp
@@ -1,6 +1,7 @@
 #include "y/code/decomp.hpp"
 #include "y/utest/run.hpp"
 #include "y/mpl/rational.hpp"
+#include "y/type/spec.hpp"
 
 using namespace upsylon;
 
@@ -10,14 +11,38 @@ namespace {
     static inline
     void doDecomp()
     {
+        std::cerr << "using <" << type_name_of<T>() << ">" << std::endl;
         typename decompose<T>::plist l;
-
+        for(T n=1;n<=20;++n)
+        {
+            std::cerr << "\tdecomp(" << n << ")\t";
+            decompose<T>::build(l,n);
+            for(typename decompose<T>::pnode *node=l.head;node;node=node->next)
+            {
+                std::cerr << " " << node->x << "*" << node->y << ":" << node->d;
+            }
+            decompose<T>::clear(l);
+            std::cerr << " => ";
+            for(typename decompose<T>::pnode *node=l.head;node;node=node->next)
+            {
+                std::cerr << " " << node->x << "*" << node->y << ":" << node->d;
+            }
+            T x,y;
+            decompose<T>::square(x,y,n);
+            std::cerr << " => " << x << "," << y;
+            std::cerr << std::endl;
+        }
     }
 }
 
 Y_UTEST(decomp)
 {
+    doDecomp<int16_t>();
     doDecomp<int>();
+    doDecomp<uint64_t>();
+    doDecomp<mpn>();
+    doDecomp<mpz>();
+    doDecomp<mpq>();
 }
 Y_UTEST_DONE()
 
--- src/main/y/code/decomp.hpp
+++ src/main/y/code/decomp.hpp
@@ -4,49 +4,106 @@
 #define Y_CODE_DECOMP_INCLUDED 1
 
 #include "y/core/inode.hpp"
-#include "y/core/list.hpp"
 #include "y/type/args.hpp"
 #include "y/object.hpp"
+#include "y/sort/merge.hpp"
+#include "y/comparison.hpp"
 
 namespace upsylon {
 
+    //--------------------------------------------------------------------------
+    //
+    //! decomposition of one 'integral' type into a product
+    //
+    //--------------------------------------------------------------------------
     template <typename T>
     struct decompose {
-
+        //______________________________________________________________________
+        //
+        //! aliases
+        //______________________________________________________________________
         Y_DECL_ARGS(T,type);
 
+        //______________________________________________________________________
+        //
+        //! node of points
+        //______________________________________________________________________
         class pnode : public object, public inode< pnode >
         {
         public:
-            typedef core::list_of_cpp<pnode> list;
-            
+
             //! setup
             inline explicit pnode(param_type X, param_type Y) :
-            x(X), y(Y) { assert(x<=y); }
+            x(X), y(Y), d(x<=y?y-x:x-y)
+            {}
 
             //! cleanup
             inline virtual ~pnode() throw() {}
 
-            //! x/y value
-            const_type x;
-            const_type y;
+            const_type x; //!< x<=y
+            const_type y; //!< x<=y
+            const_type d; //!< |x-y|
+
+            //! increasing indicator
+            static inline int compare(const pnode *lhs, const pnode *rhs, void *)
+            {
+                return comparison::increasing(lhs->d,rhs->d);
+            }
 
         private:
             Y_DISABLE_COPY_AND_ASSIGN(pnode);
         };
 
+        //______________________________________________________________________
+        //
+        //! list of values
+        //______________________________________________________________________
         typedef core::list_of_cpp<pnode> plist;
 
-        //! all x<=y, x*y = z
-        static inline void build( plist &l, param_type z )
+        //______________________________________________________________________
+        //
+        //! build all x<=y, x*y = z
+        /**
+         ranked in "less square" order
+         */
+        //______________________________________________________________________
+        static inline void build(plist &l, param_type z)
         {
+            l.release();
             for(mutable_type i=1;i<=z;++i)
             {
-                for(mutable_type j=i+1;j<=z;++j)
+                for(mutable_type j=i;j<=z;++j)
                 {
                     if(i*j==z) l.push_back( new pnode(i,j) );
                 }
             }
+            merging<pnode>::sort(l,pnode::compare,NULL);
+        }
+
+        //______________________________________________________________________
+        //
+        //! keep only most square(d) solution after build
+        //______________________________________________________________________
+        static inline void clear(plist &l) throw()
+        {
+            while(l.size>1)
+            {
+                if(l.tail->prev->d<=l.tail->d) delete l.pop_back();
+            }
+        }
+
+        //______________________________________________________________________
+        //
+        //! all in one function keep most squared decomposition
+        //______________________________________________________________________
+        static inline
+        void square(mutable_type &x, mutable_type &y, param_type z)
+        {
+            plist l;
+            build(l,z); assert(l.size>=1 || die("bad build"));
+            clear(l);   assert(l.size>=1 || die("bad clear"));
+            x=l.head->x;
+            y=l.head->y;
         }
 
 
