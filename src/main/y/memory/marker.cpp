be0adfbb92d043082b71bcbd2db87801bd21ca9c
ybouret <yann.bouret@gmail.com>
2020-07-02 15:48:26 +0200

    marker

--- src/main/y/memory/marker.cpp
+++ src/main/y/memory/marker.cpp
@@ -2,5 +2,39 @@
 
 namespace upsylon {
 
+    namespace memory {
+
+        marker:: marker() throw() : offset(0), length(0) {}
+
+        marker:: ~marker() throw() { offset=length=0; }
+
+        marker:: marker(const marker &_) throw() : offset(_.offset), length(_.length) {}
+
+        marker & marker:: operator=(const marker &_) throw()
+        {
+            offset = _.offset;
+            length = _.length;
+            return *this;
+        }
+
+        marker:: marker(const size_t _off, const size_t _len) throw() : offset(_off), length(_len) {}
+
+
+    }
 }
 
+#include "y/code/round.hpp"
+
+namespace upsylon {
+
+    namespace memory {
+
+        marker:: marker(const marker &origin, const size_t delta ) throw() :
+        offset( align( origin.offset+origin.length) ),
+        length( delta )
+        {
+        }
+
+    }
+
+}
--- src/main/y/memory/marker.hpp
+++ src/main/y/memory/marker.hpp
@@ -3,7 +3,7 @@
 #ifndef Y_MEMORY_MARKER_INCLUDED
 #define Y_MEMORY_MARKER_INCLUDED 1
 
-#include "y/os/platform.hpp"
+#include "y/parops.hpp"
 
 namespace upsylon {
 
@@ -15,8 +15,19 @@ namespace upsylon {
             size_t offset;
             size_t length;
 
-            
+            marker() throw();
+            ~marker() throw();
+            marker(const marker&) throw();
+            marker & operator=(const marker &) throw();
+            marker(const size_t,const size_t) throw();
+            marker(const marker &, const size_t t) throw(); //!< automatic offset
 
+            template <typename T>
+            marker & split(const T size, const T rank) throw()
+            {
+                parops::split_any(length, offset, size, rank);
+                return *this;
+            }
 
         };
 
