b8085828831eb47e56999528155480c7f854b9d7
ybouret <yann.bouret@gmail.com>
2020-07-07 09:43:51 +0200

    typos

--- src/main/tests/test-triplet.cpp
+++ src/main/tests/test-triplet.cpp
@@ -11,36 +11,39 @@ namespace
     static inline
     void do_test()
     {
-        triplet<T> x = { support::get<T>(), support::get<T>(), support::get<T>() };
-        triplet<T> y = { support::get<T>(), support::get<T>(), support::get<T>() };
-        std::cerr << "x_raw=" << x << std::endl;
-        x.sort();
-        std::cerr << "x_srt=" << x << std::endl;
-        std::cerr << "y_raw=" << y << std::endl;
-        y.co_sort(x);
-        std::cerr << "y_co_srt=" << y << std::endl;
-        std::cerr << "x_co_srt=" << x << std::endl;
-
-        for(size_t i=1;i<=3;++i)
+        for(unsigned iter=0;iter<512;++iter)
         {
-            std::cerr << "x[" << i << "]=" << x[i] << std::endl;
-        }
+            triplet<T> x = { support::get<T>(), support::get<T>(), support::get<T>() };
+            triplet<T> y = { support::get<T>(), support::get<T>(), support::get<T>() };
+            std::cerr << "x_raw=" << x << std::endl;
+            x.sort();
+            std::cerr << "x_srt=" << x << std::endl;
+            std::cerr << "y_raw=" << y << std::endl;
+            y.co_sort(x);
+            std::cerr << "y_co_srt=" << y << std::endl;
+            std::cerr << "x_co_srt=" << x << std::endl;
 
-        std::cerr << "size=" << x.size() << std::endl;
+            for(size_t i=1;i<=3;++i)
+            {
+                std::cerr << "x[" << i << "]=" << x[i] << std::endl;
+            }
 
-        triplet<T> u;
-        u.ld(x);
-        std::cerr << "x=" << x << std::endl;
-        std::cerr << "u=" << u << std::endl;
-        Y_CHECK(x.diff2(u)<=0);
-        for(size_t i=1;i<=3;++i)
-        {
-            u[i] = support::get<T>();
+            std::cerr << "size=" << x.size() << std::endl;
+
+            triplet<T> u;
+            u.ld(x);
+            std::cerr << "x=" << x << std::endl;
+            std::cerr << "u=" << u << std::endl;
+            Y_CHECK(x.diff2(u)<=0);
+            for(size_t i=1;i<=3;++i)
+            {
+                u[i] = support::get<T>();
+            }
+            x.ld(u);
+            std::cerr << "x=" << x << std::endl;
+            std::cerr << "u=" << u << std::endl;
+            Y_CHECK(x.diff2(u)<=0);
         }
-        x.ld(u);
-        std::cerr << "x=" << x << std::endl;
-        std::cerr << "u=" << u << std::endl;
-        Y_CHECK(x.diff2(u)<=0);
 
     }
 }
