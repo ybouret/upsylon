3b04011bd0724804d0d7d2f12f973366eba835f5
ybouret <yann.bouret@gmail.com>
2020-07-07 16:25:39 +0200

    testing...

--- src/main/tests/test-ints-to-real.cpp
+++ src/main/tests/test-ints-to-real.cpp
@@ -1,6 +1,7 @@
 
 #include "y/type/ints-to-real.hpp"
 #include "y/utest/run.hpp"
+#include "y/utest/timings.hpp"
 #include "y/type/spec.hpp"
 #include <cfloat>
 
@@ -111,6 +112,30 @@ template <typename T> static inline void display_i2r()
     std::cerr << std::endl;
 }
 
+static float        ftable[384] = { 0 };
+static const float *fentry      = &ftable[128];
+
+
+static inline void doCast()
+{
+    for(int i=-128;i<=255;++i)
+    {
+        volatile float f = static_cast<float>(i);
+        (void)f;
+    }
+}
+
+static inline void doCall()
+{
+    for(int i=-128;i<=255;++i)
+    {
+        volatile float f = fentry[i];
+        (void)f;
+    }
+}
+
+#include "y/ios/ocstream.hpp"
+
 Y_UTEST(i2r)
 {
     std::cerr << "Radix:" << std::endl;
@@ -154,6 +179,57 @@ Y_UTEST(i2r)
     std::cerr << std::endl;
     display_i2r<float>();
     display_i2r<double>();
+
+    std::cerr << "int8_t::min   = " << int(signed_int<1>::minimum)   << std::endl;
+    std::cerr << "int8_t::max   = " << int(signed_int<1>::maximum)   << std::endl;
+
+    std::cerr << "uint8_t::min  = " << int(unsigned_int<1>::minimum) << std::endl;
+    std::cerr << "uint8_t::max  = " << int(unsigned_int<1>::maximum) << std::endl;
+
+    std::cerr << "int16_t::min  = " << int(signed_int<2>::minimum)   << std::endl;
+    std::cerr << "int16_t::max  = " << int(signed_int<2>::maximum)   << std::endl;
+
+    std::cerr << "uint16_t::min = " << int(unsigned_int<2>::minimum) << std::endl;
+    std::cerr << "uint16_t::max = " << int(unsigned_int<2>::maximum) << std::endl;
+
+    if(false)
+    {
+        ios::ocstream fp("byte2float.dat");
+        int count = 0;
+        for(int i=-128;i<=255;++i)
+        {
+            fp("%d.0f",i);
+            if(i<255) fp << ',';
+            if( 0 == (i%8) ) fp << '\n';
+            ++count;
+        }
+        std::cerr << "count=" << count << std::endl;
+    }
+
+    {
+        float *p = ftable;
+        for(int i=-128;i<=255;++i)
+        {
+            *(p++) = float(i);
+        }
+        Y_CHECK( fabsf(fentry[0]) <= 0);
+        for(int i=-128;i<=255;++i)
+        {
+            Y_ASSERT( int( fentry[i] ) == i );
+        }
+    }
+
+    std::cerr << "casting..." << std::endl;
+    const double duration   = 2;
+    double       cast_speed = 0;
+    Y_TIMINGS(cast_speed,duration,doCast());
+    std::cerr << "cast_speed=" << cast_speed << std::endl;
+
+    double call_speed = 0;
+    Y_TIMINGS(call_speed,duration,doCall());
+    std::cerr << "call_speed=" << call_speed << std::endl;
+
+
 
 
 }
