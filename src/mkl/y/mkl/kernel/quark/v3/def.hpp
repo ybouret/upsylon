cdb475e30359ffe0afd920dbe80de9c7b50327ab
ybouret <yann.bouret@gmail.com>
2020-07-08 07:15:32 +0200

    typos

--- src/mkl/y/mkl/kernel/quark/v3/def.hpp
+++ src/mkl/y/mkl/kernel/quark/v3/def.hpp
@@ -2,24 +2,24 @@
 //! \file
 
 //! parallel code helper for matrices
-#define Y_QUARK_MMUL_PAR_ENTER()   \
-Y_QUARK_TASK_DECL()                \
-matrix<T>       *A;                \
-const matrix<U> *B;                \
-const matrix<V> *C;                \
-Y_QUARK_TASK_IMPL()                \
-matrix<T>       &A      = *task.A; \
-const matrix<U> &B      = *task.B; \
-const matrix<V> &C      = *task.C; \
-size_t           length = A.items; \
-size_t           offset = 0;       \
-ctx.split(length,offset)
+#define Y_QUARK_MMUL_PAR_ENTER()           \
+/**/Y_QUARK_TASK_DECL()                    \
+/**/matrix<T>       *A;                    \
+/**/const matrix<U> *B;                    \
+/**/const matrix<V> *C;                    \
+/**/Y_QUARK_TASK_IMPL()                    \
+/**/    matrix<T>       &A      = *task.A; \
+/**/    const matrix<U> &B      = *task.B; \
+/**/    const matrix<V> &C      = *task.C; \
+/**/    size_t           length = A.items; \
+/**/    size_t           offset = 0;       \
+/**/    ctx.split(length,offset)
 
 //! parallel code helper for matrices
-#define Y_QUARK_MMUL_PAR_LEAVE() \
-Y_QUARK_TASK_DATA()              \
-&A, &B, &C                       \
-Y_QUARK_TASK_EXEC(loop)
+#define Y_QUARK_MMUL_PAR_LEAVE()           \
+/**/Y_QUARK_TASK_DATA()                    \
+/**/    &A, &B, &C                         \
+/**/Y_QUARK_TASK_EXEC(loop)
 
 //! parallel code helper for matrices : OFFSET to row/col
 #define Y_QUARK_MMUL_RC(OFFSET) \
