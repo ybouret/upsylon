f1d33fb41559839a9f60fb4e5c84164af0c16bb4
ybouret <yann.bouret@gmail.com>
2020-07-10 16:04:09 +0200

    typo

--- src/main/y/concurrent/executor.cpp
+++ src/main/y/concurrent/executor.cpp
@@ -1,5 +1,6 @@
 
 #include "y/concurrent/executor.hpp"
+#include "y/type/aliasing.hpp"
 
 namespace upsylon
 {
@@ -37,8 +38,8 @@ namespace upsylon
         const parallel & executor:: operator[](const size_t context_index) const throw()
         {
             assert(context_index<num_threads());
-            executor &self = (executor &) *this;
-            return self.get_context(context_index);
+            executor &self = aliasing::_( *this );
+            return    self.get_context(context_index);
         }
 
 
