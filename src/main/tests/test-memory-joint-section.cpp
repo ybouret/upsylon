ad0eef12e24be576fc3db45d2fdf5dfc7817aa58
ybouret <yann.bouret@gmail.com>
2020-08-11 11:03:47 +0200

    WIP section

--- src/main/tests/test-memory-joint-section.cpp
+++ src/main/tests/test-memory-joint-section.cpp
@@ -62,76 +62,46 @@ Y_UTEST(joint_section)
     std::cerr << std::endl;
 
     std::cerr << "-- multiple I/O" << std::endl;
-    for(int iter=0;iter<16;++iter)
+    list<block> L;
+    for(int iter=0;iter<32;++iter)
     {
         const size_t required = 100 + alea.leq(10000);
         size_t       shift    = 0;
         const size_t bytes    = joint::section::holding(required, shift);
         zblock<char,global>  buffer(bytes);
         joint::section S(buffer.rw(),bytes,shift);
-        std::cerr << required << " => section.size=" << S.size << "=2^" << S.exp2 << std::endl;
+        std::cerr << std::setw(6) << required << " => section.size=" << std::setw(6) << S.size << " = 2^" << std::setw(2) << S.exp2;
+        std::cerr.flush();
 
-
-        list<block> L;
-        while(true)
+        size_t maxBlocks=0;
+        std::cerr << ' ' << '[';
+        for(size_t cycle=1;cycle<=256;++cycle)
         {
-            block blk = { 0, alea.range<size_t>(0,3*joint::section::block::size) };
-            blk.addr  = S.acquire(blk.size);
-            if(!blk.addr)
+            if(0==(cycle%16)) std::cerr << '.';
+            assert(0==L.size());
+            while(true)
             {
-                break;
+                block blk = { 0, alea.range<size_t>(0,3*joint::section::block::size) };
+                blk.addr  = S.acquire(blk.size);
+                if(!blk.addr)
+                {
+                    break;
+                }
+                L.push_back(blk);
             }
-            L.push_back(blk);
-        }
-        std::cerr << "#blocks v1=" << L.size() << std::endl;
-
-
-        alea.shuffle(*L);
-        while(L.size())
-        {
-            block     &b = L.back();
-            Y_ASSERT( &S == memory::joint::section::release(b.addr,b.size) );
-            L.pop_back();
-        }
-        Y_ASSERT(S.is_empty());
-
-        while(true)
-        {
-            block blk = { 0, alea.range<size_t>(0,3*joint::section::block::size) };
-            blk.addr  = S.acquire(blk.size);
-            if(!blk.addr)
+            if(L.size()>maxBlocks) maxBlocks=L.size();
+            alea.shuffle(*L);
+            while(L.size())
             {
-                break;
+                block     &b = L.back();
+                Y_ASSERT( &S == memory::joint::section::release(b.addr,b.size) );
+                L.pop_back();
             }
-            L.push_back(blk);
+            Y_ASSERT(S.is_empty());
         }
-        std::cerr << "#blocks v2=" << L.size() << std::endl;
-        alea.shuffle(*L);
-        for(size_t i=L.size()/2;i>0;--i)
-        {
-            block     &b = L.back();
-            Y_ASSERT( &S == joint::section::release(b.addr,b.size) );
-            L.pop_back();
-        }
-        while(true)
-        {
-            block blk = { 0, alea.range<size_t>(0,3*memory::joint::section::block::size) };
-            blk.addr  = S.acquire(blk.size);
-            if(!blk.addr)
-            {
-                break;
-            }
-            L.push_back(blk);
-        }
-        std::cerr << "#blocks v3=" << L.size() << std::endl;
-        alea.shuffle(*L);
-        while(L.size())
-        {
-            block     &b = L.back();
-            Y_ASSERT( &S ==  joint::section::release(b.addr,b.size) );
-            L.pop_back();
-        }
-        Y_ASSERT(S.is_empty());
+        std::cerr << ']';
+        std::cerr << " max blocks = " << std::setw(4) << maxBlocks;
+        std::cerr << std::endl;
     }
 
     Y_UTEST_SIZEOF(joint::section::block);
