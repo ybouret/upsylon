9d05391a0b0333791ece83afc1e4448f05b0f50f
ybouret <yann.bouret@gmail.com>
2020-08-17 07:55:14 +0200

    yap

--- src/main/tests/test-yap-n.cpp
+++ src/main/tests/test-yap-n.cpp
@@ -8,7 +8,8 @@ Y_UTEST(yap_n)
 {
     std::cerr << "core_bits=" << number::core_bits << std::endl;
     std::cerr << "word_bits=" << number::word_bits << std::endl;
-
+    Y_CHECK(number::word_bits<=number::core_bits/2);
+    Y_CHECK((1<<number::word_exp2)==number::word_size);
 }
 Y_UTEST_DONE()
 
