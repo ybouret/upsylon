1ff38c1fdde3a8792666fa48fbf3d30ac8c38323
ybouret <yann.bouret@gmail.com>
2020-08-26 09:26:39 +0200

    WIP

--- src/main/tests/test-yap-pt.cpp
+++ src/main/tests/test-yap-pt.cpp
@@ -1,6 +1,7 @@
 
 #include "y/yap/prime/iterator.hpp"
 #include "y/yap/prime/miller.hpp"
+#include "y/yap/prime/sprp.hpp"
 
 #include "y/utest/run.hpp"
 #include "y/utest/sizeof.hpp"
@@ -24,14 +25,28 @@ namespace {
 Y_UTEST(yap_pt)
 {
     library          &apl = library::instance();
-    prime_iterator p(apl);
 
-    do {
+    if(argc>1)
+    {
+        for(int i=1;i<argc;++i)
+        {
+            const natural n = natural::parse(argv[i]);
+            const natural top = sprp::end(n);
+            std::cerr << "natural : " << n << std::endl;
+            std::cerr << "testing < " << top << std::endl;
+            std::cerr << "result  : " << Miller::is_prime(n) << std::endl;
+        }
+    }
+    else
+    {
+        prime_iterator p(apl);
+        do {
+            do_check(*p);
+            ++p;
+        } while(apl.size()<200);
         do_check(*p);
-        ++p;
-    } while(apl.size()<200);
-    do_check(*p);
-    std::cerr << std::endl;
+        std::cerr << std::endl;
+    }
 }
 Y_UTEST_DONE()
 
