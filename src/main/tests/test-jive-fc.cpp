773a27bb360bcb80f0090c0227bd0495fa3bbe32
ybouret <yann.bouret@gmail.com>
2020-08-31 15:41:06 +0200

    jive lead

--- src/main/tests/test-jive-fc.cpp
+++ src/main/tests/test-jive-fc.cpp
@@ -21,29 +21,36 @@ namespace {
     class Leading : public object
     {
     public:
-        Leading *next;
-        Leading *prev;
-
+        Leading      *next;
+        Leading      *prev;
+        const uint8_t kind;
+        static const unsigned Byte = 0x01;
+        static const unsigned Tier = 0x02;
         virtual ~Leading() throw()
         {
         }
 
 
-
         virtual Ownership whose(const uint8_t c) const throw() = 0;
         virtual Leading  *clone() const  = 0;
         virtual void      display( std::ostream &os ) const    = 0;
-
+        virtual const void *self() const throw() = 0;
 
         bool owns(const uint8_t c) const throw()
         {
             return OwnedByThis == whose(c);
         }
 
+        friend std::ostream & operator<<(std::ostream &os, const Leading &l)
+        {
+            l.display(os);
+            return os;
+        }
+
         typedef core::list_of_cloneable<Leading> List;
 
     protected:
-        explicit Leading() throw() : next(0), prev(0) {}
+        explicit Leading(const uint8_t k) throw() : next(0), prev(0), kind(k) {}
 
     private:
         Y_DISABLE_COPY_AND_ASSIGN(Leading);
@@ -53,7 +60,7 @@ namespace {
     {
     public:
         explicit LeadingByte(const uint8_t c) throw() :
-        Leading(),
+        Leading(Byte),
         code(c),
         priv(0)
         {
@@ -94,6 +101,10 @@ namespace {
             return new LeadingByte(code);
         }
 
+        const void *self() const throw()
+        {
+            return static_cast<const LeadingByte *>(this);
+        }
 
     private:
         Y_DISABLE_COPY_AND_ASSIGN(LeadingByte);
@@ -103,7 +114,7 @@ namespace {
     {
     public:
         explicit LeadingTier(const uint8_t lo, const uint8_t up) throw() :
-        Leading(),
+        Leading(Tier),
         lower(lo),
         upper(up)
         {
@@ -144,6 +155,10 @@ namespace {
             os << cchars::visible[lower] << '-' << cchars::visible[upper];
         }
 
+        const void *self() const throw()
+        {
+            return static_cast<const LeadingTier *>(this);
+        }
 
     private:
         Y_DISABLE_COPY_AND_ASSIGN(LeadingTier);
@@ -215,6 +230,7 @@ namespace {
 
             INSERTED:
             ++aliasing::_(size);
+            compact();
         }
 
         void display(std::ostream &os) const
@@ -226,7 +242,7 @@ namespace {
                 node->display(os);
                 for(node=node->next;node;node=node->next)
                 {
-                    node->display(os << ' ');
+                    node->display(os<<' ');
                 }
             }
             os << '}';
@@ -247,6 +263,73 @@ namespace {
             assert(0==lead.size);
             lead.push_back( new LeadingByte(c) );
             ++aliasing::_(size);
+        }
+
+
+#define Y_JIVE_LEADING_STATUS(A,B) ( (A<<8) | B )
+
+        void compact()
+        {
+            if(lead.size>1)
+            {
+                Leading::List temp;
+                temp.push_back( lead.pop_front() );
+
+                while(lead.size)
+                {
+                    const Leading *lhs = temp.tail;
+                    const Leading *rhs = lead.head;
+                    const unsigned Lk   = lhs->kind;
+                    const unsigned Rk   = rhs->kind;
+                    switch( Y_JIVE_LEADING_STATUS(Lk,Rk) )
+                    {
+                        case Y_JIVE_LEADING_STATUS(Leading::Byte,Leading::Byte):
+                        {
+                            const LeadingByte *L = static_cast<const LeadingByte *>(lhs->self());
+                            const LeadingByte *R = static_cast<const LeadingByte *>(rhs->self());
+                            assert(L->code<R->code);
+                            if(L->code+1==R->code)
+                            {
+                                const uint8_t lower = L->code;
+                                const uint8_t upper = R->code;
+                                Leading    *node = temp.pop_back();
+                                void       *addr = (void*)(node->self());
+                                node->~Leading();
+                                temp.push_back( new (addr) LeadingTier(lower,upper) );
+                                delete lead.pop_front();
+                                goto COMPACTED;
+                            }
+                        } break;
+
+                        case Y_JIVE_LEADING_STATUS(Leading::Byte,Leading::Tier):
+                        {
+                            const LeadingByte *L = static_cast<const LeadingByte *>(lhs->self());
+                            const LeadingTier *R = static_cast<const LeadingTier *>(rhs->self());
+                            assert(L->code<R->lower);
+                        } break;
+
+                        case Y_JIVE_LEADING_STATUS(Leading::Tier,Leading::Byte): {
+                            const LeadingTier *L = static_cast<const LeadingTier *>(lhs->self());
+                            const LeadingByte *R = static_cast<const LeadingByte *>(rhs->self());
+                            assert(L->upper<R->code);
+                        } break;
+
+                        case Y_JIVE_LEADING_STATUS(Leading::Tier,Leading::Tier): {
+                            const LeadingTier *L = static_cast<const LeadingTier *>(lhs->self());
+                            const LeadingTier *R = static_cast<const LeadingTier *>(rhs->self());
+                            assert(L->upper<R->lower);
+                        } break;
+
+                        default:
+                            throw exception("Jive::LeadingChars: corrupted!");
+                    }
+                    // no merge
+                    temp.push_back(lead.pop_front());
+                }
+            COMPACTED:
+                temp.merge_back(lead);
+                lead.swap_with(temp);
+            }
         }
 
 
