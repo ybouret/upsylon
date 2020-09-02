8671c9b0ffa08afbbe12ed1281a18c0234be90ca
Yann Bouret <yann.bouret@unice.fr>
2020-09-02 10:21:42 +0200

    jive

---
+++ src/jive/y/jive/lexical/rule.cpp
@@ -0,0 +1,33 @@
+
+
+
+#include "y/jive/lexical/rule.hpp"
+#include "y/exception.hpp"
+
+namespace upsylon {
+
+    namespace Jive {
+
+        namespace Lexical {
+
+            Rule:: ~Rule() throw()
+            {
+            }
+
+            Rule:: Rule(const Tag           &ruleLabel,
+                        const Motif         &ruleMotif,
+                        const Event::Handle &ruleEvent) :
+            Object(), inode<Rule>(),
+            label( ruleLabel ),
+            motif( ruleMotif ),
+            event( ruleEvent )
+            {
+                if( motif->feeble() )
+                {
+                    throw exception("Jargon::Rule <%s> has feeble motif", **ruleLabel);
+                }
+            }
+
+        }
+    }
+}
---
+++ src/jive/y/jive/lexical/rule.hpp
@@ -0,0 +1,49 @@
+
+//! \file
+
+#ifndef Y_JIVE_LEXICAL_RULE_INCLUDED
+#define Y_JIVE_LEXICAL_RULE_INCLUDED 1
+
+#include "y/jive/lexical/event.hpp"
+#include "y/jive/pattern.hpp"
+
+namespace upsylon {
+
+    namespace Jive {
+
+        namespace Lexical {
+
+            //------------------------------------------------------------------
+            //! base class for rules
+            /**
+             trigger and event uppon motif
+             */
+            //------------------------------------------------------------------
+
+            class Rule : public Object, public inode<Rule>
+            {
+            public:
+
+                const Tag           label; //!< shared label for Lexemes
+                const Motif         motif; //!< motif to match
+                const Event::Handle event; //!< event to trigger
+
+                //! cleanup
+                virtual ~Rule() throw();
+
+                //! setup
+                explicit Rule(const Tag           &ruleLabel,
+                              const Motif         &ruleMotif,
+                              const Event::Handle &ruleEvent);
+
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(Rule);
+            };
+
+            typedef core::list_of_cpp<Rule> Rules; //!< alias
+        }
+    }
+}
+
+#endif
+
