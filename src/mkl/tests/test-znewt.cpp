7bbdf190360f5e61f493d4cbe8b6e8beb2f68b62
Yann Bouret <yann.bouret@unice.fr>
2020-09-09 09:48:36 +0200

    znewt basic

--- src/mkl/tests/test-znewt.cpp
+++ src/mkl/tests/test-znewt.cpp
@@ -1,11 +1,71 @@
 
 #include "y/utest/run.hpp"
 #include "y/mkl/root/znewt.hpp"
+#include "y/mkl/fcn/djacobian.hpp"
+#include "y/sequence/vector.hpp"
 
 using namespace upsylon;
+using namespace mkl;
+
+static const double rho_s = 12.0192;
+static const double d7out = 14.57;
+static const double eps6  = 1.0/(1.0+rho_s*(1+1e-3*d7out));
+static const double eps7  = 1.0-eps6;
+static const double sigma = 1.003772;
+
+namespace {
+
+    struct mysys
+    {
+        double nu;
+        double d7ini;
+
+        inline void operator()( addressable<double> &F, const accessible<double> &X )
+        {
+            const double t2 = X[1];
+            const double tk = X[2];
+
+            const double t2p1  = 1.0 + t2;
+            const double s2    = t2/t2p1;
+            const double c2    = 1.0/t2p1;
+            const double kappa = 1.0 + tk / 1000.0;
+
+
+            F[1] = (1.0+1e-3*d7out)/(1.0+1e-3*d7ini) - (sigma*c2 + kappa*s2);
+            F[2] = (nu-1.0) - (kappa*eps6+eps7)/(sigma*eps6+eps7) * t2;
+        }
+
+    };
+
+
+}
 
 Y_UTEST(znewt)
 {
+
+    mysys                         mySys = { 30, 2 };
+    numeric<double>::vector_field f     = mySys;
+    derivative<double>::pointer   drvs  = new derivative<double>();
+    djacobian<double>             fjac(f,drvs);
+
+    vector<double> F(2,0);
+    vector<double> X(2,0);
+
+    // initialize
+    X[1] = 30;
+    X[2] = 10;
+    f(F,X);
+    znewt<double> solver;
+
+    //! run
+    while(!solver.step(F,X,f,fjac))
+    {
+
+    }
+    std::cerr << std::endl;
+    std::cerr << "kappa = " << 1.0 + X[2] / 1000.0 << std::endl;
+    std::cerr << "t2    = " << X[1] << std::endl;
+
 
 }
 Y_UTEST_DONE()
--- src/mkl/y/mkl/root/znewt.hpp
+++ src/mkl/y/mkl/root/znewt.hpp
@@ -6,6 +6,7 @@
 
 #include "y/mkl/fcn/jacobian.hpp"
 #include "y/mkl/kernel/lu.hpp"
+#include "y/mkl/kernel/quark.hpp"
 
 namespace upsylon
 {
@@ -15,6 +16,55 @@ namespace upsylon
         template <typename T>
         class znewt
         {
+        public:
+
+            typedef typename numeric<T>::vector_field field_type;
+
+
+            inline explicit znewt()
+            {
+            }
+
+            inline virtual ~znewt() throw()
+            {
+            }
+
+            //! start from f(F,X)
+            inline bool step(addressable<T> &F,
+                             addressable<T> &X,
+                             field_type     &f,
+                             jacobian<T>    &fjac )
+            {
+                static const T ftol = numeric<T>::ftol;
+                assert(F.size()==X.size());
+                const size_t n = X.size();
+                std::cerr << F << "@" << X;
+                J.make(n,n);
+                fjac(J,X);
+                if( !LU::build(J) ) return false;
+                array<T> &step = J.r_aux1; assert(step.size()==X.size());
+                quark::neg(step,F);
+                LU::solve(J,step);
+
+                bool converged = true;
+                for(size_t i=n;i>0;--i)
+                {
+                    const T x_old = X[i];
+                    const T x_new = (X[i]  += step[i]);
+                    const T dx    = fabs_of(x_new-x_old);
+                    if( dx > ftol * fabs_of(x_new) ) converged=false;
+                }
+
+                f(F,X);
+                std::cerr << " => " << F << "@" << X << std::endl;
+                return converged;
+            }
+
+            matrix<T> J;
+
+
+        private:
+
 
         };
 
