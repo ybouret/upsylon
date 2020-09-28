02b63265a817164cd2aa9d191b8470f1bce42cbd
Yann Bouret <yann.bouret@unice.fr>
2020-09-28 07:36:15 +0200

    wip

--- src/aqua/tests/test-species.cpp
+++ src/aqua/tests/test-species.cpp
@@ -1,8 +1,18 @@
 #include "y/aqua/species.hpp"
 #include "y/utest/run.hpp"
 
+using namespace upsylon;
+using namespace Aqua;
+
 Y_UTEST(species)
 {
+
+    Species::Pointer h = new Species("H+",1);
+    Species::Pointer w = new Species("HO-",-1);
+    std::cerr << h->name << std::endl;
+    std::cerr << w->name << std::endl;
+
+    h->make<double>( memory::storage::shared );
 
 }
 Y_UTEST_DONE()
--- src/aqua/y/aqua/species.hpp
+++ src/aqua/y/aqua/species.hpp
@@ -2,17 +2,34 @@
 #define Y_AQUA_SPECIES_INCLUDED 1
 
 #include "y/string.hpp"
+#include "y/memory/groove.hpp"
+#include "y/ptr/arc.hpp"
 
 namespace upsylon {
  
     namespace Aqua
     {
-        
-        class Species
+        typedef object         Object;
+        typedef counted        Counted;
+        typedef memory::groove Groove;
+
+        class Species : public Object, public Counted, public Groove
         {
         public:
+            typedef arc_ptr<Species> Pointer;
             virtual ~Species() throw();
-            
+
+            template <typename ID>
+            Species(const ID &id, const int z) :
+            Object(), Counted(), Groove(),
+            name(id),
+            Z(z)
+            {
+            }
+
+            const string name;
+            const int    Z;
+
         private:
             Y_DISABLE_COPY_AND_ASSIGN(Species);
         };
