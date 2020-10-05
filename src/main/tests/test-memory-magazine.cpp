703851d24d46dc09e499f080b25625ec06adbefd
Yann Bouret <yann.bouret@unice.fr>
2020-10-05 08:26:11 +0200

    WIP

--- src/main/tests/test-memory-magazine.cpp
+++ src/main/tests/test-memory-magazine.cpp
@@ -1,31 +1,56 @@
 
 #include "y/memory/magazine.hpp"
 #include "y/utest/run.hpp"
+#include "y/core/inode.hpp"
 
 using namespace upsylon;
 
 namespace {
 
-    class dummy
+    class simple
+    {
+    public:
+        static int count;
+        inline  simple() throw() { ++count; }
+        inline ~simple() throw() { --count; }
+        inline  simple(const simple &) throw() { ++count; }
+
+        typedef memory::magazine<simple> repo;
+
+    private:
+        Y_DISABLE_ASSIGN(simple);
+    };
+
+    int simple::count = 0;
+
+
+    class dummy : public inode<dummy>
     {
     public:
         int a;
-        inline   dummy() throw() : a(0) {}
+        inline   dummy() throw() : inode<dummy>(), a(0) {}
         inline   dummy(int x)    : a(x) { if(0==x) throw exception("dummy(0)"); }
-        inline  ~dummy() throw() {}
+        inline   dummy(const dummy &other) throw() : inode<dummy>(), a(other.a) {}
+        inline  virtual ~dummy() throw() {}
 
         typedef memory::magazine<dummy> repo;
 
+        typedef repo::list_ list;
+
     private:
-        Y_DISABLE_COPY_AND_ASSIGN(dummy);
+        Y_DISABLE_ASSIGN(dummy);
     };
+
+
 }
 
 namespace upsylon
 {
     namespace memory
     {
+        Y_SINGLETON_TEMPLATE_WITH(7,memory::magazine<simple>,simple:repo);
         Y_SINGLETON_TEMPLATE_WITH(0,memory::magazine<dummy>,dummy:repo);
+        
     }
 }
 
@@ -34,31 +59,62 @@ Y_UTEST(magazine)
 {
     concurrent::singleton::verbose = true;
 
-    dummy::repo &mgr = dummy::repo::instance();
-    std::cerr << "using " << mgr.call_sign << std::endl;
-
     {
-        dummy *d = mgr.acquire();
-        std::cerr << "a=" << d->a << std::endl;
-        mgr.release(d);
+        simple::repo &mgr = simple::repo::instance();
+        std::cerr << "using " << mgr.call_sign << std::endl;
+
+        simple *s = mgr.acquire();
+        simple *c = mgr.copycat(s);
+        mgr.release(s);
+        mgr.release(c);
+        Y_CHECK(simple::count==0);
     }
 
     {
-        dummy *d = mgr.acquire<int>(5);
-        std::cerr << "a=" << d->a << std::endl;
-        mgr.release(d);
-    }
+        dummy::repo &mgr = dummy::repo::instance();
+        std::cerr << "using " << mgr.call_sign << std::endl;
 
-    try
-    {
-        dummy *d = mgr.acquire<int>(0);
-        mgr.release(d);
-    }
-    catch(const exception &e)
-    {
-        std::cerr << "OK: " << e.when() << std::endl;
-    }
+        {
+            dummy *d = mgr.acquire();
+            std::cerr << "a=" << d->a << std::endl;
+            mgr.release(d);
+        }
 
+        {
+            dummy *d = mgr.acquire<int>(5);
+            std::cerr << "a=" << d->a << std::endl;
+            mgr.release(d);
+        }
+
+        try
+        {
+            dummy *d = mgr.acquire<int>(0);
+            mgr.release(d);
+        }
+        catch(const exception &e)
+        {
+            std::cerr << "OK: " << e.when() << std::endl;
+        }
+
+        {
+            dummy::list L;
+            for(size_t i=1000;i>0;--i)
+            {
+                if( alea.choice() )
+                {
+                    L.push_back( mgr.acquire<int>( alea.full<int>() ) );
+                }
+                else
+                {
+                    L.push_front( mgr.acquire<int>( alea.full<int>() ) );
+                }
+            }
+            dummy::list L2(L);
+
+        }
+
+        std::cerr << "#prefetched: " << mgr.prefetched() << std::endl;
+    }
 }
 Y_UTEST_DONE()
 
--- src/main/y/memory/magazine.hpp
+++ src/main/y/memory/magazine.hpp
@@ -3,23 +3,37 @@
 
 #include "y/memory/tight/supply.hpp"
 #include "y/concurrent/singleton.hpp"
+#include "y/ptr/ptr.hpp"
 
 namespace upsylon
 {
     
     namespace memory
     {
-        
+
+        //______________________________________________________________________
+        //
+        //
+        //! create specialized magazine for very dynamic same objects
+        //
+        //______________________________________________________________________
         template <typename T>
         class magazine : public singleton< magazine<T> >, public tight::supply_of<T>
         {
         public:
-            Y_DECL_ARGS(T,type);
+            //__________________________________________________________________
+            //
+            // types and definitions
+            //__________________________________________________________________
+            Y_DECL_ARGS(T,type);                              //!< aliases
+            typedef singleton< magazine<T> > singleton_type;  //!< alias
+            typedef tight::supply_of<T>      supply_type;     //!< alias
+            Y_SINGLETON_DECL(magazine);                       //!< prepare singleton
 
-            typedef singleton< magazine<T> > singleton_type;
-            typedef tight::supply_of<T>      supply_type;
-
-            Y_SINGLETON_DECL(magazine);
+            //__________________________________________________________________
+            //
+            // single object memory management
+            //__________________________________________________________________
 
             //! no arguments acquire
             inline type *acquire()
@@ -38,6 +52,25 @@ namespace upsylon
                 try { return new(ptr) type(argU); } catch(...) { this->zstore(ptr); throw; }
             }
 
+            //! two arguments acquire
+            template <typename U, typename V>
+            inline type *acquire(typename type_traits<U>::parameter_type argU,
+                                 typename type_traits<V>::parameter_type argV)
+            {
+                Y_LOCK(this->access);
+                mutable_type *ptr = this->zquery();
+                try { return new(ptr) type(argU,argV); } catch(...) { this->zstore(ptr); throw; }
+            }
+
+            //! copy another object
+            inline type *copycat(const_type *other)
+            {
+                Y_LOCK(this->access);
+                assert(NULL!=other);
+                mutable_type *ptr = this->zquery();
+                try { return new(ptr) type(*other); } catch(...) { this->zstore(ptr); throw; }
+            }
+
             //! release a previously aquired object
             inline void release(type *obj) throw()
             {
@@ -48,11 +81,62 @@ namespace upsylon
                 this->zstore(ptr);
             }
 
+            //__________________________________________________________________
+            //
+            // memory management
+            //__________________________________________________________________
+
+            //! prefetch extra blocks
             inline void reserve(const size_t n)
             {
                 Y_LOCK(this->access);
                 this->fetch(n);
             }
+
+            //__________________________________________________________________
+            //
+            //! base class for list
+            //__________________________________________________________________
+            typedef core::list_of<T> list__;
+
+            //__________________________________________________________________
+            //
+            //! specialized list
+            //__________________________________________________________________
+            class list_ : public list__, public releasable
+            {
+            public:
+                //! setup empty
+                inline explicit     list_()   throw() : list__() {}
+
+                //! cleanup
+                inline virtual     ~list_()   throw() { clr(); }
+
+                //! release all node
+                inline virtual void release() throw() { clr(); }
+
+                //! full copy
+                inline              list_(const list_ &other) : list__(), releasable()
+                {
+                    try {
+                        static magazine &mgr = magazine::instance();
+                        for(const_type *node=other.head;node;node=node->next)
+                        this->push_back( mgr.copycat(node) );
+                    }
+                    catch(...) { clr(); throw; }
+                }
+                
+            private:
+                Y_DISABLE_ASSIGN(list_);
+                inline void clr() throw() {
+                    while(this->size)
+                    {
+                        static magazine &mgr = magazine::location();
+                        mgr.release(this->pop_back());
+                    }
+                }
+            };
+
 
         private:
             Y_DISABLE_COPY_AND_ASSIGN(magazine);
