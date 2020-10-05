2d0aa2fe18b764eb05545096b63822db0c02e175
Yann Bouret <yann.bouret@unice.fr>
2020-10-05 07:10:07 +0200

    typos

--- src/main/tests/test-memory-magazine.cpp
+++ src/main/tests/test-memory-magazine.cpp
@@ -2,9 +2,30 @@
 #include "y/memory/magazine.hpp"
 #include "y/utest/run.hpp"
 
+using namespace upsylon;
+
+namespace {
+
+    class dummy
+    {
+    public:
+        inline   dummy() throw() {}
+        inline  ~dummy() throw() {}
+
+        typedef memory::magazine<dummy> repo;
+
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(dummy);
+    };
+
+
+
+}
+
 Y_UTEST(magazine)
 {
-    
+    //dummy::repo &mgr = dummy::repo::instance();
+
 }
 Y_UTEST_DONE()
 
--- src/main/y/memory/magazine.hpp
+++ src/main/y/memory/magazine.hpp
@@ -14,12 +14,17 @@ namespace upsylon
         class magazine : public singleton< magazine<T> >, public tight::supply_of<T>
         {
         public:
+            typedef singleton< magazine<T> > singleton_type;
+            typedef tight::supply_of<T>      supply_type;
+
+            Y_SINGLETON_DECL(magazine);
             
         private:
             Y_DISABLE_COPY_AND_ASSIGN(magazine);
-            inline explicit magazine()
+            inline explicit magazine() : singleton_type(), supply_type()
             {
             }
+
             inline virtual ~magazine() throw()
             {
             }
