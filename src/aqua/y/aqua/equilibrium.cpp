741e93e490dbbe288990805850f246b6e5a1ce32
Yann Bouret <yann.bouret@unice.fr>
2020-10-19 08:08:13 +0200

    wip

--- src/aqua/y/aqua/equilibrium.cpp
+++ src/aqua/y/aqua/equilibrium.cpp
@@ -10,17 +10,7 @@ namespace upsylon {
 
     namespace Aqua
     {
-        Component:: ~Component() throw()
-        {
-        }
-
-        Component::Component(const Species &sp_, const int nu_) throw() :
-        sp(sp_),
-        nu(nu_),
-        p( abs_of(nu) ),
-        pm1( p-1 )
-        {
-        }
+       
 
         Equilibrium:: ~Equilibrium() throw()
         {}
