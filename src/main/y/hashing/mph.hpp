7cc77763574ba6f262e9315f674f02eb2e08e486
Yann Bouret <yann.bouret@unice.fr>
2020-10-23 08:07:09 +0200

    tuypos

--- src/main/y/hashing/mph.hpp
+++ src/main/y/hashing/mph.hpp
@@ -9,18 +9,42 @@ namespace upsylon
 
     namespace hashing
     {
+        //! base class
         typedef affix_tree<unsigned> minimal_perfect_tree;
 
+        //______________________________________________________________________
+        //
+        //
         //! minimal perfect hashing
+        /**
+         based on an affix_tree
+         */
+        //
+        //______________________________________________________________________
         class minimal_perfect : public minimal_perfect_tree
         {
         public:
-            static const_type invalid = limit_of<const_type>::maximum;
-            explicit minimal_perfect();
-            virtual ~minimal_perfect() throw();
-            minimal_perfect(const minimal_perfect &);
-            minimal_perfect(const char **words, const unsigned count);
+            //__________________________________________________________________
+            //
+            // types and definition
+            //__________________________________________________________________
+            static const_type invalid = limit_of<const_type>::maximum; //!< unallowed key
 
+            //__________________________________________________________________
+            //
+            // C++
+            //__________________________________________________________________
+            explicit minimal_perfect();                                //!< setup empty
+            virtual ~minimal_perfect() throw();                        //!< cleanup
+            minimal_perfect(const minimal_perfect &);                  //!< copy
+            minimal_perfect(const char **words, const unsigned count); //!< build from C-strings
+
+
+            //__________________________________________________________________
+            //
+            // methods
+            //__________________________________________________________________
+            //! a new key/hash
             template <typename ID> inline
             bool add(const ID &key, const_type h)
             {
@@ -28,6 +52,7 @@ namespace upsylon
                 return insert_by(key,h);
             }
 
+            //! get hash, invalid if not found
             template <typename ID> inline
             type operator()(const ID &key)
             {
@@ -50,6 +75,7 @@ namespace upsylon
             void check(const_type) const;
         };
 
+        //! macro for static arrays of strings
 #define  Y_MPH_WORDS(DATA) DATA, sizeof(DATA)/sizeof(DATA[0])
 
 
