55eca4c69ed94b35dbb14cfd0cec1669fc5b4145
Yann Bouret <yann.bouret@unice.fr>
2020-11-19 09:49:33 +0100

    typos

--- src/main/y/associative/catalog.hpp
+++ src/main/y/associative/catalog.hpp
@@ -7,21 +7,33 @@
 #include "y/container/associative.hpp"
 
 namespace upsylon {
-    
-    //! a catalog insert args (possesing a key)
+
+    //__________________________________________________________________________
+    //
+    //
+    //! a catalog insert args (possesing a key() method)
+    //
+    //__________________________________________________________________________
     template <typename KEY, typename T>
     class catalog : public associative<KEY,T>
     {
     public:
+        //______________________________________________________________________
+        //
+        // types and defintions
+        //______________________________________________________________________
         Y_DECL_ARGS(T,type);       //!< alias
         Y_DECL_ARGS(KEY,key_type); //!< alias
-        
+
+        //______________________________________________________________________
+        //
+        // methods
+        //______________________________________________________________________
         inline  virtual ~catalog() throw() {}         //!< cleanup
         virtual bool     insert(param_type args) = 0; //!< false if already exists
         
     protected:
-        //! setup
-        inline explicit catalog() throw() : associative<KEY,T>() {}
+        inline explicit catalog() throw() : associative<KEY,T>() {} //! setup
         
     private:
         Y_DISABLE_COPY_AND_ASSIGN(catalog);
