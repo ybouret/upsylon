4817833902e5481ae1876fb5e15d7b6adb356aa3
Yann Bouret <yann.bouret@unice.fr>
2020-11-19 10:39:50 +0100

    typo

--- src/main/y/associative/hash/nodes.hpp
+++ src/main/y/associative/hash/nodes.hpp
@@ -3,13 +3,13 @@
 #ifndef Y_HASHED_NODES_INCLUDED
 #define Y_HASHED_NODES_INCLUDED 1
 
-#include "y/associative/hash/slots.hpp"
+#include "y/associative/hash/meta-node.hpp"
 #include "y/type/args.hpp"
 
 namespace upsylon
 {
 
-    
+
 
 }
 
