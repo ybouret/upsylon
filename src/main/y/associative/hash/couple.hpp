c30f56407a83a9b64ff6ff198f797c5b25778140
Yann Bouret <yann.bouret@unice.fr>
2020-11-19 15:30:49 +0100

    wip

---
+++ src/main/y/associative/hash/couple.hpp
@@ -0,0 +1,16 @@
+
+//! \file
+
+#ifndef Y_HASH_ZPAIRS_INCLUDED
+#define Y_HASH_ZPAIRS_INCLUDED 1
+
+#include "y/core/list.hpp"
+#include "y/object.hpp"
+#include "y/sort/merge.hpp"
+
+namespace upsylon
+{
+
+}
+
+#endif
