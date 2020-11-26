f5d3f9165e6bc9d9fa20216a0a903ffed44b91b5
Yann Bouret <yann.bouret@unice.fr>
2020-11-26 08:46:48 +0100

    wip build-all

--- scripts/build-all.cmake
+++ scripts/build-all.cmake
@@ -1,4 +1,63 @@
-FILE(GLOB_RECURSE entries "./forge/*/Debug*" LIST_DIRECTORIES true)
-FOREACH( ep IN ITEMS ${entries} )
-	MESSAGE( ${ep} )
+FILE(GLOB compilers "./forge/*" )
+
+FOREACH( compiler ${compilers} )
+	MESSAGE( STATUS "processing ${compiler}" )
+	SET(known_compiler FALSE)
+	
+	#single configuration compilers
+	SET(gnu         FALSE)
+	SET(intel       FALSE)
+	set(clang       FALSE)
+	
+	#mutliple configurations compilers
+	set(msc         FALSE)
+	set(xcode       FALSE)
+	
+	#detecting compiler
+	IF( ${compiler} MATCHES ".*gnu.*" )
+	SET(known_compiler TRUE)
+	SET(gnu            TRUE)
+	ENDIF()
+	
+	IF( ${compiler} MATCHES ".*intel.*")
+	SET(known_compiler TRUE)
+	SET(intel          TRUE)
+	ENDIF()
+	
+	IF( ${compiler} MATCHES ".*clang.*" )
+	SET(known_compiler TRUE)
+	SET(clang          TRUE)
+	ENDIF()
+	
+	IF( ${compiler} MATCHES "VisualStudio.*")
+	SET(known_compiler TRUE)
+	SET(msc            TRUE)
+	ENDIF()
+	
+	IF( ${compiler} MATCHES "Xcode")
+	SET(known_compiler TRUE)
+	SET(xcode          TRUE)
+	ENDIF()
+	
+	IF( NOT known_compiler )
+	MESSAGE( FATAL_ERROR "unknown compiler" )
+	ENDIF()
+	
+	IF(xcode)
+		MESSAGE( STATUS "building for Xcode in '${compiler}'" )
+		EXECUTE_PROCESS(
+		COMMAND           ${CMAKE_COMMAND} --build . --config Release --target ALL_BUILD
+		WORKING_DIRECTORY ${compiler}
+ 		)
+	ENDIF()
+	
+	IF(msc)
+		MESSAGE( STATUS "building for Microsoft" )
+		EXECUTE_PROCESS(
+		COMMAND           ${CMAKE_COMMAND} --build . --config Release --target ALL_BUILD -- -m
+		WORKING_DIRECTORY ${compiler}
+ 		)
+	ENDIF()
+	
 ENDFOREACH()
+
