3787489c6083428783030a77c085976e5f5f9da6
Yann Bouret <yann.bouret@unice.fr>
2020-12-08 17:11:27 +0100

    doxygen update

--- src/net/y/net/comm/queues.hpp
+++ src/net/y/net/comm/queues.hpp
@@ -11,16 +11,25 @@ namespace upsylon
 {
     namespace net
     {
-        
+
+        //______________________________________________________________________
+        //
+        //
+        //! send/recv queues
+        //
+        //______________________________________________________________________
         class comm_queues
         {
         public:
+            //! setup with send/recv block size
             explicit comm_queues(const size_t send_block_size,
                                  const size_t recv_block_size);
+
+            //! cleanup
             virtual ~comm_queues() throw();
 
-            send_queue sendQ;
-            recv_queue recvQ;
+            send_queue sendQ; //!< to send
+            recv_queue recvQ; //!< to recv
             
         private:
             Y_DISABLE_COPY_AND_ASSIGN(comm_queues);
