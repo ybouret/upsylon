c9ef326e79fc51bb3c6a2ffd2dc6db70b0b7cdde
Yann Bouret <yann.bouret@unice.fr>
2020-12-10 15:54:17 +0100

    fixed bugs and docs

--- src/main/y/memory/dblock.hpp
+++ src/main/y/memory/dblock.hpp
@@ -1,7 +1,7 @@
 //! \file
 
 #ifndef Y_MEMORY_DBLOCK_INCLUDED
-#define Y_MEMORT_DBLOCK_INCLUDED 1
+#define Y_MEMORY_DBLOCK_INCLUDED 1
 
 #include "y/core/list.hpp"
 #include "y/object.hpp"
@@ -12,26 +12,37 @@ namespace upsylon
 
     namespace memory
     {
-        class dblocks;
+        class dblocks; //!< forward declaration
 
-        //! low-level data block
+        //______________________________________________________________________
+        //
+        //
+        //! low-level data block, cached or not
+        //
+        //______________________________________________________________________
         class dblock : public object
         {
         public:
+            //__________________________________________________________________
+            //
+            // types and definitions
+            //__________________________________________________________________
 
+            //! smart pointer, using 'zap' to delete
             class pointer : public ptr<dblock>
             {
             public:
-                explicit pointer(dblock *blk) throw();
-                virtual ~pointer() throw();
+                explicit pointer(dblock *blk) throw(); //!< setup blk!=NULL
+                virtual ~pointer() throw();            //!< zap(pointee)
 
             private:
                 Y_DISABLE_COPY_AND_ASSIGN(pointer);
             };
 
-
+            //! core list
             typedef core::list_of<dblock> core_list;
 
+            //! list using 'zap' to delete nodes
             class list : public core_list
             {
             public:
@@ -42,21 +53,30 @@ namespace upsylon
                 Y_DISABLE_COPY_AND_ASSIGN(list);
             };
 
-            virtual ~dblock() throw();
-            explicit dblock(const size_t bs);
 
+            //__________________________________________________________________
+            //
+            // C++
+            //__________________________________________________________________
 
+            virtual ~dblock() throw();          //!< cleanup
+            explicit dblock(const size_t bs);   //!< setup
 
-            uint8_t *       operator*()       throw();
-            const uint8_t * operator*() const throw();
-            static   void zap(dblock *pointee) throw();
+            //__________________________________________________________________
+            //
+            // methods
+            //__________________________________________________________________
+
+            uint8_t *       operator*()       throw();  //!< access
+            const uint8_t * operator*() const throw();  //!< access
+            static   void zap(dblock *pointee) throw(); //!< smart delete
 
             const size_t size; //!< available bytes
             const size_t exp2; //!< block_size = 2^block_exp2
         public:
             uint8_t     *addr; //!< bytes
-            dblock      *next;
-            dblock      *prev;
+            dblock      *next; //!< for list
+            dblock      *prev; //!< for list
 
         private:
             Y_DISABLE_COPY_AND_ASSIGN(dblock);
@@ -64,26 +84,49 @@ namespace upsylon
                                 const size_t block_exp2,
                                 dblocks    * block_org,
                                 core_list  * block_src);
-            
-            dblocks     *origin;
-            core_list   *source;
+
+            dblocks     *origin; // cache
+            core_list   *source; // slot in cache
+
             friend class dblocks;
         };
 
-        
+        //______________________________________________________________________
+        //
+        //
+        //! cache of dblocks
+        //
+        //______________________________________________________________________
         class dblocks
         {
         public:
-            typedef dblock::core_list slot_type;
+            //__________________________________________________________________
+            //
+            // types and definitions
+            //__________________________________________________________________
+            typedef dblock::core_list slot_type; //!< alias
 
-            explicit dblocks(lockable &);
-            virtual ~dblocks() throw();
+            //__________________________________________________________________
+            //
+            // C++
+            //__________________________________________________________________
+            explicit dblocks(lockable &); //!< setup with a lock
+            virtual ~dblocks() throw();   //!< clean all up
 
+            //__________________________________________________________________
+            //
+            // methods
+            //__________________________________________________________________
+
+            //! access slot in [0..slots-1]
             const slot_type & operator[](const size_t) const throw();
 
+            //! query/create a block
             dblock * query(const size_t bs);
-            
+
+            //! display (to debug)
             std::ostream        & display( std::ostream &os ) const;
+            //! display (to debug)
             friend std::ostream & operator<<(std::ostream &os, const dblocks &db);
 
             const size_t slots;    //!< base2<size_t>::max_slots
