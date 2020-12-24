0c70acaabf633c46ae06ba3d2a460aa085947588
Yann Bouret <yann.bouret@unice.fr>
2020-12-24 14:05:06 +0100

    wip

--- src/python/spam.cpp
+++ src/python/spam.cpp
@@ -15,7 +15,7 @@ spam_system(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "s", &command))
         return NULL;
     sts = system(command);
-    if (sts < 0) {
+    if (sts != 0) {
         PyErr_SetString(SpamError, "System command failed");
         return NULL;
     }
