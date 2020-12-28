98d36acc10187a8f35e30bdcbbb90f355acb8db3
Yann Bouret <yann.bouret@unice.fr>
2020-12-28 13:43:04 +0100

    wip

--- src/main/y/mkl/types.cxx
+++ src/main/y/mkl/types.cxx
@@ -5,18 +5,18 @@
 
 namespace upsylon
 {
-	namespace mkl
-	{
+    namespace mkl
+    {
         template <> const real_t numeric<real_t>::minimum      = REAL_MIN;
         template <> const real_t numeric<real_t>::epsilon      = REAL_EPSILON;
         template <> const real_t numeric<real_t>::maximum      = REAL_MAX;
-        template <> const unsigned numeric<real_t>::mant_dig     = REAL_MANT_DIG;
+        template <> const unsigned numeric<real_t>::mant_dig   = REAL_MANT_DIG;
         template <> const real_t numeric<real_t>::pi           = REAL_PI;
         template <> const real_t numeric<real_t>::two_pi       = REAL_TWO_PI;
         template <> const real_t numeric<real_t>::half_pi      = REAL_PI*REAL(0.5);
         template <> const real_t numeric<real_t>::min_exp      = REAL_MIN_EXP;
         template <> const real_t numeric<real_t>::max_exp      = REAL_MAX_EXP;
-        template <> const unsigned numeric<real_t>::dig          = REAL_DIG;
+        template <> const unsigned numeric<real_t>::dig        = REAL_DIG;
         template <> const int    numeric<real_t>::min_10_exp   = REAL_MIN_10_EXP;
         template <> const int    numeric<real_t>::max_10_exp   = REAL_MAX_10_EXP;
         template <> const real_t numeric<real_t>::ftol         = REAL(pow)( REAL(10.0), -real_t(dig) );
@@ -67,7 +67,7 @@ namespace upsylon
             }
         }
 
-	}
+    }
 
     template <> const mkl::real_t & xnumeric<mkl::real_t>::abs_minimum() { return mkl::numeric<mkl::real_t>::minimum; }
 
