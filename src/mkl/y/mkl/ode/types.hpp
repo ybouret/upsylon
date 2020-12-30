8d10f0c18198a81717aeef64c55661340594bda1
Yann Bouret <yann.bouret@unice.fr>
2020-12-30 12:01:11 +0100

    typos

--- src/mkl/y/mkl/ode/types.hpp
+++ src/mkl/y/mkl/ode/types.hpp
@@ -18,14 +18,14 @@ namespace upsylon {
             //! proxy for types definition
             template <typename T> struct Field
             {
-                typedef array<T>                                            Array;    //!< array of variables
-                typedef matrix<T>                                           Matrix;   //!< alias for matrix
-                typedef arrays<T,memory_type>                               Arrays;   //!< multiple arrays at once
+                typedef arrays<T,memory_type>                               Arrays;      //!< multiple arrays at once
+                typedef array<T>                                            Array;       //!< for interfaces
+                typedef matrix<T>                                           Matrix;      //!< alias
 
-                typedef functor<void,TL3(Array&,T,const Array&)>            Equation; //!< derivs( dydx[], x, y[] );
-                typedef functor<void,TL4(Array&,Matrix&, T, const Array &)> Jacobian; //!< jacobn( dfdx[], dfdy[][], x, y[] );
-                typedef functor<void,TL2(Array&,T)>                         Callback; //!< adjust trials y[]
-                typedef functor<void,TL2(T,const Array&)>                   Collect;  //!< to use information (x,y[])
+                typedef functor<void,TL3(Array &, T, const Array &)>            Equation; //!< derivs( dydx[], x, y[] );
+                typedef functor<void,TL4(Array &, Matrix  &, T, const Array &)> Jacobian; //!< jacobn( dfdx[], dfdy[][], x, y[] );
+                typedef functor<void,TL2(Array &,T)>                            Callback; //!< adjust trials y[]
+                typedef functor<void,TL2(T, const Array &)>                     Collect;  //!< to use information (x,y[])
 
             };
 
