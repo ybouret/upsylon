4bef7664ec27a1f576bc1d35050b210968e2e853
ybouret <yann.bouret@gmail.com>
2021-01-10 10:26:03 +0100

    wip

--- src/mkl/tests/test-fitting-ode.cpp
+++ src/mkl/tests/test-fitting-ode.cpp
@@ -14,7 +14,7 @@ using namespace mkl;
 using namespace fitting;
 
 namespace {
-
+    
     template <typename T>
     class Damped : public ODE::ExplicitAdjust<T>
     {
@@ -23,17 +23,17 @@ namespace {
         explicit Damped() throw() : calls(0)
         {
         }
-
+        
         virtual ~Damped() throw()
         {
         }
-
+        
         inline virtual size_t dimensions() const throw() { return 2; }
-
+        
         inline virtual T start() const throw() { return 0; }
-
+        
         inline virtual T delta() const throw() { return T(0.01); }
-
+        
         virtual void   setup(addressable<T>       &Y,
                              const accessible<T>  &aorg,
                              const variables      &vars) const
@@ -43,7 +43,7 @@ namespace {
             Y[1] =  vars(aorg,"y0");
             Y[2] = 0;
         }
-
+        
         virtual void   rates(addressable<T>       &dYdt,
                              const T               ,
                              const accessible<T>  &Y,
@@ -60,7 +60,7 @@ namespace {
             dYdt[2] = -mu * speed - (om*om)*position;
             ++calls;
         }
-
+        
         virtual T query(const T              ,
                         const accessible<T> &Y,
                         const accessible<T> &,
@@ -68,16 +68,16 @@ namespace {
         {
             return Y[1]; // position
         }
-
+        
     private:
         Y_DISABLE_COPY_AND_ASSIGN(Damped);
     };
-
+    
     template <typename T>
     void doAdjust()
     {
         const string &sfx = type_name_of<T>();
-
+        
         const size_t                   n = 10 + alea.leq(10);
         typename sample<T,T>::pointer  s = sample<T,T>::create("sample",n);
         
@@ -89,8 +89,8 @@ namespace {
             const T y  = exp_of( -T(0.3) * x ) * cos_of( T(2.7) * x );
             s->add(x,y);
         }
-
-
+        
+        
         const string filename = "adjode-" + sfx + ".dat";
         const string savename = "adjfcn-" + sfx + ".dat";
         {
@@ -98,82 +98,90 @@ namespace {
             s->save(fp);
         }
         ios::ocstream::overwrite(savename);
-
+        
         Damped<T> *damped = new Damped<T>();
         typename ODE::ExplicitAdjust<T>::Pointer crunch = damped;
         typename ODE::ExplicitSolver<T>::Pointer solver = ODE::DriverCK<T>::New();
         correlation<T>                           corr;
-
+        
         explode<T>         F(solver,crunch);
         least_squares<T,T> ls(true);
         variables         &vars = **s;
-
-
-
+        
+        
+        
         vars << "mu" << "omega" << "y0";
-
+        
         vector<T>    aorg( vars.sweep(),  0 );
         vector<bool> used( aorg.size(), true);
         vector<T>    aerr( aorg.size() );
-
-
-
+        
+        
+        
         const char * pass[] =
         {
             "mu:omega", "y0", "mu:omega:y0"
         };
-
-        const unsigned flags[] = { 0 , Y_GLS_EXPAND };
-        vector<size_t> calls;
-        for(size_t q=0;q<sizeof(flags)/sizeof(flags[0]);++q)
+        
+        try
         {
-            vars(aorg,"mu")    = 0.6;
-            vars(aorg,"omega") = 2.6;
-            vars(aorg,"y0")    = 1;
-            damped->calls      = 0;
-            for(size_t k=0;k<sizeof(pass)/sizeof(pass[0]);++k)
+            const unsigned flags[] = { 0 , Y_GLS_EXPAND };
+            vector<size_t> calls;
+            for(size_t q=0;q<sizeof(flags)/sizeof(flags[0]);++q)
             {
-                vars.only_on(used,pass[k]);
-                if(ls.fit(*s, F, aorg, used, aerr, flags[q]) )
+                vars(aorg,"mu")    = 0.6;
+                vars(aorg,"omega") = 2.6;
+                vars(aorg,"y0")    = 1;
+                damped->calls      = 0;
+                for(size_t k=0;k<sizeof(pass)/sizeof(pass[0]);++k)
                 {
-                    display_variables::errors(std::cerr,"\t",vars,aorg,used,aerr);
+                    vars.only_on(used,pass[k]);
+                    if(ls.fit(*s, F, aorg, used, aerr, flags[q]) )
                     {
-                        ios::ocstream fp(filename);
-                        s->save(fp);
-                    }
-                    std::cerr<< "R2    = " << s->compute_R2() << std::endl;
-                    std::cerr<< "corr  = " << s->compute_corr(corr) << std::endl;
-                    {
-                        ios::ocstream fp(savename);
-                        T        x = 0;
-                        const T dx = T(0.02);
-
-                        fp("%g %g\n",x,F.start(x,aorg,vars));
-                        for(x+=dx;x<=s->abscissa->back();x+=dx)
+                        display_variables::errors(std::cerr,"\t",vars,aorg,used,aerr);
                         {
-                            fp("%g %g\n",x,F.reach(x,aorg,vars));
+                            ios::ocstream fp(filename);
+                            s->save(fp);
                         }
-
+                        std::cerr<< "R2    = " << s->compute_R2() << std::endl;
+                        std::cerr<< "corr  = " << s->compute_corr(corr) << std::endl;
+                        {
+                            ios::ocstream fp(savename);
+                            T        x = 0;
+                            const T dx = T(0.02);
+                            
+                            fp("%g %g\n",x,F.start(x,aorg,vars));
+                            for(x+=dx;x<=s->abscissa->back();x+=dx)
+                            {
+                                fp("%g %g\n",x,F.reach(x,aorg,vars));
+                            }
+                            
+                        }
+                    }
+                    else
+                    {
+                        std::cerr << "couldn't fit '" << pass[k] << "'" << std::endl;
+                        break;
                     }
                 }
-                else
-                {
-                    std::cerr << "couldn't fit '" << pass[k] << "'" << std::endl;
-                    break;
-                }
+                
+                display_variables::values(std::cerr << "aorg" << std::endl, " (*) ", vars, aorg, "//") << std::endl;
+                display_variables::values(std::cerr << "used" << std::endl, NULL,    vars, used, NULL) << std::endl;
+                
+                display_sample::results(std::cerr,*s, aorg, used, aerr);
+                std::cerr << "#calls=" << damped->calls << std::endl;
+                calls.push_back(damped->calls);
             }
-
-            display_variables::values(std::cerr << "aorg" << std::endl, " (*) ", vars, aorg, "//") << std::endl;
-            display_variables::values(std::cerr << "used" << std::endl, NULL,    vars, used, NULL) << std::endl;
-
-            display_sample::results(std::cerr,*s, aorg, used, aerr);
-            std::cerr << "#calls=" << damped->calls << std::endl;
-            calls.push_back(damped->calls);
+            std::cerr << "calls=" << calls << std::endl;
         }
-        std::cerr << "calls=" << calls << std::endl;
-
+        catch(const exception &e)
+        {
+            std::cerr << "something went wrong: " << e.what() << std::endl;
+            std::cerr << e.when() << std::endl;
+        }
+        
     }
-
+    
 }
 
 
@@ -182,7 +190,7 @@ Y_UTEST(fitting_ode)
 {
     doAdjust<double>();
     doAdjust<float>();
-
+    
 }
 Y_UTEST_DONE()
 
