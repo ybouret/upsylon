41d362eab237eec92f299d9f4491db04292c270e
ybouret <yann.bouret@gmail.com>
2021-02-04 15:42:40 +0100

    mlock for win32

--- src/main/tests/test-memory-lock.cpp
+++ src/main/tests/test-memory-lock.cpp
@@ -20,38 +20,38 @@ using namespace upsylon;
 Y_UTEST(mlock)
 {
 
-    int block_size = 1024;
-    if(argc>1)
-    {
-        block_size = atoi(argv[1]);
-    }
+	int block_size = 1024;
+	if (argc > 1)
+	{
+		block_size = atoi(argv[1]);
+	}
 
 #if defined(Y_BSD)
-    std::cerr << "+" << block_size << std::endl;
-    void * addr = calloc(block_size,1);
-    if(addr)
-    {
-        const int status = mlock(addr,block_size);
-        if(status!=0)
-        {
-            perror("mlock");
-        }
-        else
-        {
-            munlock(addr,block_size);
-        }
-        free(addr);
-    }
-    else
-    {
-        perror("no memory");
-    }
+	std::cerr << "+" << block_size << std::endl;
+	void * addr = calloc(block_size, 1);
+	if (addr)
+	{
+		const int status = mlock(addr, block_size);
+		if (status != 0)
+		{
+			perror("mlock");
+		}
+		else
+		{
+			munlock(addr, block_size);
+		}
+		free(addr);
+	}
+	else
+	{
+		perror("no memory");
+	}
 #endif
 
 #if defined(Y_WIN)
-    SYSTEM_INFO sSysInfo;         // useful information about the system
-    GetSystemInfo(&sSysInfo);     // initialize the structure
-    printf("This computer has page size %u.\n", sSysInfo.dwPageSize);
+	SYSTEM_INFO sSysInfo;         // useful information about the system
+	GetSystemInfo(&sSysInfo);     // initialize the structure
+	printf("This computer has page size %u.\n", sSysInfo.dwPageSize);
 	//block_size = sSysInfo.dwPageSize;
 	void *addr = VirtualAlloc(NULL, block_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 	if (!addr)
@@ -67,6 +67,21 @@ Y_UTEST(mlock)
 		tgt[i] = alea.range<char>('a', 'z');
 	}
 	std::cerr << tgt << std::endl;
+
+	if (!VirtualLock(addr, block_size))
+	{
+		win32::exception e(GetLastError(), "VirtualLock");
+		std::cerr << e.what() << " " << e.when() << std::endl;
+	}
+	else
+	{
+		if (!VirtualUnlock(addr, block_size))
+		{
+			win32::exception e(GetLastError(), "VirtualUnlock");
+			std::cerr << e.what() << " " << e.when() << std::endl;
+		}
+	}
+
 	if (!VirtualFree(addr, 0, MEM_RELEASE))
 	{
 		throw win32::exception(GetLastError(), "VirtualFree");
