256a56040952afb5bf3586d395a9806ba063ea2f
Yann Bouret <yann.bouret@unice.fr>
2021-02-04 18:19:24 +0100

    typos

--- src/main/tests/test-memory-lock.cpp
+++ src/main/tests/test-memory-lock.cpp
@@ -24,47 +24,14 @@ Y_UTEST(mlock)
     memory::locked_area blk( block_size );
     std::cerr << "blk.bytes  = " << blk.bytes << std::endl;
 
+    char  *      s = (char *)(blk.entry);
+    const size_t n = alea.lt(blk.bytes);
+    for(size_t i=0;i<n;++i)
+    {
+        s[i] = alea.range<char>('a','z');
+    }
+    
 
-#if 0
-#if defined(Y_WIN)
-	SYSTEM_INFO sSysInfo;         // useful information about the system
-	GetSystemInfo(&sSysInfo);     // initialize the structure
-    void *addr = VirtualAlloc(NULL, block_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	if (!addr)
-	{
-		throw win32::exception(GetLastError(), "VirtualAlloc");
-	}
-
-	memset(addr, 0, block_size);
-	char *tgt = (char *)addr;
-	const size_t num = alea.leq(block_size);
-	for (size_t i = 0; i < num; ++i)
-	{
-		tgt[i] = alea.range<char>('a', 'z');
-	}
-	std::cerr << tgt << std::endl;
-
-	if (!VirtualLock(addr, block_size))
-	{
-		win32::exception e(GetLastError(), "VirtualLock");
-		std::cerr << e.what() << " " << e.when() << std::endl;
-	}
-	else
-	{
-		if (!VirtualUnlock(addr, block_size))
-		{
-			win32::exception e(GetLastError(), "VirtualUnlock");
-			std::cerr << e.what() << " " << e.when() << std::endl;
-		}
-	}
-
-	if (!VirtualFree(addr, 0, MEM_RELEASE))
-	{
-		throw win32::exception(GetLastError(), "VirtualFree");
-	}
-
-#endif
-#endif
 
 
 }
