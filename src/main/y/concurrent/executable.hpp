b9925f76bd2dcc43a03a35a88ffb73507c6fb4c5
ybouret <yann.bouret@gmail.com>
2021-02-19 16:14:29 +0100

    typo, need to win test

--- src/main/y/concurrent/executable.hpp
+++ src/main/y/concurrent/executable.hpp
@@ -19,6 +19,8 @@ namespace upsylon
         class executable
         {
         public:
+            
+            //! launcher to redirect executable.call
             class launcher : public thread
             {
             public:
