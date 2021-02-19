a96d129124084450ac6dfadc245807fcb6eb1dff
Yann Bouret <yann.bouret@unice.fr>
2021-02-19 11:16:52 +0100

    need to cleanup

--- src/main/tests/test-thr-loop.cpp
+++ src/main/tests/test-thr-loop.cpp
@@ -8,9 +8,13 @@ using namespace upsylon;
 
 namespace
 {
+    static size_t work_loops = 0;
+    static double wait_time  = 0;
+
     class doSome : public concurrent::runnable
     {
     public:
+
         explicit doSome() throw()
         {
 
@@ -23,18 +27,54 @@ namespace
 
         virtual void run(const concurrent::context & ctx, lockable &sync)
         {
-            Y_LOCK(sync);
-            std::cerr << "\t...do something @" << ctx.label << std::endl;
+            {
+                Y_LOCK(sync);
+                std::cerr << "\t...do something @" << ctx.label << " / " << work_loops << std::endl;
+            }
+
+            volatile double sum = 0;
+            for(size_t i=0;i<work_loops;++i)
+            {
+                sum += cos(double(i));
+            }
+
         }
 
     private:
         Y_DISABLE_COPY_AND_ASSIGN(doSome);
 
     };
+
+    class doNope : public concurrent::runnable
+    {
+    public:
+        explicit doNope() throw() {}
+        virtual ~doNope() throw() {}
+
+        virtual void run(const concurrent::context &, lockable &)
+        {
+        }
+
+    private:
+        Y_DISABLE_COPY_AND_ASSIGN(doNope);
+    };
 }
+
+#include "y/string/convert.hpp"
 
 Y_UTEST(thr_loop)
 {
+    real_time_clock clk;
+
+    if(argc>1)
+    {
+        work_loops = string_convert::to<size_t>(argv[1],"work_loops");
+    }
+
+    if(argc>2)
+    {
+        wait_time = string_convert::to<double>(argv[2],"wait_time");
+    }
 
     std::cerr << "Empty   SIMT" << std::endl;
     {
@@ -45,13 +85,30 @@ Y_UTEST(thr_loop)
     std::cerr << "Working SIMT" << std::endl;
     {
         concurrent::simt par;
-        doSome           code;
-        par.loop(code);
         {
-            Y_LOCK(par.access);
-            std::cerr << "launched!"<< std::endl;
+            doSome           code;
+
+            std::cerr << "one loop" << std::endl;
+            {
+                par.loop(code);
+                {
+                    Y_LOCK(par.access);
+                    std::cerr << "launched!"<< std::endl;
+                }
+                clk.sleep(wait_time);
+                par.join();
+            }
         }
-        par.join();
+
+        {
+            std::cerr << "many loops" << std::endl;
+            doNope nope;
+            for(size_t i=0;i<10000;++i)
+            {
+                par.loop(nope);
+                par.join();
+            }
+        }
     }
 
 
