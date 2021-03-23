932cec5cbd5f6d8720bf4a88d4ce625d2933c5f0
Yann Bouret <yann.bouret@unice.fr>
2021-03-23 14:04:59 +0100

    wip

--- src/main/tests/ctypes-example.py
+++ src/main/tests/ctypes-example.py
@@ -1,11 +1,17 @@
 from ctypes import * 
 import numpy as np
+import platform
 
-m            = cdll.LoadLibrary("./libmodule.so")
+
+module_name  = "./libmodule.so"
+if platform.system() == "Windows":
+	module_name="./libmodule.dll"
+
+m            = cdll.LoadLibrary(module_name)
 Sin          = m.Sin
 Sin.argtypes = [c_double]
 Sin.restype  = c_double
 
-for i in np.arange(0,3,0.1):
+for i in np.arange(0,3,0.5):
 	print(i,Sin(i))
 
