d588a5d6b92f408e3dcffe3aa5a7c5fe2718a6c5
ybouret <yann.bouret@gmail.com>
2021-03-28 18:02:44 +0200

    wip runic

--- src/main/y/concurrent/loop/runic.hpp
+++ src/main/y/concurrent/loop/runic.hpp
@@ -15,27 +15,46 @@ namespace upsylon
     namespace concurrent
     {
         
-        
+        //______________________________________________________________________
+        //
+        //
+        //! Runnning In C
+        /**
+         local cache are handled and passed to C functions
+         */
+        //
+        //______________________________________________________________________
         class runic :
         public ref_ptr<looper>,
         public memory::shacks,
         public runnable
         {
         public:
-            typedef memory::shack  cache;
+            //__________________________________________________________________
+            //
+            // types and definition
+            //__________________________________________________________________
             
-            explicit runic( const arc_ptr<looper> & );
-            explicit runic( looper * );
-
-         
-            
-            virtual ~runic() throw();
-            
+            //! C-style procedure, call for each context with private memory
             typedef void (*proc)(const context &,
                                  memory::shack &,
                                  void          *,
                                  lockable      &);
             
+            //__________________________________________________________________
+            //
+            // C++
+            //__________________________________________________________________
+            explicit runic(const arc_ptr<looper> &); //!< setup from a shared looper
+            explicit runic(looper * );               //!< setup from a new looper
+            virtual ~runic() throw();                //!< cleanup
+            
+            
+           
+            //__________________________________________________________________
+            //
+            //! calling a C procedure for each context, providing local memory
+            //__________________________________________________________________
             void operator()(proc,void*) throw();
             
         private:
