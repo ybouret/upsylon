f56ba3457de18a317f0c2585e46a231fde3d34b5
Yann Bouret <yann.bouret@unice.fr>
2021-03-29 15:12:11 +0200

    wip hunt

--- src/main/tests/test-hunt.cpp
+++ src/main/tests/test-hunt.cpp
@@ -1,13 +1,37 @@
 #include "y/code/hunt.hpp"
 #include "y/utest/run.hpp"
+#include "y/sequence/vector.hpp"
 
 using namespace upsylon;
 
 
 namespace
 {
+    template <typename T> static inline
+    void test_hunt()
+    {
+        const size_t n = 2 + alea.lt(100);
+        vector<T>    x(n,0);
+        for(size_t i=2;i<=n;++i)
+        {
+            x[i] = x[i-1]+ alea.to<T>();
+        }
+        const T xmax = x[n];
+        std::cerr << x[1] << " -> " << x[n] << std::endl;
+        const T ampli = T(0.6) * xmax;
+        const T xhalf = xmax/2;
 
+        for(size_t iter=100;iter>0;--iter)
+        {
+            size_t j=alea.leq(n+10);
+
+            hunt(xhalf+ampli*alea.symm<T>(),&x[1], n, j);
+        }
+
+    }
 }
+
+
 Y_UTEST(hunt)
 {
     
--- src/main/y/code/hunt.hpp
+++ src/main/y/code/hunt.hpp
@@ -5,39 +5,72 @@
 #define Y_HUNT_INCLUDED 1
 
 #include "y/sequence/accessible.hpp"
+#include "y/type/utils.hpp"
 
 namespace upsylon
 {
 
+
+    //! x[j] <= xx <= x[j+1]
     template <typename T>
-    size_t hunt(accessible<T> &x, typename type_traits<T>::parameter_type xx)
+    void hunt(const T xx, const T x[], const size_t n, size_t &j) throw()
     {
-        const size_t n = x.size();
         switch(n)
         {
-            case 0: return 0;
-            case 1: return 1;
+            case 0: j=0; return; // bad
+            case 1: j=1; return; // bad
             default:
                 break;
         }
+        assert(n>=2);
+        const size_t m   = n-1;
+        size_t       jlo = clamp<size_t>(1,j,m);
+        size_t       jup = jlo+1;
 
-        if(xx<=x[1])
+        if(xx<x[jlo])
         {
-            return 1;
-        }
-        else
-        {
-            const size_t m = n-1;
-            if(xx>=x[n])
+            if(xx<x[1])
             {
-                return m;
+                // extrapolate from x[1]..x[2]
+                j=1;
+                return;
             }
             else
             {
-                assert(x>=x[1] && xx<=x[n]);
-                return 1;
+                jup = jlo;
+                jlo = 1;
+                goto BISECTION;
             }
         }
+        else
+        {
+            assert(xx>=x[jlo]);
+            if(x>x[jup])
+            {
+                if(x>=x[m])
+                {
+                    // extrapolate from x[n-1]..x[n]
+                    j=m;
+                    return;
+                }
+                else
+                {
+                    jlo = jup;
+                    jup = n;
+                    goto BISECTION;
+                }
+
+            }
+            else
+            {
+                // xx <= x[jup] => cached
+                return;
+            }
+        }
+
+        BISECTION:
+        assert(x[jlo]<=xx); assert(xx<=x[jup]);
+        
 
     }
 
