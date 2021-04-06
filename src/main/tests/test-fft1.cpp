88547affbef026ac34a2d0a0ccc32930af8e5850
Yann Bouret <yann.bouret@unice.fr>
2021-04-06 16:55:58 +0200

    fft1 with corrected trigonometric recurrence

--- src/main/tests/test-fft1.cpp
+++ src/main/tests/test-fft1.cpp
@@ -12,12 +12,28 @@ namespace
     {
         typedef complex<T> cplx;
 
-        for(size_t n=1;n<=256; n <<= 1)
+        for(size_t n=1;n<=4096; n <<= 1)
         {
-            vector<cplx> c(n,0);
-            support::fill1D(c);
+            vector<cplx> source(n,0);
+            vector<cplx> fourier(n,0);
+            support::fill1D(source);
 
-            fft1::forward(&c[1].re,n);
+            for(size_t i=n;i>0;--i) fourier[i] = source[i];
+
+            fft1::forward(&fourier[1].re-1,n);
+            fft1::reverse(&fourier[1].re-1,n);
+
+            T rms = 0;
+            for(size_t i=n;i>0;--i)
+            {
+                const cplx delta = fourier[i]/n-source[i];
+                rms += delta.mod2();
+            }
+            rms /= n;
+            rms = mkl::sqrt_of(rms);
+            std::cerr << "rms#" << n << " = " << rms << std::endl;
+
+
         }
     }
 }
@@ -25,6 +41,8 @@ namespace
 Y_UTEST(fft1)
 {
     do_test<float>();
+    do_test<double>();
+
 }
 Y_UTEST_DONE()
 
--- src/main/y/fft/fft1.hpp
+++ src/main/y/fft/fft1.hpp
@@ -56,8 +56,69 @@ namespace upsylon
                             real_t      *d_i   = data+i;
                             const size_t j     = i+mmax;
                             real_t      *d_j   = data+j;
-                            const real_t tempr = wr*d_j[0]-wi*d_j[1];
-                            const real_t tempi = wr*d_j[1]+wi*d_j[0];
+                            const real_t rwr   = real_t(wr);
+                            const real_t rwi   = real_t(wi);
+                            const real_t tempr = rwr*d_j[0]-rwi*d_j[1];
+                            const real_t tempi = rwr*d_j[1]+rwi*d_j[0];
+
+                            d_j[0]  = d_i[0]-tempr;
+                            d_j[1]  = d_i[1]-tempi;
+                            d_i[0] += tempr;
+                            d_i[1] += tempi;
+                        }
+                        wr=(wtemp=wr)*wpr-wi*wpi+wr;
+                        wi=wi*wpr+wtemp*wpi+wi;
+                    }
+                    mmax=istep;
+                    mln2=isln2;
+                }
+            }
+        }
+
+        //______________________________________________________________________
+        //
+        //!  inverse FFT of data[1..2*size]
+        //______________________________________________________________________
+        template <typename real_t> static inline
+        void reverse(real_t      *data,
+                     const size_t size) throw()
+        {
+            assert( data != NULL );
+            assert( is_a_power_of_two(size) );
+
+            //==================================================================
+            // bit reversal algorithm
+            //==================================================================
+            xbitrev::run(data, size);
+
+            //==================================================================
+            // Lanczos-Algorithm
+            //==================================================================
+            {
+                const size_t n    = size << 1;
+                size_t       mmax = 2;
+                size_t       mln2 = 1;
+                while (n > mmax) {
+                    const size_t istep = mmax << 1;
+                    const size_t isln2 = mln2+1;
+                    double       wtemp = -sin_table[isln2]; //sin(0.5*theta);
+                    const double wsq   = wtemp*wtemp;
+                    double wpr         = -(wsq+wsq);
+                    double wpi         = -sin_table[mln2];
+                    double wr          = 1.0;
+                    double wi          = 0.0;
+
+                    for (size_t m=1; m<mmax; m+=2)
+                    {
+                        for (size_t i=m; i<=n; i+=istep)
+                        {
+                            real_t      *d_i   = data+i;
+                            const size_t j     = i+mmax;
+                            real_t      *d_j   = data+j;
+                            const real_t rwr   = real_t(wr);
+                            const real_t rwi   = real_t(wi);
+                            const real_t tempr = rwr*d_j[0]-rwi*d_j[1];
+                            const real_t tempi = rwr*d_j[1]+rwi*d_j[0];
 
                             d_j[0]  = d_i[0]-tempr;
                             d_j[1]  = d_i[1]-tempi;
