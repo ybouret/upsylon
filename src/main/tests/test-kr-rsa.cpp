56df296d4891ae89f29eca41ba585aa73d805156
Yann Bouret <yann.bouret@unice.fr>
2021-04-16 08:49:17 +0200

    updated rsa

--- src/main/tests/test-kr-rsa.cpp
+++ src/main/tests/test-kr-rsa.cpp
@@ -27,11 +27,22 @@ void check_max_bits(const apn &modulus)
     
 }
 
+#include "y/yap/library.hpp"
+#include "y/sequence/vector.hpp"
+#include "y/sort/heap.hpp"
+
+static inline apn small_prime(const size_t nbit)
+{
+    static yap::library &lib = yap::library::instance();
+    apn p(alea,nbit);
+    return lib.next_prime_(p);
+    
+}
+
 Y_UTEST(kr_rsa)
 {
 
 
-   
 
     if(argc>1)
     {
@@ -68,20 +79,20 @@ Y_UTEST(kr_rsa)
         const apn      encoded = apn::mod_exp(message,pub->publicExponent,  pub->modulus);
         const uint64_t enc     = real_time_clock::ticks()-mark;
         
-        (std::cerr << "encoded: " << encoded << std::endl).flush();
-        (std::cerr << "        \\_ticks=" << enc << std::endl).flush();
+        (std::cerr << std::hex << "encoded: " << encoded << std::endl).flush();
+        (std::cerr << std::dec << "        \\_ticks=" << enc << std::endl).flush();
         
         mark                   = real_time_clock::ticks();
         const apn      decoded = apn::mod_exp(encoded,prv->privateExponent, prv->modulus);
         const uint64_t dec     = real_time_clock::ticks()-mark;
-        (std::cerr << "decoded: " << decoded << std::endl).flush();
-        (std::cerr << "        \\_ticks=" << dec << std::endl).flush();
+        (std::cerr << std::hex << "decoded: " << decoded << std::endl).flush();
+        (std::cerr << std::dec << "        \\_ticks=" << dec << std::endl).flush();
 
         mark                  = real_time_clock::ticks();
         const apn decoded_crt = prv->CRT(encoded);
         const uint64_t    crt = real_time_clock::ticks()-mark;
-        (std::cerr << "decoded: " << decoded_crt << std::endl).flush();
-        (std::cerr << "        \\_ticks=" << crt << std::endl).flush();
+        (std::cerr << std::hex << "decoded: " << decoded_crt << std::endl).flush();
+        (std::cerr << std::dec << "        \\_ticks=" << crt << std::endl).flush();
 
         Y_CHECK(decoded_crt==decoded);
         Y_CHECK(decoded==message);
@@ -90,9 +101,15 @@ Y_UTEST(kr_rsa)
     {
         std::cerr << std::dec;
         std::cerr << "Testing manual RSA" << std::endl;
-        const apn p = 61;
-        const apn q = 53;
+        const size_t nbit = 6;
+        const apn    p = small_prime(nbit);
+        apn q = small_prime(nbit);
+        while(q==p) q = small_prime(nbit);
         const apn e = 65537;
+        std::cerr << "\tp=" << p << std::endl;
+        std::cerr << "\tq=" << q << std::endl;
+        std::cerr << "\te=" << e << std::endl;
+
         const auto_ptr<crypto::rsa_public_key>  pub = crypto::rsa_public_key::pub(p,q,e);
         const auto_ptr<crypto::rsa_private_key> prv = crypto::rsa_private_key::prv(q,p,e); prv->check();
         std::cerr << "publicExponent  : " << pub->publicExponent  << std::endl;
@@ -103,17 +120,33 @@ Y_UTEST(kr_rsa)
         Y_CHECK(pub->modulus==prv->modulus);
         Y_CHECK(pub->publicExponent==prv->publicExponent);
         (std::cerr << "checking..." << std::endl).flush();
+        const size_t n = pub->modulus.cast_to<size_t>("modulus");
+
+        vector<apn> crypted(n,as_capacity);
+        uint64_t    raw_ticks = 0;
+        uint64_t    crt_ticks = 0;
         for(apn P=0;P<pub->modulus;++P)
         {
             (std::cerr << "  " << P << "  \r" ).flush();
-            const apn C = apn::mod_exp(P,pub->publicExponent,pub->modulus);
-            const apn M = apn::mod_exp(C,prv->privateExponent,prv->modulus);
-            const apn D = prv->CRT(C);
-            //std::cerr << P << " -> " << C << " -> " << M << "/" << D << std::endl;
+            const apn C    = apn::mod_exp(P,pub->publicExponent,pub->modulus);
+            uint64_t  mark = real_time_clock::ticks();
+            const apn M    = apn::mod_exp(C,prv->privateExponent,prv->modulus);
+            raw_ticks     += real_time_clock::ticks() - mark;
+            mark           = real_time_clock::ticks();
+            const apn D    = prv->CRT(C);
+            crt_ticks     += real_time_clock::ticks() - mark;
             Y_ASSERT(M==P);
             Y_ASSERT(D==P);
+            crypted.push_back_(C);
         }
         std::cerr << std::endl;
+        const double ratio = double(raw_ticks)/double(crt_ticks);
+        std::cerr << "crt speed up: " << ratio << std::endl;
+
+        hsort(crypted,apn::compare);
+
+
+
     }
 }
 Y_UTEST_DONE()
