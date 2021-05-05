72459c4a7801282bbd0eeff98f6bfb878f9685dd
Yann Bouret <yann.bouret@unice.fr>
2021-05-05 09:02:05 +0200

    updated dust.cmake

--- etc/dust.cmake
+++ etc/dust.cmake
@@ -8,5 +8,5 @@ make-clean
 
 FOREACH(script IN LISTS scripts )
 MESSAGE( STATUS " << executing ${script}.cmake >> ")
-EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -P scripts/${script}.cmake)
+EXECUTE_PROCESS(COMMAND ${CMAKE_COMMAND} -P etc/${script}.cmake)
 ENDFOREACH()
