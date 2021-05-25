d2bee7d542a45454048b5fecd9d30d196ad163be
Yann Bouret <yann.bouret@unice.fr>
2021-05-25 08:57:06 +0200

    need to check what happen when an aggregate accepts with no child

--- src/alchemy/y/alchemy/species.hpp
+++ src/alchemy/y/alchemy/species.hpp
@@ -10,12 +10,17 @@ namespace upsylon
 {
     namespace alchemy
     {
-        
+        //______________________________________________________________________
+        //
+        //
+        //! one species with minimal info
+        //
+        //______________________________________________________________________
         class species : public counted, public object
         {
         public:
-            const string name;
-            const int    z;
+            const string name; //!< identifier
+            const int    z;    //!< algebraic charge
             
         private:
             Y_DISABLE_COPY_AND_ASSIGN(species);
