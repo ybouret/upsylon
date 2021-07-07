09a31f43cd7abf28ad42795fd4e4b3006aba1ccf
Yann Bouret <yann.bouret@unice.fr>
2021-07-07 12:50:43 +0200

    wip

--- src/gfx/tests/test-mask.cpp
+++ src/gfx/tests/test-mask.cpp
@@ -1,5 +1,8 @@
 #include "y/gfx/mask.hpp"
 #include "y/utest/run.hpp"
+#include "y/ios/ocstream.hpp"
+#include "y/sequence/vector.hpp"
+#include "y/sort/heap.hpp"
 
 using namespace upsylon;
 using namespace graphic;
@@ -21,6 +24,19 @@ namespace
                 Y_ASSERT(++n==m.size());
             }
         }
+    }
+
+    static inline void save_data(const accessible<size_t> &px,
+                                 const accessible<size_t> &nl,
+                                 const char *filename)
+    {
+        assert(px.size()==nl.size());
+        ios::ocstream fp(filename);
+        for(size_t i=1;i<=px.size();++i)
+        {
+            fp("%u %u\n", unsigned( px[i] ), unsigned( nl[i] ) );
+        }
+
     }
 }
 
@@ -64,20 +80,59 @@ Y_UTEST(mask)
     }
 
     std::cerr << "Filling..." << std::endl;
-    for(unit_t w=1;w<=16;++w)
+
+    vector<size_t> w_px; // width first pixels
+    vector<size_t> w_nl; // and number of leaves
+
+    vector<size_t> h_px;
+    vector<size_t> h_nl;
+
+
+
+    for(unit_t w=1;w<=32;++w)
     {
-        for(unit_t h=1;h<=16;++h)
+        for(unit_t h=1;h<=32;++h)
         {
+            const size_t px = h*w;
             mask m;
             fill_mask(m,w,h);
             std::cerr << w << "x" << h << ":";
             m.free();
             //m.stats(std::cerr);
-            Y_ASSERT(size_t(w*h)==m.capacity());
+            Y_ASSERT(px==m.capacity());
+            const size_t leaves = m.get_tree().pool.size;
             std::cerr << m.get_tree().pool.size;
             std::cerr << std::endl;
+            if(w>h)
+            {
+                w_px << px;
+                w_nl << leaves;
+            }
+            else
+            {
+                if(w<h)
+                {
+                    h_px << px;
+                    h_nl << leaves;
+                }
+                else
+                {
+                    w_px << px;
+                    w_nl << leaves;
+                    h_px << px;
+                    h_nl << leaves;
+                }
+            }
         }
     }
+
+    hsort(w_px,w_nl, comparison::increasing<size_t> );
+    hsort(h_px,h_nl, comparison::increasing<size_t> );
+
+    save_data(w_px,w_nl,"mask-w.dat");
+    save_data(h_px,h_nl,"mask-h.dat");
+
+
 
 
 
