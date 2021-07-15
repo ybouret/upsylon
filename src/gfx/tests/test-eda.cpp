1cba3eadc56bf68be6878bd71e31d757a683fbaa
Yann Bouret <yann.bouret@unice.fr>
2021-07-15 10:59:41 +0200

    bug somewhere

--- src/gfx/tests/test-eda.cpp
+++ src/gfx/tests/test-eda.cpp
@@ -13,11 +13,17 @@
 #include "y/gfx/color/size-to-rgba.hpp"
 #include "y/gfx/color/ramp/cold-to-hot.hpp"
 #include "y/gfx/color/chart.hpp"
+#include "y/gfx/ops/gaussian-blur.hpp"
 
 using namespace upsylon;
 using namespace graphic;
 
 namespace {
+
+    static inline uint8_t on_off(const uint8_t x)
+    {
+        return x ? 0xff:0;
+    }
 }
 
 Y_UTEST(eda)
@@ -28,6 +34,8 @@ Y_UTEST(eda)
     color_ramp           ramp  = new color_tableau<cold_to_hot>();
     color_chart          chart(ramp);
     const shared_filters delta = filters_db::instance()["Sobel3"];
+    shared_knots         cache = new knots();
+    size_to_rgba         sz2c;
 
     if(argc>1)
     {
@@ -36,9 +44,20 @@ Y_UTEST(eda)
         broker              app(eng,img);
 
         // convert
-        const pixmap<float> pxm(img,app,convert<float,rgb>::from);
+        pixmap<float> pxm(img,app,convert<float,rgb>::from);
         IMG.save(img,"img.png");
         IMG.save(pxm,"pxm.png",0,ramp);
+        if(argc>2)
+        {
+            const float          sigma = string_convert::to<float>(argv[2],"sigma");
+            gaussian_blur<float> fuzz(7,7,sigma);
+            pixmap<float>        src(pxm.w,pxm.h);
+            fuzz.cover(src,app,pxm);
+            pxm.assign(src,app,convert<float,float>::from);
+        }
+
+        IMG.save(pxm,"src.png",0,ramp);
+
 
         // compute gradient
         edges::gradient grad(img.w,img.h);
@@ -48,8 +67,15 @@ Y_UTEST(eda)
         // keep only maximum parts
         edges::keep_max kmax(img.w,img.h);
         kmax(app,grad);
-        IMG.save(kmax,"kmax.png",0,chart);
+        IMG.save(kmax,"kmax.png");
         kmax.save("hist.dat");
+
+        {
+            pixmap<uint8_t> kbis(kmax,app,on_off);
+            IMG.save(kmax,"kbis.png");
+
+        }
+
 
         {
             const histogram old_kmax(kmax);
@@ -57,12 +83,21 @@ Y_UTEST(eda)
             Y_CHECK(old_kmax==kmax);
         }
 
+        // get historgram threshild
         const uint8_t  strong = kmax.threshold();
         const uint8_t  feeble = strong>>1;
+
+        // tighten edges
         edges::profile prof;
         prof.tighten(kmax,app,feeble,strong);
         std::cerr << "feeble=" << int(feeble) << " -> strong=" << int(strong) << std::endl;
         IMG.save(kmax,"prof.png",0,chart);
+
+        blobs          B;
+        pixmap<size_t> M(img.w,img.h);
+        (void) prof.track(B,kmax,M,cache);
+        B.sort(M);
+        IMG.save(M,"edges.png",0,sz2c);
 
         
     }
