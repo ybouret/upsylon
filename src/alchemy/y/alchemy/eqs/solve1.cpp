f523de1891c881c6fe75896739e9e6da6508db7a
Yann Bouret <yann.bouret@unice.fr>
2021-08-02 12:39:53 +0200

    first solve1, need to test

--- src/alchemy/y/alchemy/eqs/solve1.cpp
+++ src/alchemy/y/alchemy/eqs/solve1.cpp
@@ -59,7 +59,17 @@ namespace upsylon
 
                 case HasReverseLimit:
                     std::cerr << name << " has reverse limit | " << extents.reverse.value << std::endl;
-                    
+                    xi.a = -extents.reverse.value;
+                    eq.a = F(xi.a);
+                    xi.c = 1;
+
+                    while( (eq.c = F(xi.c)) * eq.a > 0 )
+                    {
+                        xi.c *= 2.0;
+                    }
+                    std::cerr << "xi=" << xi << std::endl;
+                    std::cerr << "eq=" << eq << std::endl;
+
                     break;
 
                 case HasMirrorLimits:
@@ -69,6 +79,30 @@ namespace upsylon
                 default: throw exception("%s corrupted extent", *name);
             }
 
+            zrid<double> zsolve;
+            if(!zsolve.run(F,xi,eq))
+            {
+                throw exception("%s has no zero point!",*name);
+            }
+
+            std::cerr << "xi=" << xi.b << std::endl;
+            const double Xi = xi.b;
+
+            for(const Actor::Node *a=reac->head();a;a=a->next)
+            {
+                const Actor &r = **a;
+                double      &c = C[r->indx];
+                c = max_of<double>(0.0,c-r.nu*Xi);
+            }
+
+            for(const Actor::Node *a=prod->head();a;a=a->next)
+            {
+                const Actor &p = **a;
+                double      &c = C[p->indx];
+                c = max_of<double>(0.0,c+p.nu*Xi);
+            }
+
+            std::cerr << "C=" << C << " / " << compute(K0,C) << std::endl;
 
         }
 
