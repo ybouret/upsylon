51c443415e271a92fceb43afb3747a90370f9d21
Yann Bouret <yann.bouret@unice.fr>
2021-08-13 12:27:38 +0200

    wip

--- src/alchemy/y/alchemy/eqs/balance.cpp
+++ src/alchemy/y/alchemy/eqs/balance.cpp
@@ -32,44 +32,9 @@ namespace upsylon
                         const Guard::State st = guards[eq.indx]->solve(C,NuT,Xi); std::cerr << "  " << Guard::StateText(st) << std::endl;
 
                     }
-
                 }
 
-                lib.display(std::cerr,C) << std::endl;
-                
-#if 0
-                //bool solvedPrimary = true;
-                for(size_t i=1;i<=N;++i)
-                {
-                    const Sentry &sentry = *sentries[i];
-                    std::cerr << "For " << eqs(i).name << " [" << sentry.typeText() << "]" << std::endl;
-                    switch(sentry.type)
-                    {
-                        case Sentry::HasNoBound: break;
-                        case Sentry::HasOnlyGEQ: {
-                            const Primary &pmin = sentry.getMin(C);
-                            lib.print( std::cerr << "min for ", lib(pmin.sp) ) << " = " << -C[pmin.sp] << std::endl;
-                            const Sentry::State state = sentry.solveHasOnlyGEQ(C,NuT, aliasing::_(xi));
-                            std::cerr << Sentry::StateText(state) << std::endl;
-                        }
-                        break;
-
-                        case Sentry::HasOnlyLEQ: {
-                            const Primary &pmax = sentry.getMax(C);
-                            lib.print( std::cerr << "max for ", lib(pmax.sp) ) << " = " << C[pmax.sp] << std::endl;
-                            const Sentry::State state = sentry.solveHasOnlyLEQ(C,NuT, aliasing::_(xi));
-                            std::cerr << Sentry::StateText(state) << std::endl;
-                        } break;
-
-                        case Sentry::IsBothWays: {
-                            const Sentry::State state = sentry.solveIsBothWays(C,NuT, aliasing::_(xi));
-                            std::cerr << Sentry::StateText(state) << std::endl;
-                        } break;
-                    }
-
-                }
-                lib.display(std::cerr,C) << std::endl;
-#endif
+                lib.display(std::cerr << "C=", C) << std::endl;
 
                 return false;
             }
