4b847d991b59394be47e4aa55c6e383fc889a1af
Yann Bouret <yann.bouret@unice.fr>
2021-08-17 11:02:23 +0200

    wip coding

---
+++ src/alchemy/y/alchemy/reactor/check-topology.cpp
@@ -0,0 +1,52 @@
+#include "y/alchemy/reactor.hpp"
+#include "y/mkl/kernel/apk.hpp"
+#include "y/exception.hpp"
+#include "y/code/textual.hpp"
+
+
+namespace upsylon
+{
+    using namespace mkl;
+
+    namespace Alchemy
+    {
+
+        void Reactor:: checkTopology()
+        {
+            std::cerr << "  <Topology>" << std::endl;
+            //__________________________________________________________________
+            //
+            //
+            // initialize Nu and Nu'
+            //
+            //__________________________________________________________________
+            eqs.fill( aliasing::_(Nu) );
+            aliasing::_(NuT).assign_transpose(Nu);
+            std::cerr << " Nu     = " << Nu  << std::endl;
+            std::cerr << " NuT    = " << NuT << std::endl;
+
+            //__________________________________________________________________
+            //
+            //
+            // checking consistency
+            //
+            //__________________________________________________________________
+            if(N>0)
+            {
+                const size_t r = apk::rank(Nu);
+                std::cerr << " NuT    = " << NuT << std::endl;
+                std::cerr << " NuRank = " << r   << "/" << N << std::endl;
+
+                if(r<N)
+                {
+                    const unsigned long ns = static_cast<unsigned long>(N-r);
+                    throw exception("%s has %lu redundant equilibri%s",CLID,ns,textual::plural_a(ns));
+                }
+            }
+            std::cerr << "  <Topology/>" << std::endl;
+        }
+
+    }
+
+}
+
---
+++ src/alchemy/y/alchemy/reactor/make-balancing.cpp
@@ -0,0 +1,112 @@
+#include "y/alchemy/reactor.hpp"
+#include "y/mkl/kernel/apk.hpp"
+#include "y/exception.hpp"
+
+
+namespace upsylon
+{
+    using namespace mkl;
+
+    namespace Alchemy
+    {
+        void Reactor:: makeBalancing()
+        {
+            //__________________________________________________________________
+            //
+            // Build balancing info:
+            // outer loop over species
+            //__________________________________________________________________
+            std::cerr << "  <Balancing>" << std::endl;
+            for(const Species::Node *snode = lib->head(); snode; snode=snode->next)
+            {
+                const Species &sp = ***snode;
+                const size_t   si = sp.indx;
+
+                //______________________________________________________________
+                //
+                // initialize
+                //______________________________________________________________
+                const accessible<unit_t> &v   = NuT[si];
+                addressable<unit_t>      &s   = aliasing::_(NuTS[si]);
+                size_t                    nok = 0;
+                const Equilibrium        *pEq = 0;
+                unit_t                    snu = 0;
+
+                //______________________________________________________________
+                //
+                // inner loop over equilibria
+                //______________________________________________________________
+                for(const Equilibrium::Node *enode=eqs->head();enode;enode=enode->next)
+                {
+                    const Equilibrium &eq  = ***enode;
+                    const size_t       ei  = eq.indx;
+                    const unit_t       cof = v[ei];
+                    if(0!=cof)
+                    {
+                        ++nok;
+                        pEq = &eq;
+                        snu = cof;
+                    }
+                }
+
+                if(nok>0)
+                {
+                    assert(true==active[sp.indx]);
+                    assert(snu!=0);
+                    assert(pEq!=NULL);
+
+                    if(1==nok)
+                    {
+                        Guard &guard = aliasing::_(*guards[pEq->indx]);
+                        if(snu>0)
+                        {
+                            //__________________________________________________
+                            //
+                            // Create a new GEQ
+                            //__________________________________________________
+                            const Leading leading(*pEq,sp, static_cast<size_t>(snu), Leading::GEQ);
+                            leading.print(std::cerr << "   | ",lib,eqs) << std::endl;
+                            guard.addGEQ(leading);
+                        }
+                        else
+                        {
+                            //__________________________________________________
+                            //
+                            // Create a new LEQ
+                            //__________________________________________________
+                            const Leading leading(*pEq,sp, static_cast<size_t>(-snu), Leading::LEQ);
+                            leading.print(std::cerr << "   | ",lib,eqs) << std::endl;
+                            guard.addLEQ(leading);
+                        }
+                        aliasing::incr(NL);
+                    }
+                    else
+                    {
+                        aliasing::incr(NS);
+                        tao::set(s,v);
+                    }
+
+                }
+                else
+                {
+                    assert(false==active[sp.indx]);
+                    tao::set(s,v);
+                }
+
+            }
+            aliasing::_(NuS).assign_transpose(NuTS);
+            std::cerr << "    NuTS  = " << NuTS << std::endl;
+            std::cerr << "    NuS   = " << NuS  << std::endl;
+            std::cerr << "    NEqs  = " << N    << std::endl;
+            std::cerr << "    NActv = " << NA   << std::endl;
+            std::cerr << "      NLead = " << NL   << std::endl;
+            std::cerr << "      NScnd = " << NS   << std::endl;
+            std::cerr << "      Srank = " << apk::rank(NuS) << std::endl;
+            assert(NL+NS==NA);
+            std::cerr << "  <Balancing/>" << std::endl;
+        }
+    }
+
+}
+
+
