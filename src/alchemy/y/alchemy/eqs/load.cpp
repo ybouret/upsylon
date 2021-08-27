d08cb2ee2086bf8fa10fbd2250a9c75e5b69911d
Yann Bouret <yann.bouret@unice.fr>
2021-08-27 08:19:54 +0200

    wip

--- src/alchemy/y/alchemy/eqs/load.cpp
+++ src/alchemy/y/alchemy/eqs/load.cpp
@@ -19,11 +19,11 @@ namespace upsylon
             lua_settop(L,0);
             lua_getglobal(L,*id);
             if(!lua_istable(L,-1)) throw exception("%s%s '%s' is not a table",CLID,fn,*id);
-            const unsigned long n = lua_rawlen(L,-1);
-            for(unsigned long i=1;i<=n;++i)
+            const int n = int( lua_rawlen(L,-1) );
+            for(int i=1;i<=n;++i)
             {
                 lua_rawgeti(L,-1,i);
-                if(!lua_isstring(L,-1) )throw exception("%s%s %s[%lu] is not a string",CLID,fn,*id,i);
+                if(!lua_isstring(L,-1) )throw exception("%s%s %s[%d] is not a string",CLID,fn,*id,i);
                 const string info = lua_tostring(L,-1);
                 lua_pop(L,1);
                 parse(info,lib,vm);
