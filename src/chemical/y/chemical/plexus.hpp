e2497c23fa98f4bfdbf34ce0546b69c37f8032d6
Yann Bouret <yann.bouret@unice.fr>
2021-09-13 11:48:51 +0200

    wip

---
+++ src/chemical/y/chemical/plexus.hpp
@@ -0,0 +1,35 @@
+
+//! \file
+#ifndef Y_CHEMICAL_SYSTEM_INCLUDED
+#define Y_CHEMICAL_SYSTEM_INCLUDED 1
+
+#include "y/chemical/sys/primary.hpp"
+#include "y/chemical/sys/replica.hpp"
+
+
+namespace upsylon
+{
+    namespace Chemical
+    {
+        //______________________________________________________________________
+        //
+        //
+        //! System
+        //
+        //______________________________________________________________________
+        class Plexus : public Object
+        {
+        public:
+            //__________________________________________________________________
+            //
+            // types and definitions
+            //__________________________________________________________________
+            static const char CLID[];         //!< "Chemical::System"
+
+        }
+
+    }
+
+}
+
+
