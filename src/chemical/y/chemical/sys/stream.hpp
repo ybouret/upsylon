e800ccde9fa53f9f5bd7649ec4d9eedf386f4ac5
Yann Bouret <yann.bouret@unice.fr>
2021-09-21 10:18:14 +0200

    wip stream

--- src/chemical/y/chemical/sys/stream.hpp
+++ src/chemical/y/chemical/sys/stream.hpp
@@ -13,24 +13,47 @@ namespace upsylon
 
         namespace Stream
         {
+            enum Genus
+            {
+                IsLineage,
+                IsPrimary,
+            };
 
-
-            class Vertex : public Object
+            //! a variant vertex in the graph
+            class Vertex : public Object, public dnode<Vertex>
             {
             public:
-                enum Genus
-                {
-                    IsStrain,
-                    IsPrimary,
-                };
 
                 const Genus genus;
-                
+                union {
+                    const Lineage *lineage;
+                    const Primary *primary;
+                };
 
+                explicit Vertex(const Lineage &) throw();
+                explicit Vertex(const Primary &) throw();
+                virtual ~Vertex() throw();
 
             private:
                 Y_DISABLE_COPY_AND_ASSIGN(Vertex);
             };
+
+            typedef core::list_of_cpp<Vertex> Vertices;
+
+            //! graph
+            class Graph
+            {
+            public:
+                explicit Graph(const Lineage::Array &lineage,
+                               const Primary::Array &primary);
+                virtual ~Graph() throw();
+
+                const Vertices vertices;
+                
+            private:
+                Y_DISABLE_COPY_AND_ASSIGN(Graph);
+            };
+
 
         }
 
