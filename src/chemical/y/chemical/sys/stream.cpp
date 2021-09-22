0b33d3b19ec9714aa890fc7494ac4cac718df0c5
Yann Bouret <yann.bouret@unice.fr>
2021-09-22 11:03:50 +0200

    wip

--- src/chemical/y/chemical/sys/stream.cpp
+++ src/chemical/y/chemical/sys/stream.cpp
@@ -107,7 +107,7 @@ namespace upsylon
             }
 
             template <typename LIST>
-            static inline void graphVizList(ios::ostream &fp, const LIST &L)
+            static inline void graphVizVertices(ios::ostream &fp, const LIST &L)
             {
                 for(const typename LIST::node_type *v=L.head;v;v=v->next)
                 {
@@ -115,14 +115,52 @@ namespace upsylon
                 }
             }
 
+            template <typename EDGES>
+            static inline void graphVizEdges(ios::ostream &fp,
+                                             const EDGES  &edges,
+                                             const char   *arrowhead,
+                                             const bool    showWeight)
+            {
+                assert(arrowhead);
+                for(const typename EDGES::node_type *edge=edges.head;edge;edge=edge->next)
+                {
+                    const Vizible &src = *(edge->source);
+                    const Vizible &tgt = *(edge->target);
+                    src.vizName(fp); Vizible::arrow(fp); tgt.vizName(fp);
+                    fp << '[';
+                    fp << "arrowhead=" << arrowhead;
+                    if(showWeight && (edge->weight>1) )
+                    {
+                        fp(",label=%u",unsigned(edge->weight));
+                    }
+                    fp << ']';
+                    Vizible::endl(fp);
+                }
+            }
+
+
             void Graph:: graphViz(const string &fileName) const
             {
                 {
                     ios::ocstream fp(fileName);
 
                     Vizible::enterDigraph(fp,"G");
-                    graphVizList(fp,lineageVertices);
-                    graphVizList(fp,primaryVertices);
+                    Vizible::endl(fp << "edge [colorscheme=spectral11]");
+
+                    graphVizVertices(fp,lineageVertices);
+                    graphVizVertices(fp,primaryVertices);
+
+
+                    graphVizEdges(fp,forwardOutgoingEdges,"normal",true);
+                    graphVizEdges(fp,forwardIncomingEdges,"vee",true);
+
+                    if(false)
+                    {
+                        graphVizEdges(fp,reverseOutgoingEdges,"onormal",false);
+                        graphVizEdges(fp,reverseIncomingEdges,"ovee",false);
+                    }
+                    
+
                     Vizible::leaveDigraph(fp);
 
                 }
